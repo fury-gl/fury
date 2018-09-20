6d74f11157480096ad2f4ce4da6e7eaf6a42c27b
Serge Koudoro <skab12@gmail.com>
2018-09-20 16:13:57 +0200

    add __version__

--- fury/__init__.py
+++ fury/__init__.py
@@ -1,5 +1,9 @@
 # Init file for visualization package
 from __future__ import division, print_function, absolute_import
+from fury._version import get_versions
+
+__version__ = get_versions()['version']
+del get_versions
 
 # We make the visualization requirements optional imports:
 
