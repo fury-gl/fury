0e6cfb627a6a421145147a4e6021aee74a0a4391
Serge Koudoro <skab12@gmail.com>
2018-09-24 21:03:24 +0200

    split requirements

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
-with open(path.join(here, 'requirements.txt')) as requirements_file:
+with open(path.join(here, 'requirements', 'default.txt')) as requirements_file:
     # Parse requirements.txt, ignoring any commented-out lines.
     requirements = [line for line in requirements_file.read().splitlines()
                     if not line.startswith('#')]
