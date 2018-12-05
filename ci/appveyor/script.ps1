3bd544c2afdbdb8c9cd909a22dfccfab789b429e
skoudoro <skab12@gmail.com>
2018-12-05 17:18:46 -0500

    replace invoke-expression by invoke command

--- ci/appveyor/script.ps1
+++ ci/appveyor/script.ps1
@@ -3,8 +3,8 @@
   
 # Print and Install FURY
 Write-Host "Install FURY"
-Invoke-Expression "$env:PIPI --user -e ."
+Invoke-Command { $env:PIPI --user -e .}
 
 # Run tests
 Write-Host "Run FURY tests"
-Invoke-Expression "pytest -svv fury"
+Invoke-Command {pytest -svv fury}
