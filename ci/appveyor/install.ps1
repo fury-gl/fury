46de3a8b8a8b5faadbc15b7e43dec51600f7d1df
skoudoro <skab12@gmail.com>
2018-12-05 15:07:51 -0500

    move python version setup

--- ci/appveyor/install.ps1
+++ ci/appveyor/install.ps1
@@ -1,20 +1,9 @@
 # Powershell Install script
 
-# Setup some environment variable 
-$env:Path = "$env:PYTHON;$env:PYTHON\\Scripts;$env:Path"
+
 $env:PIPI = "pip install $env:EXTRA_PIP_FLAGS"
-
 # Print and check this environment variable
-Write-Host $env:PIPI
-
-# Check that we have the expected version and architecture for Python
-Invoke-Expression "python --version"
-
-$env:PYTHON_ARCH = (Invoke-Expression "python -c 'import struct; print(struct.calcsize("P") * 8)'") | Out-String
-$env:PYTHON_VERSION = (Invoke-Expression "python -c 'import platform;print(platform.python_version())'") | Out-String
-
-Write-Host "Python version: " + $env:PYTHON_VERSION 
-Write-Host "Python architecture: " + $env:PYTHON_ARCH
+Write-Host "Pip command: " + $env:PIPI
 
 if($env:PYTHON -match "conda")
 {
