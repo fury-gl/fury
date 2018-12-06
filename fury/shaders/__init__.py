75221904cf9f527dbbd524b8a3d55c72bbdd2137
David Reagan <dmreagan@iu.edu>
2018-12-06 17:09:25 -0500

    Pep8

--- fury/shaders/__init__.py
+++ fury/shaders/__init__.py
@@ -4,6 +4,7 @@ from os.path import join as pjoin, dirname
 
 SHADERS_DIR = pjoin(dirname(__file__))
 
+
 def load(filename):
     with open(pjoin(SHADERS_DIR, filename)) as shader_file:
         return shader_file.read()
