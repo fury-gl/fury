3f3ecc8968d4cfcaf1d3d7eb9effbfff988aa236
skoudoro <skab12@gmail.com>
2019-02-06 17:28:22 -0500

    add load_image save_image

--- fury/io.py
+++ fury/io.py
@@ -1,6 +1,126 @@
+import os
 import numpy as np
 import vtk
+from vtk.util import numpy_support
 from fury.utils import set_input
+
+
+def load_image(file_name, as_vtktype=False):
+    """Load an image.
+
+    Parameters
+    ----------
+    file_name: string
+        should be *.png, *.bmp, *.jpeg, *.jpg
+    as_vtktype: bool
+        if True, return vtk output otherwise a ndarray. default(False)
+
+    Returns
+    -------
+    image: ndarray or vtk output
+
+    """
+    d_reader = {".png": vtk.vtkPNGReader,
+                ".bmp": vtk.vtkBMPReader,
+                ".jpeg": vtk.vtkJPEGReader,
+                ".jpg": vtk.vtkJPEGReader,
+                ".tiff": vtk.vtkTIFFReader,
+                ".tif": vtk.vtkTIFFReader,
+                }
+
+    extension = os.path.splitext(os.path.basename(file_name).lower())[1]
+
+    if extension.lower() not in d_reader.keys():
+        raise IOError("Impossible to read the file {0}: Unknown extension {1}".
+                      format(file_name, extension))
+
+    reader = d_reader.get(extension)()
+    reader.SetFileName(file_name)
+    reader.Update()
+    reader.GetOutput().GetPointData().GetArray(0).SetName("original")
+
+    if as_vtktype:
+        return reader.GetOutput()
+
+    h, w, _ = reader.GetOutput().GetDimensions()
+    vtk_array = reader.GetOutput().GetPointData().GetScalars()
+    # print(vtk_array.GetDataSize(), vtk_array.GetNumberOfComponents(), vtk_array.GetDataType)
+    components = vtk_array.GetNumberOfComponents()
+    image = numpy_support.vtk_to_numpy(vtk_array).reshape(h, w, components)
+    return image
+
+
+def save_image(arr, file_name, compression_quality=100,
+               compression_type='deflation'):
+    """Save an 2d or 3d image 
+
+    Parameters
+    ----------
+    arr: ndarray
+        array to save
+    file_name: string
+        should be *.png, *.bmp, *.jpeg, *.jpg
+    compression_quality: int
+        compression_quality for jpeg data.
+        0 = Low quality, 100 = High quality
+    compression_type: str
+        compression type for tiff file
+        select between: None, jpeg, lzw, deflation (default)
+
+
+    """
+    if arr.ndim > 3:
+        raise IOError("Image Dimensions should be <=3")
+    if arr.ndim == 2:
+        arr = arr[..., None]
+
+    d_writer = {".png": vtk.vtkPNGWriter,
+                ".bmp": vtk.vtkBMPWriter,
+                ".jpeg": vtk.vtkJPEGWriter,
+                ".jpg": vtk.vtkJPEGWriter,
+                ".tiff": vtk.vtkTIFFWriter,
+                ".tif": vtk.vtkTIFFWriter,
+                }
+
+    extension = os.path.splitext(os.path.basename(file_name).lower())[1]
+
+    if extension.lower() not in d_writer.keys():
+        raise IOError("Impossible to save the file {0}: Unknown extension {1}".
+                      format(file_name, extension))
+
+    vtk_array_type = numpy_support.get_vtk_array_type(arr.dtype)
+    vtk_array = numpy_support.numpy_to_vtk(num_array=arr.ravel(), deep=True,
+                                           array_type=vtk_array_type)
+
+    # Todo, look the following link for managing png 16bit
+    # https://stackoverflow.com/questions/15667947/vtkpngwriter-printing-out-black-images
+    vtk_data = vtk.vtkImageData()
+    vtk_data.SetDimensions(arr.shape)
+    vtk_data.SetSpacing(1.0, 1.0, 1.0)
+    vtk_data.SetOrigin(0.0, 0.0, 0.0)
+    vtk_data.GetPointData().SetScalars(vtk_array)
+
+    writer = d_writer.get(extension)()
+    writer.SetFileName(file_name)
+    writer.SetInputData(vtk_data)
+    if extension.lower() in [".jpg", ".jpeg"]:
+        writer.ProgressiveOn()
+        writer.SetQuality(compression_quality)
+    if extension.lower() in [".tif", ".tiff"]:
+        if not compression_type:
+            print('None')
+            writer.SetCompressionToNoCompression()
+        elif compression_type.lower() == 'jpeg':
+            print('jpeg')
+            writer.SetCompressionToJPEG()
+        elif compression_type.lower() == 'lzw':
+            print('lzw')
+            writer.SetCompressionToLZW()
+        elif compression_type.lower() == 'deflation':
+            print('deflation')
+            writer.SetCompressionToDeflate()
+
+    writer.Write()
 
 
 def load_polydata(file_name, is_mni_obj=False):
