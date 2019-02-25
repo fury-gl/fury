747097b113021646d8ff646c1a3b841b8c4b94b9
skoudoro <skab12@gmail.com>
2019-02-25 12:57:42 -0500

    add test for optpkg

---
+++ fury/tests/test_optpkg.py
@@ -0,0 +1,40 @@
+"""Function for testing optpkg module."""
+
+import numpy.testing as npt
+from fury.testing import assert_true, assert_false
+from fury.optpkg import is_tripwire, TripWire, TripWireError, optional_package
+from types import ModuleType
+
+
+def test_is_tripwire():
+    assert_false(is_tripwire(object()))
+    assert_true(is_tripwire(TripWire('some message')))
+
+
+def test_tripwire():
+    # Test tripwire object
+    silly_module_name = TripWire('We do not have silly_module_name')
+    npt.assert_raises(TripWireError,
+                      getattr,
+                      silly_module_name,
+                      'do_silly_thing')
+    npt.assert_raises(TripWireError,
+                      silly_module_name)
+    # Check AttributeError can be checked too
+    try:
+        silly_module_name.__wrapped__
+    except TripWireError as err:
+        assert_true(isinstance(err, AttributeError))
+    else:
+        raise RuntimeError("No error raised, but expected")
+
+
+def test_optional_package():
+    pkg, have_pkg, _ = optional_package('fake_pkg')
+    npt.assert_raises(TripWireError, pkg)
+    assert_false(have_pkg)
+
+    pkg, have_pkg, _ = optional_package('os')
+    assert_true(isinstance(pkg, ModuleType))
+    npt.assert_equal(pkg.__name__, 'os')
+    assert_true(have_pkg)
