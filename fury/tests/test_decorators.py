2702a431250c206b54eb936b1ee7ee0f826c3d26
skoudoro <skab12@gmail.com>
2019-02-25 14:54:39 -0500

    pep8 global

--- fury/tests/test_decorators.py
+++ fury/tests/test_decorators.py
@@ -4,7 +4,8 @@ import numpy.testing as npt
 from fury.testing import assert_true
 from fury.decorators import doctest_skip_parser
 
-global HAVE_AMODULE, HAVE_BMODULE
+HAVE_AMODULE = False
+HAVE_BMODULE = True
 
 
 def test_skipper():
@@ -18,9 +19,6 @@ def test_skipper():
         >>> something2   # skip if HAVE_AMODULE
         """
     f.__doc__ = docstring
-    global HAVE_AMODULE, HAVE_BMODULE
-    HAVE_AMODULE = False
-    HAVE_BMODULE = True
     f2 = doctest_skip_parser(f)
     assert_true(f is f2)
     npt.assert_equal(f2.__doc__,
@@ -30,6 +28,7 @@ def test_skipper():
         >>> a = 1
         >>> something2
         """)
+    global HAVE_AMODULE, HAVE_BMODULE
     HAVE_AMODULE = True
     HAVE_BMODULE = False
     f.__doc__ = docstring
