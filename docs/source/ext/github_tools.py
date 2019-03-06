b6ca09be2e0781b917f7c46f2e70c3a6be34e9a6
Serge Koudoro <skab12@gmail.com>
2019-03-06 14:54:26 -0500

    update authentification

--- docs/source/ext/github_tools.py
+++ docs/source/ext/github_tools.py
@@ -21,7 +21,7 @@ import operator
 from datetime import datetime, timedelta
 from subprocess import check_output
 
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 
 # ----------------------------------------------------------------------------
 # Globals
@@ -36,7 +36,6 @@ rel_pat = re.compile(r'rel=[\'"](\w+)[\'"]')
 LAST_RELEASE = datetime(2015, 3, 18)
 CONTRIBUTORS_FILE = "contributors.json"
 GH_TOKEN = os.environ.get('GH_TOKEN', '')
-GH_TOKEN += '@' if GH_TOKEN else ''
 
 
 # ----------------------------------------------------------------------------
@@ -59,7 +58,10 @@ def get_paged_request(url):
     results = []
     while url:
         print("fetching %s" % url, file=sys.stderr)
-        f = urlopen(url)
+        request = Request(url)
+        if GH_TOKEN:
+            request.add_header('Authorization', 'token {}'.format(GH_TOKEN))
+        f = urlopen(request)
         results.extend(json.load(f))
         links = parse_link_header(f.headers)
         url = links.get('next')
@@ -69,14 +71,14 @@ def get_paged_request(url):
 def get_issues(project="fury-gl/fury", state="closed", pulls=False):
     """Get a list of the issues from the Github API."""
     which = 'pulls' if pulls else 'issues'
-    url = "https://%sapi.github.com/repos/%s/%s?state=%s&per_page=%i" % \
-        (GH_TOKEN, project, which, state, PER_PAGE)
+    url = "https://api.github.com/repos/%s/%s?state=%s&per_page=%i" % \
+        (project, which, state, PER_PAGE)
     return get_paged_request(url)
 
 
 def get_tags(project="fury-gl/fury"):
     """Get a list of the tags from the Github API."""
-    url = "https://{0}api.github.com/repos/{1}/tags".format(GH_TOKEN, project)
+    url = "https://api.github.com/repos/{0}/tags".format(project)
     return get_paged_request(url)
 
 
@@ -100,7 +102,7 @@ def fetch_basic_stats(project="fury-gl/fury"):
     desired_keys = ["stargazers_count", "stargazers_url", "watchers_count",
                     "watchers_url", "forks_count", "forks_url", "open_issues",
                     "issues_url", "subscribers_count", "subscribers_url"]
-    url = "https://{0}api.github.com/repos/{1}".format(GH_TOKEN, project)
+    url = "https://api.github.com/repos/{0}".format(project)
     f = urlopen(url)
     r_json = json.load(f)
     basic_stats = dict((k, r_json[k]) for k in desired_keys if k in r_json)
@@ -136,7 +138,7 @@ def fetch_contributor_stats(project="fury-gl/fury"):
         }
 
     """
-    url = "https://{0}api.github.com/repos/{1}/stats/contributors".format(GH_TOKEN,project)
+    url = "https://api.github.com/repos/{0}/stats/contributors".format(project)
     f = urlopen(url)
     r_json = json.load(f)
 
@@ -223,8 +225,8 @@ def issues_closed_since(period=LAST_RELEASE, project="fury-gl/fury",
 
     if isinstance(period, timedelta):
         period = datetime.now() - period
-    url = ("https://%sapi.github.com/repos/%s/%s?state=closed&sort=updated&"
-           "since=%s&per_page=%i") % (GH_TOKEN, project, which,
+    url = ("https://api.github.com/repos/%s/%s?state=closed&sort=updated&"
+           "since=%s&per_page=%i") % (project, which,
                                       period.strftime(ISO8601), PER_PAGE)
     allclosed = get_paged_request(url)
     # allclosed = get_issues(project=project, state='closed', pulls=pulls,
