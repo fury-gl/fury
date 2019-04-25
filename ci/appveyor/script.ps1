e58cc28f4a661fec3d5589c8354c6d2459ee53e1
Serge Koudoro <skab12@gmail.com>
2019-04-25 11:29:59 -0400

    return error code

--- ci/appveyor/script.ps1
+++ ci/appveyor/script.ps1
@@ -2,20 +2,13 @@
 
 $env:PIPI = "pip install $env:EXTRA_PIP_FLAGS"
 # Print and check this environment variable
-Write-Output "Pip command: " + $env:PIPI
+Write-Output "Pip command: $env:PIPI"
 
 
 # Print and Install FURY
-Write-Output "Install FURY"
-Invoke-Expression "$env:PIPI --user -e ."
+Write-Output "======================== Install FURY ========================"
+$env:PIPI --user -e .
 
 # Run tests
-Write-Output "Run FURY tests"
+Write-Output "======================== Run FURY tests ========================"
 pytest -svv fury
-# try {
-#     Invoke-Expression "pytest -svv fury"
-# } catch {
-#     Write-Output "Error, Exception "
-#     Write-Host $_
-#     exit 1
-# }
