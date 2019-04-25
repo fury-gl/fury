36f46b8cc8d73deb9f351b971e143400c8477be5
Serge Koudoro <skab12@gmail.com>
2019-04-25 10:39:12 -0400

    run without invoke-expression

--- ci/appveyor/script.ps1
+++ ci/appveyor/script.ps1
@@ -2,19 +2,20 @@
 
 $env:PIPI = "pip install $env:EXTRA_PIP_FLAGS"
 # Print and check this environment variable
-Write-Host "Pip command: " + $env:PIPI
+Write-Output "Pip command: " + $env:PIPI
 
 
 # Print and Install FURY
-Write-Host "Install FURY"
+Write-Output "Install FURY"
 Invoke-Expression "$env:PIPI --user -e ."
 
 # Run tests
-Write-Host "Run FURY tests"
-try {
-    Invoke-Expression "pytest -svv fury"
-} catch {
-    Write-Host "Error, Exception "
-    Write-Host $_
-    exit 1
-}
+Write-Output "Run FURY tests"
+pytest -svv fury
+# try {
+#     Invoke-Expression "pytest -svv fury"
+# } catch {
+#     Write-Output "Error, Exception "
+#     Write-Host $_
+#     exit 1
+# }
