1ae248b1fe7e6c8b9d92dddcf2bd0a8f29eaf1e7
Serge Koudoro <skab12@gmail.com>
2019-08-12 15:37:23 -0400

    update test_timer

--- fury/tests/test_ui.py
+++ fury/tests/test_ui.py
@@ -811,9 +811,10 @@ def test_timer():
     showm.add_timer_callback(True, 200, timer_callback)
     showm.start()
 
-    # arr = window.snapshot(scene)
+    del showm.scene
+    arr = window.snapshot(scene)
 
-    # npt.assert_(np.sum(arr) > 0)
+    npt.assert_(np.sum(arr) > 0)
 
 
 def test_ui_file_menu_2d(interactive=False):
