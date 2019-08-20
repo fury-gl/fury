80bf7ddccdd16e02d33ea504323509cb759decc9
Serge Koudoro <skab12@gmail.com>
2019-08-20 14:36:21 -0400

    update osx build and catch warnings

--- fury/tests/test_colormap.py
+++ fury/tests/test_colormap.py
@@ -63,11 +63,12 @@ def test_create_colormap():
                       value.reshape((5, 5)))
     npt.assert_raises(ValueError, colormap.create_colormap,
                       value, name='fake')
-    npt.assert_warns(PendingDeprecationWarning, colormap.create_colormap, value,
-                     name='jet', auto=False)
+    npt.assert_warns(PendingDeprecationWarning, colormap.create_colormap,
+                     value, name='jet', auto=False)
 
     if not have_matplotlib:
-        npt.assert_warns(UserWarning, colormap.create_colormap, value)
+        with npt.assert_warns(UserWarning):
+            npt.assert_raises(ValueError, colormap.create_colormap, value)
 
 
 def test_lab_delta():
