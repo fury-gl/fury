1e30080a9973802c004cac40b141472be07d835b
Serge Koudoro <skab12@gmail.com>
2019-09-06 23:47:47 +0200

    python version ok, refresh env ?

--- ci/windows/install.ps1
+++ ci/windows/install.ps1
@@ -4,13 +4,13 @@ if($env:INSTALL_TYPE -match "conda")
 {
   # Get Anaconda path
   Write-Output "Conda path: $env:CONDA\Scripts"
-  gci env:*
+  #gci env:*
 
   Invoke-Expression "conda config --set always_yes yes --set changeps1 no"
   Invoke-Expression "conda update -yq conda"
   Invoke-Expression "conda install conda-build anaconda-client"
   Invoke-Expression "conda config --add channels conda-forge"
-  Invoke-Expression "conda create -n testenv --yes python=$env:python.version pip"
+  Invoke-Expression "conda create -n testenv --yes python=$env:PYTHON_VERSION pip"
   Invoke-Expression "conda activate testenv"
   Invoke-Expression "conda install -yq --file requirements/default.txt"
   Invoke-Expression "conda install -yq --file requirements/test.txt"
