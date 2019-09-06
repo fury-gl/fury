8aaf1faf4f8cec8b6bcbc6497d379bfa52a7aa83
Serge Koudoro <skab12@gmail.com>
2019-09-06 23:58:56 +0200

    change the way to activate env

--- ci/windows/install.ps1
+++ ci/windows/install.ps1
@@ -11,7 +11,7 @@ if($env:INSTALL_TYPE -match "conda")
   Invoke-Expression "conda install conda-build anaconda-client"
   Invoke-Expression "conda config --add channels conda-forge"
   Invoke-Expression "conda create -n testenv --yes python=$env:PYTHON_VERSION pip"
-  Invoke-Expression "conda activate testenv"
+  CALL conda.bat activate testenv
   Invoke-Expression "conda install -yq --file requirements/default.txt"
   Invoke-Expression "conda install -yq --file requirements/test.txt"
   if($env:OPTIONAL_DEPS)
