7f6e801f4c9546da87d9701ebf842b600128d34b
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2019-09-11 17:35:23 -0400

    Trying different ideas

--- docs/examples/viz_shader_wave.py
+++ docs/examples/viz_shader_wave.py
@@ -2,7 +2,8 @@
 import numpy as np
 from fury import actor, window, ui
 from fury.utils import numpy_to_vtk_points, set_polydata_colors
-from fury.utils import set_input, rotate
+from fury.utils import set_input, rotate, set_polydata_vertices
+from fury.utils import get_actor_from_polydata
 import vtk
 
 def rectangle(size = (1, 1)):
@@ -40,6 +41,35 @@ def rectangle(size = (1, 1)):
     actor = vtk.vtkActor()
     actor.SetMapper(mapper)
     return actor
+
+
+def cube():
+    my_polydata = vtk.vtkPolyData()
+
+    my_vertices = np.array([[0.0, 0.0, 0.0],
+                            [0.0, 0.0, 1.0],
+                            [0.0, 1.0, 0.0],
+                            [0.0, 1.0, 1.0],
+                            [1.0, 0.0, 0.0],
+                            [1.0, 0.0, 1.0],
+                            [1.0, 1.0, 0.0],
+                            [1.0, 1.0, 1.0]])
+
+    my_triangles = np.array([[0, 6, 4],
+                            [0, 2, 6],
+                            [0, 3, 2],
+                            [0, 1, 3],
+                            [2, 7, 6],
+                            [2, 3, 7],
+                            [4, 6, 7],
+                            [4, 7, 5],
+                            [0, 4, 5],
+                            [0, 5, 1],
+                            [1, 5, 7],
+                            [1, 7, 3]], dtype='i8')
+
+    set_polydata_vertices(my_polydata, my_vertices)
+    return get_actor_from_polydata(my_polydata)
 
 
 def disk():
@@ -81,10 +111,13 @@ rec = rectangle(size=(100, 100))
 scene.add(rec)
 mapper = rec.GetMapper()
 
+# cu = cube()
+# scene.add(cu)
+# mapper = cu.GetMapper()
+
 # dis = disk()
 # scene.add(dis)
 # mapper = dis.GetMapper()
-
 
 mapper.AddShaderReplacement(
     vtk.vtkShader.Fragment,
@@ -109,7 +142,7 @@ def timer_callback(obj, event):
     global timer
     timer += 1.0
     # print(timer)
-    # showm.render()
+    showm.render()
 
 @window.vtk.calldata_type(window.vtk.VTK_OBJECT)
 def vtk_shader_callback(caller, event, calldata=None):
@@ -137,15 +170,20 @@ mapper.AddShaderReplacement(
     vec3 bColor = vec3(.0, .3, .9);
     vec3 yColor = vec3(.9, .9, .3);
 
-    float a = sin(normalVCVSOutput.y * 5. - time * .2) / 2.;
-    float b = cos(normalVCVSOutput.y * 5. - time * .2) / 2.;
-    float c = sin(normalVCVSOutput.y * 5. - time * .2 + 3.14) / 2.;
-    float d = cos(normalVCVSOutput.y * 5. - time * .2 + 3.14) / 2.;
+    float tm = .2; // speed
+    float vcm = 5;
 
-    float e = .01 / abs(normalVCVSOutput.x + a);
-    float f = .01 / abs(normalVCVSOutput.x + b);
-    float g = .01 / abs(normalVCVSOutput.x + c);
-    float h = .01 / abs(normalVCVSOutput.x + d);
+    float a = sin(normalVCVSOutput.y * vcm - time * tm) / 2.;
+    float b = cos(normalVCVSOutput.y * vcm - time * tm) / 2.;
+    float c = sin(normalVCVSOutput.y * vcm - time * tm + 3.14) / 2.;
+    float d = cos(normalVCVSOutput.y * vcm - time * tm + 3.14) / 2.;
+
+    float div = 0.1; // default 0.01
+
+    float e = div / abs(normalVCVSOutput.x + a);
+    float f = div / abs(normalVCVSOutput.x + b);
+    float g = div / abs(normalVCVSOutput.x + c);
+    float h = div / abs(normalVCVSOutput.x + d);
 
     vec3 destColor = rColor * e + gColor * f + bColor * g + yColor * h;
     fragOutput0 = vec4(destColor, 1.);
