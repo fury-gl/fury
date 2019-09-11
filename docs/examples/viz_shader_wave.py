6e3b547bf1b156fd9e3c93532acfda48262a7adb
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2019-09-11 15:58:12 -0400

    Checking some sine waves

---
+++ docs/examples/viz_shader_wave.py
@@ -0,0 +1,160 @@
+
+import numpy as np
+from fury import actor, window, ui
+from fury.utils import numpy_to_vtk_points, set_polydata_colors
+from fury.utils import set_input, rotate
+import vtk
+
+def rectangle(size = (1, 1)):
+
+    X, Y = size
+
+    # Setup four points
+    points = vtk.vtkPoints()
+    points.InsertNextPoint(-X/2, -Y/2, 0)
+    points.InsertNextPoint(-X/2, Y/2, 0)
+    points.InsertNextPoint(X/2, Y/2, 0)
+    points.InsertNextPoint(X/2, -Y/2, 0)
+
+    # Create the polygon
+    polygon = vtk.vtkPolygon()
+    polygon.GetPointIds().SetNumberOfIds(4)  # make a quad
+    polygon.GetPointIds().SetId(0, 0)
+    polygon.GetPointIds().SetId(1, 1)
+    polygon.GetPointIds().SetId(2, 2)
+    polygon.GetPointIds().SetId(3, 3)
+
+    # Add the polygon to a list of polygons
+    polygons = vtk.vtkCellArray()
+    polygons.InsertNextCell(polygon)
+
+    # Create a PolyData
+    polygonPolyData = vtk.vtkPolyData()
+    polygonPolyData.SetPoints(points)
+    polygonPolyData.SetPolys(polygons)
+
+    # Create a mapper and actor
+    mapper = vtk.vtkPolyDataMapper()
+    mapper = set_input(mapper, polygonPolyData)
+
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+    return actor
+
+
+def disk():
+
+    np.random.seed(42)
+    n_points = 1
+    centers = np.random.rand(n_points, 3)
+    colors = 255 * np.random.rand(n_points, 3)
+
+    vtk_points = numpy_to_vtk_points(centers)
+
+    points_polydata = vtk.vtkPolyData()
+    points_polydata.SetPoints(vtk_points)
+
+    vertex_filter = vtk.vtkVertexGlyphFilter()
+    vertex_filter.SetInputData(points_polydata)
+    vertex_filter.Update()
+
+    polydata = vtk.vtkPolyData()
+    polydata.ShallowCopy(vertex_filter.GetOutput())
+
+    # set_polydata_colors(polydata, colors)
+
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputData(polydata)
+
+    points_actor = vtk.vtkActor()
+    points_actor.SetMapper(mapper)
+    points_actor.GetProperty().SetPointSize(1000)
+    points_actor.GetProperty().SetRenderPointsAsSpheres(True)
+
+    return points_actor
+
+scene = window.Scene()
+# scene.background((1, 1, 1))
+showm = window.ShowManager(scene, size=(1920, 1080), order_transparent=True, interactor_style='custom')
+
+rec = rectangle(size=(100, 100))
+scene.add(rec)
+mapper = rec.GetMapper()
+
+# dis = disk()
+# scene.add(dis)
+# mapper = dis.GetMapper()
+
+
+mapper.AddShaderReplacement(
+    vtk.vtkShader.Fragment,
+    '//VTK::Light::Dec',
+    True,
+    '''
+    //VTK::Light::Dec
+    uniform float time;
+    ''',
+    False
+)
+
+import itertools
+counter = itertools.count(start=1)
+
+global timer
+
+timer = 0
+
+def timer_callback(obj, event):
+
+    global timer
+    timer += 1.0
+    # print(timer)
+    # showm.render()
+
+@window.vtk.calldata_type(window.vtk.VTK_OBJECT)
+def vtk_shader_callback(caller, event, calldata=None):
+    program = calldata
+    global timer
+    if program is not None:
+        try:
+            program.SetUniformf("time", timer)
+        except ValueError:
+            pass
+
+
+mapper.AddObserver(window.vtk.vtkCommand.UpdateShaderEvent, vtk_shader_callback)
+
+
+# TODO: Create Fragment Shader Canvas
+mapper.AddShaderReplacement(
+    vtk.vtkShader.Fragment,
+    '//VTK::Light::Impl',
+    True,
+    '''
+    //VTK::Light::Impl
+    vec3 rColor = vec3(.9, .0, .3);
+    vec3 gColor = vec3(.0, .9, .3);
+    vec3 bColor = vec3(.0, .3, .9);
+    vec3 yColor = vec3(.9, .9, .3);
+
+    float a = sin(normalVCVSOutput.y * 5. - time * .2) / 2.;
+    float b = cos(normalVCVSOutput.y * 5. - time * .2) / 2.;
+    float c = sin(normalVCVSOutput.y * 5. - time * .2 + 3.14) / 2.;
+    float d = cos(normalVCVSOutput.y * 5. - time * .2 + 3.14) / 2.;
+
+    float e = .01 / abs(normalVCVSOutput.x + a);
+    float f = .01 / abs(normalVCVSOutput.x + b);
+    float g = .01 / abs(normalVCVSOutput.x + c);
+    float h = .01 / abs(normalVCVSOutput.x + d);
+
+    vec3 destColor = rColor * e + gColor * f + bColor * g + yColor * h;
+    fragOutput0 = vec4(destColor, 1.);
+    ''',
+    False
+)
+
+showm.initialize()
+showm.add_timer_callback(True, 200, timer_callback)
+
+showm.initialize()
+showm.start()
