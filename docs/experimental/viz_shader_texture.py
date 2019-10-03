4656bf8cf2ebab8e5b551475b1304a838e2d31ad
Javier Guaje <jrguajeg@iu.edu>
2019-10-03 17:47:49 -0400

    Added sphere actor to the texture experiment.

--- docs/experimental/viz_shader_texture.py
+++ docs/experimental/viz_shader_texture.py
@@ -4,21 +4,40 @@ from viz_shader_canvas import cube
 
 import vtk
 
-
-scene = window.Scene()
-
-canvas_actor = cube()
-canvas_actor.GetProperty().BackfaceCullingOff()
-scene.add(canvas_actor)
-mapper = canvas_actor.GetMapper()
-
-texture = vtk.vtkTexture()
-texture.CubeMapOn()
+# TODO: Add https://stackoverflow.com/questions/49783981/using-3-channel-image-in-numpy-as-texture-image-in-vtk
 file = "sugar.jpg"
-
 imgReader = vtk.vtkJPEGReader()
 imgReader.SetFileName(file)
 
+texture = vtk.vtkTexture()
+
+scene = window.Scene()
+
+selected_actor = 'sphere'
+
+if selected_actor == 'cube':
+    canvas_actor = cube()
+
+if selected_actor == 'sphere':
+    # Generate an sphere polydata
+    sphere = vtk.vtkSphereSource()
+    sphere.SetThetaResolution(32)
+    sphere.SetPhiResolution(32)
+
+    norms = vtk.vtkPolyDataNormals()
+    norms.SetInputConnection(sphere.GetOutputPort())
+
+    mapper = vtk.vtkOpenGLPolyDataMapper()
+    mapper.SetInputConnection(norms.GetOutputPort())
+
+    canvas_actor = vtk.vtkActor()
+    canvas_actor.SetMapper(mapper)
+
+texture.CubeMapOn()
+#texture.InterpolateOn()
+#texture.RepeatOff()
+#texture.EdgeClampOn()
+#texture.MipmapOn()
 for i in range(6):
     flip = vtk.vtkImageFlip()
     flip.SetInputConnection(imgReader.GetOutputPort())
@@ -26,6 +45,7 @@ for i in range(6):
     texture.SetInputConnection(i, flip.GetOutputPort())
 
 canvas_actor.SetTexture(texture)
+mapper = canvas_actor.GetMapper()
 
 # // Add new code in default VTK vertex shader
 mapper.AddShaderReplacement(
@@ -46,6 +66,7 @@ mapper.AddShaderReplacement(
     """
     //VTK::PositionVC::Impl  // we still want the default
     vec3 camPos = -MCVCMatrix[3].xyz * mat3(MCVCMatrix);
+    //TexCoords.xyz = reflect(vertexMC.xyz - camPos, normalize(normalMC));
     //TexCoords.xyz = normalMC;
     TexCoords.xyz = vertexMC.xyz;
     """,
@@ -58,11 +79,14 @@ mapper.SetFragmentShaderCode(
     //VTK::Output::Dec  // always have this line in your FS
     in vec3 TexCoords;
     uniform samplerCube texture_0;
-    
+
     void main() {
         gl_FragData[0] = texture(texture_0, TexCoords);
     }
     """
 )
+
+scene.add(canvas_actor)
+#scene.add(actor.axes())
 
 window.show(scene)
