4297ab3c68c212bb7168609f5b99482a05b0f2d5
Serge Koudoro <skab12@gmail.com>
2019-11-27 16:28:04 +0100

    improve tests

--- fury/tests/test_io.py
+++ fury/tests/test_io.py
@@ -99,9 +99,12 @@ def test_save_load_image():
 
     npt.assert_raises(IOError, load_image, "test.vtk")
     npt.assert_raises(IOError, load_image, "test.vtk", use_pillow=False)
-    npt.assert_raises(TypeError, save_image, np.random.randint(0, 255,
-                                                               size=(50, 3)),
+    npt.assert_raises(IOError, save_image,
+                      np.random.randint(0, 255, size=(50, 3)),
                       "test.vtk")
+    npt.assert_raises(IOError, save_image,
+                      np.random.randint(0, 255, size=(50, 3)),
+                      "test.vtk", use_pillow=False)
     npt.assert_raises(IOError, save_image,
                       np.random.randint(0, 255, size=(50, 3, 1, 1)),
                       "test.png")
