dd8890801255b4f832cf2435fbce417d540c36bb
Serge Koudoro <skab12@gmail.com>
2019-12-01 17:56:57 +0100

    beginning of sphere integration

--- fury/primitive.py
+++ fury/primitive.py
@@ -1,4 +1,16 @@
+from os.path import join as pjoin
 import numpy as np
+from fury.data import DATA_DIR
+
+
+SPHERE_FILES = {
+    'symmetric362': pjoin(DATA_DIR, 'evenly_distributed_sphere_362.npz'),
+    'symmetric642': pjoin(DATA_DIR, 'evenly_distributed_sphere_642.npz'),
+    'symmetric724': pjoin(DATA_DIR, 'evenly_distributed_sphere_724.npz'),
+    'repulsion724': pjoin(DATA_DIR, 'repulsion724.npz'),
+    'repulsion100': pjoin(DATA_DIR, 'repulsion100.npz'),
+    'repulsion200': pjoin(DATA_DIR, 'repulsion200.npz')
+}
 
 
 def prim_square():
@@ -9,7 +21,7 @@ def prim_square():
     vertices: ndarray
         4 vertices coords that composed our square
     triangles: ndarray
-        2 vertices that composed our square
+        2 triangles that composed our square
 
     """
     vertices = np.array([[-.5, -.5, 0.0],
@@ -29,7 +41,7 @@ def prim_box():
     vertices: ndarray
         8 vertices coords that composed our box
     triangles: ndarray
-        12 vertices that composed our box
+        12 triangles that composed our box
 
     """
     vertices = np.array([[-.5, -.5, -.5],
@@ -53,3 +65,43 @@ def prim_box():
                           [1, 5, 7],
                           [1, 7, 3]], dtype='i8')
     return vertices, triangles
+
+
+def prim_sphere(name='symmetric362'):
+    """Provide vertices and triangles of the spheres.
+
+    Parameters
+    ----------
+    name : str
+        which sphere - one of:
+        * 'symmetric362'
+        * 'symmetric642'
+        * 'symmetric724'
+        * 'repulsion724'
+        * 'repulsion100'
+        * 'repulsion200'
+
+    Returns
+    -------
+    vertices: ndarray
+        vertices coords that composed our sphere
+    triangles: ndarray
+        triangles that composed our sphere
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from fury.primitive import prim_sphere
+    >>> verts, faces = prim_sphere('symmetric362')
+    >>> verts.shape == (362, 3)
+    True
+    >>> faces.shape == (720, 3)
+    True
+
+    """
+    fname = SPHERE_FILES.get(name)
+    if fname is None:
+        raise ValueError('No sphere called "%s"' % name)
+    res = np.load(fname)
+
+    return res['vertices'], res['faces']
