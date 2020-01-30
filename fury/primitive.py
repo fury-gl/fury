fix scaling issue

fury/primitive.py
@@ -124,20 +124,15 @@ def repeat_primitive(vertices, faces, centers, directions=(1, 0, 0),
     # duplicated vertices if needed
     if not have_tiled_verts:
         vertices = np.tile(vertices, (centers.shape[0], 1))
-
+    big_vertices = vertices
     # Get unit shape
     unit_verts_size = vertices.shape[0] // centers.shape[0]
     unit_triangles_size = faces.shape[0]
 
-    big_centers = np.repeat(centers, unit_verts_size, axis=0)
-
-    # apply centers position
-    big_vertices = vertices + big_centers
     # scale them
     if isinstance(scale, (list, tuple, np.ndarray)):
         scale = np.repeat(scale, unit_verts_size, axis=0)
         scale = scale.reshape((big_vertices.shape[0], 1))
-    print(scale)
     big_vertices *= scale
 
     # update triangles
@@ -168,7 +163,6 @@ def repeat_primitive(vertices, faces, centers, directions=(1, 0, 0),
 
     # update orientations
     directions = normalize_input(directions, 'directions')
-    big_vertices -= big_centers
     for pts, dirs in enumerate(directions):
         ai, aj, ak = Rotation.from_rotvec(np.pi/2 * dirs).as_euler('zyx')
         rotation_matrix = euler_matrix(ai, aj, ak)
@@ -176,9 +170,12 @@ def repeat_primitive(vertices, faces, centers, directions=(1, 0, 0),
             np.dot(rotation_matrix[:3, :3],
                    big_vertices[pts * unit_verts_size:
                                 (pts+1) * unit_verts_size].T).T
+
+    # apply centers position
+    big_centers = np.repeat(centers, unit_verts_size, axis=0)
     big_vertices += big_centers
 
-    return big_vertices, big_triangles, big_colors
+    return big_vertices, big_triangles, big_colors, big_centers
 
 
 def prim_square():