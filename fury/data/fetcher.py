8d4fa857f942c6752d625e8bbd9bf3125243b411
lenixlobo <lenixlobo@gmail.com>
2020-02-07 23:09:45 +0530

    updated fetcher.py
    replaced md5 with sha

--- fury/data/fetcher.py
+++ fury/data/fetcher.py
@@ -83,9 +83,16 @@ def _get_file_sha(filename):
 			sha256_data.update(chunk)
 	return sha256_data.hexdigest()
 
-def check_md5(filename, stored_md5=None):
-    """Compute the md5 of filename.
 def check_sha(filename,stored_sha256=None):
+	if stored_sha256 is not None:
+		computed_sha256 = _get_file_sha(filename).upper()
+		if stored_sha256 != computed_sha256:
+			msg = """The downloaded file, %s, does not have the expected sha
+			checksum of "%s". Instead, the sha checksum was: "%s". This could mean that
+			something is wrong with the file or that the upstream file has been updated.
+			You can try downloading the file again or updating to the newest version of
+			Fury.""" % (filename, stored_sha256,computed_sha256)
+			raise FetcherError(msg)
 
 
 
