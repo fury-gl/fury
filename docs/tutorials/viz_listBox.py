ff6d500ca0e1e206a3458bea1bd134e52535700e
Saransh <b18191@students.iitmandi.ac.in>
2020-02-21 04:13:21 +0530

    reformat

--- docs/tutorials/viz_listBox.py
+++ docs/tutorials/viz_listBox.py
@@ -60,7 +60,7 @@ listbox.on_change = display_element
 # manager.
 
 current_size = (800, 800)
-show_manager = window.ShowManager(size=current_size, 
+show_manager = window.ShowManager(size=current_size,
                                   title="DIPY UI ListBox_Example")
 
 show_manager.scene.add(listbox)
