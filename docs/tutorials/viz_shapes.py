6116aa72358f96bee7dade09aa3b06f18d995673
Saransh <b18191@students.iitmandi.ac.in>
2020-02-20 23:51:55 +0530

    added viz_shapes.py (tutorial for drawing shapes)

---
+++ docs/tutorials/viz_shapes.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+===============
+User Interfaces
+===============
+
+This example shows how to use the UI API. We will demonstrate how to draw
+some geometric shapes from DIPY UI elements.
+
+First, a bunch of imports.
+"""
+from fury.data import read_viz_icons, fetch_viz_icons
+
+from fury import ui, window
+
+###############################################################################
+# Shapes
+# ======
+#
+# Let's draw some simple shapes. First, a rectangle.
+
+rect = ui.Rectangle2D(size=(100, 100), position=(400, 400), color=(1, 0, 1))
+
+###############################################################################
+# Then we can draw a solid circle, or disk.
+
+disk = ui.Disk2D(outer_radius=50, center=(400, 200), color=(1, 1, 0))
+
+###############################################################################
+# Add an inner radius to make a ring.
+
+ring = ui.Disk2D(outer_radius=50, inner_radius=45, center=(500, 600),
+                 color=(0, 1, 1))
+
+
+###############################################################################
+# Show Manager
+# ==================================
+#
+# Now that all the elements have been initialised, we add them to the show
+# manager.
+
+current_size = (800, 800)
+show_manager = window.ShowManager(size=current_size, title="DIPY UI Example")
+
+show_manager.scene.add(rect)
+show_manager.scene.add(disk)
+show_manager.scene.add(ring)
+
+show_manager.start()
