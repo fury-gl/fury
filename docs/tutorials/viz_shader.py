a70b610a6fa136a648aab8c8d2d88c66ecdff614
lenixlobo <lenixlobo@gmail.com>
2020-02-22 16:26:29 +0530

    Update viz_shader.py

--- docs/tutorials/viz_shader.py
+++ docs/tutorials/viz_shader.py
@@ -1,12 +1,12 @@
-#Create a basic shader
+# Create a basic shader
 
 import numpy as np
 from fury import window, actor, ui, io, utils
 import vtk
 
-#import the 3D model of choice
-#to import a model, use io.load_polydata()
-#currently supported formats include OBJ, VKT, FIB, PLY, STL and XML
+# import the 3D model of choice
+# to import a model, use io.load_polydata()
+# currently supported formats include OBJ, VKT, FIB, PLY, STL and XML
 
 utah = io.load_polydata('models/utah.obj')
 utah = utils.get_polymapper_from_polydata(utah)
@@ -14,10 +14,10 @@ utah = utils.get_actor_from_polymapper(utah)
 mapper = utah.GetMapper()
 
 
-#Replace fragment shader using vtkShader.Vertex
+# Replace fragment shader using vtkShader.Vertex
 mapper.AddShaderReplacement(
      vtk.vtkShader.Vertex,
-    "//VTK::Output::Dec", #declaration any uniforms/varying needed for normals
+    "//VTK::Output::Dec", # declaration any uniforms/varying needed for normals
     True,
     """
     //VTK::Output::Dec
@@ -29,7 +29,7 @@ mapper.AddShaderReplacement(
 
 mapper.AddShaderReplacement(
     vtk.vtkShader.Vertex,
-    "//VTK::Output::Impl",#implementation for normals
+    "//VTK::Output::Impl",# implementation for normals
     True,
     """
     //VTK::Output::Impl
@@ -60,17 +60,17 @@ mapper.AddShaderReplacement(
     vec2 iResolution = vec2(1024,720);
     
     vec2 uv = myVertexVC.xy/iResolution;
-    vec3 col = 0.5 + 0.5 * cos((time/30) + uv.xyx + vec3(0,2,4));
+    vec3 col = 0.5 + 0.5 * cos((time/30) + uv.xyx + vec3(0, 2, 4));
     fragOutput0 = vec4(col, 1.0);
     """,
     False
 )
 
 
-#create a scene to be rendered in 
+# create a scene to be rendered in 
 scene = window.Scene()
 
-showm = window.ShowManager(scene,size=(1024,720),reset_camera=False)
+showm = window.ShowManager(scene, size=(1024, 720), reset_camera=False)
 
 global timer
 timer = 0
@@ -80,10 +80,11 @@ def timer_callback(obj, event):
     global timer
     timer += 1.0
     showm.render()
-    # print(timer)
-    # scene.azimuth(10)
+    #  print(timer)
+    #  scene.azimuth(10)
 
-# add a decorator to your custom callback
+
+#  add a decorator to your custom callback
 @window.vtk.calldata_type(window.vtk.VTK_OBJECT)
 def vtk_shader_callback(caller, event, calldata=None):
     program = calldata
@@ -94,25 +95,20 @@ def vtk_shader_callback(caller, event, calldata=None):
         except ValueError:
             pass
 
-
-#Add a textblock to add text to 
+# Add a textblock to add text to 
 tb = ui.TextBlock2D()
 tb.message = "Hello Shaders"
 
-#change the property of the actor
+# change the property of the actor
 utah.GetProperty().SetOpacity(0.5)
 
-#Invoke callbacks to any VTK object
-mapper.AddObserver(window.vtk.vtkCommand.UpdateShaderEvent,vtk_shader_callback)
-
+# Invoke callbacks to any VTK object
+mapper.AddObserver(window.vtk.vtkCommand.UpdateShaderEvent, vtk_shader_callback)
 
 showm.initialize()
-showm.add_timer_callback(True,30, timer_callback)
-#add created actors to the scene
+showm.add_timer_callback(True, 30, timer_callback)
+# add created actors to the scene
 scene.add(utah)
 scene.add(tb)
 
 showm.start()
-
-
-
