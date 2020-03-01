1a58bbe8961b9d926826afc7a06e82e5f1d435ac
ibrahimAnis <anisibrahim21@gmail.com>
2020-03-01 10:40:20 +0530

    new changes

---
+++ examples/textWrapperTest.py
@@ -0,0 +1,6 @@
+import TextWrapper;
+from TextWrapper import *;
+
+#w=new TextWrapper();
+print(textwrapper.dedent("Hello there.\n  This is indented."))
+#print(dedent("Hello there.\n  This is indented."))
