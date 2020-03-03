7be1ffb918e98ad8b7570390572bc002b45dec78
Nibba2018 <sohambiswas41@gmail.com>
2020-03-04 00:31:56 +0530

    fixed pep8

--- fury/tests/test_ui.py
+++ fury/tests/test_ui.py
@@ -736,8 +736,8 @@ of List Box 2D"], [1], values]
     show_manager.play_events_from_file(recording_filename)
 
     # Check if the right values were selected.
-    expected = [[1], [2], [2], ["A Very Very Long Item To Test \
-Text Overflow of List Box 2D"], [1], ["A Very Very Long Item To Test \
+    expected = [[1], [2], [2], ["A Very Very Long Item \
+To Test Text Overflow of List Box 2D"], [1], ["A Very Very Long Item To Test \
 Text Overflow of List Box 2D"]]
     npt.assert_equal(len(selected_values), len(expected))
     assert_arrays_equal(selected_values, expected)
