9726c9cba0043fb16d5ef7cd8bb9f8b900b9184f
ibrahimAnis <31585047+ibrahimAnis@users.noreply.github.com>
2020-03-03 11:14:22 +0530

    added test file

--- fury/tests/test_list_box_text_overflow.py
+++ fury/tests/test_list_box_text_overflow.py
@@ -3,20 +3,19 @@ import fury.window as window
 
 
 def display_element():
-    value=values[values.index(listbox.selected[0])]
-    print(value);
-    textbox.message=value;
+    value = values[values.index(listbox.selected[0])]
+    textbox.message = value
 
 values = ['abcdefghijklmnopqrstuvwxyz', 'ahdfjkhkashdfjkhdskfhiweuroqweqr', 'akdiuhdaiufhasdfusdffsdfusdyf', 'Soham']
 listbox = ui.ListBox2D(values=values, size=(300, 300))
-listbox.on_change=display_element;
-panel = ui.Panel2D(size=(300,100),position=(550,250) ,color=(0,0,0), align="center")
+listbox.on_change=display_element
+panel = ui.Panel2D(size=(300,100), position=(550,250), color=(0,0,0), align="center")
 panel.center = (300,200)
-textbox=ui.TextBlock2D();
+textbox = ui.TextBlock2D()
 panel.add_element(textbox, (150,50))
 sm=window.ShowManager(size=(800,800))
-sm.scene.add(panel);
-sm.scene.add(listbox);
+sm.scene.add(panel)
+sm.scene.add(listbox)
 sm.start()
 
 
