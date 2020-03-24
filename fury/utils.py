3498ef5d9217d26164f5a520ac826953c366b742
Naman Bansal <35342183+namanb009@users.noreply.github.com>
2020-03-24 15:26:54 +0530

    Pep8 fixes

--- fury/utils.py
+++ fury/utils.py
@@ -967,7 +967,7 @@ def check_order(vert, triarr):
 
 def hex_color(color):
     if color[0] == "#":
-        color[1:]
+        color = color[1:]
 
     r = int("0x" + color[0: 2], 0) / 256
     g = int("0x" + color[2: 4], 0) / 256
