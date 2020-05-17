b7a3e2d7decd7659f8ac913a37e55ff3ce9ebf4a
mlraglin <43075495+mlraglin@users.noreply.github.com>
2020-05-17 13:04:19 -0400

    Removed unneccessary test funtions

--- fury/tests/test_primitive.py
+++ fury/tests/test_primitive.py
@@ -123,12 +123,3 @@ def test_repeat_primitive_function():
 
     # npt.assert_equal(big_verts.shape[0],  verts.shape[0] * centers.shape[0])
 
-def test_vertices_primitives_octagonalprism():
-    #Tests default vertices of octagonal prism
-    vertices, _ = fp.prim_octagonalprism()
-    shape = (16, 3)
-    phi = (1 + math.sqrt(5)) / 2.0
-    npt.assert_equal(vertices.shape, shape)
-    npt.assert_equal(np.mean(vertices), 0)
-    npt.assert_equal(vertices.min(), -phi)
-    npt.assert_equal(vertices.max(),phi)
