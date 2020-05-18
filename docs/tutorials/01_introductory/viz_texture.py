31241b12b92c8604d5f632ed32ba26550d048c4e
mlraglin <43075495+mlraglin@users.noreply.github.com>
2020-05-18 12:55:12 -0400

    Fixing syntax errors

--- docs/tutorials/01_introductory/viz_texture.py
+++ docs/tutorials/01_introductory/viz_texture.py
@@ -15,23 +15,23 @@ import itertools
 scene = window.Scene()
 
 ##############################################################################
-# Load an image (png, bmp, jpeg or jpg) using``io.prim_sphere``.
+# Load an image (png, bmp, jpeg or jpg) using ``io.prim_sphere``.
 
 filename = r"C:\Users\melin\Downloads\1_earth_8k.jpg"
 image = io.load_image(filename)
 
 ##############################################################################
-# Next, use ``actor.texture_on_sphere`` to add a sphere with the texture from 
-# your loaded image to the already existing scene. 
-# To add a texture to your scene as visualized on a plane, use 
+# Next, use ``actor.texture_on_sphere`` to add a sphere with the texture from
+# your loaded image to the already existing scene.
+# To add a texture to your scene as visualized on a plane, use
 # ``actor.texture`` instead.
 
 scene.add(actor.texture_on_sphere(image))
 
 ##############################################################################
-# Lastly, record the scene. If you want to manipulate your new sphere with a 
+# Lastly, record the scene. If you want to manipulate your new sphere with a
 # texture, use ``window.show`` as commented out.
 
-#window.show(scene, size=(600, 600), reset_camera=False)
+# window.show(scene, size=(600, 600), reset_camera=False)
 window.record(scene, size=(900, 768), out_path="viz_texture.png")
 
