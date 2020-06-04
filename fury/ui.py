0fcefe53d6bd1667e0bfa41ac492c3f92c98ff63
Nibba2018 <sohambiswas41@gmail.com>
2020-06-04 21:50:40 +0530

    Implement position2

--- fury/ui.py
+++ fury/ui.py
@@ -1082,11 +1082,10 @@ class TextBlock2D(UI):
         Adds text shadow.
     """
 
-    def __init__(self, text="Text Block", font_size=0, font_family='Arial',
+    def __init__(self, text="Text Block", font_size=None, font_family='Arial',
                  justification='left', vertical_justification="bottom",
-                 bold=False, italic=False, shadow=False, border=False,
-                 color=(1, 1, 1), bg_color=None, bbox_size=(100, 100),
-                 position=(0, 0)):
+                 bold=False, italic=False, shadow=False, position2=None,
+                 color=(1, 1, 1), bg_color=None, position=(0, 0)):
         """
         Parameters
         ----------
@@ -1117,12 +1116,15 @@ class TextBlock2D(UI):
         bbox_size : (int, int)
             width & height of bounding box
         """
-        self.bbox_size = bbox_size
-        self.border = border
         super(TextBlock2D, self).__init__(position=position)
         self.color = color
         self.background_color = bg_color
-        self.font_size = font_size
+        if font_size is not None:
+            self.font_size = font_size
+        if position2 is not None:
+            self.position2 = position2
+        if font_size is None and position2 is None:
+            self.font_size = 18
         self.font_family = font_family
         self.justification = justification
         self.bold = bold
@@ -1133,32 +1135,6 @@ class TextBlock2D(UI):
 
     def _setup(self):
         self.actor = vtk.vtkTextActor()
-
-        text_representation = vtk.vtkTextRepresentation()
-        # Set Coordinate systems for:
-        # PositionCoordinate = bottom-left corner.
-        # PositionCoordinate2 = top-right corner.
-        text_representation.GetPositionCoordinate().SetCoordinateSystemToDisplay()
-        text_representation.GetPosition2Coordinate().SetCoordinateSystemToDisplay()
-
-        # Set coordinates.
-        text_representation.GetPositionCoordinate().SetValue(*self.position)
-        text_representation.GetPosition2Coordinate().SetValue(*self.bbox_size)
-
-        if self.border:
-            text_representation.SetShowBorderToOn()
-        else:
-            text_representation.SetShowBorderToOff()
-
-        text_widget = vtk.vtkTextWidget()
-        text_widget.SetRepresentation(text_representation)
-        text_widget.SetTextActor(self.actor)
-        text_widget.SelectableOff()
-        text_widget.ResizableOff()
-        text_widget.On()
-
-        self.actor = text_widget.GetTextActor()
-
         self._background = None  # For VTK < 7
         self.handle_events(self.actor)
 
@@ -1463,6 +1439,30 @@ class TextBlock2D(UI):
         self.actor.SetPosition(*position)
         if self._background is not None:
             self._background.SetPosition(*self.actor.GetPosition())
+
+    @property
+    def position2(self):
+        """
+        Gets top-right corner position.
+
+        Returns
+        -------
+        (float, float, float)
+        """
+        return self.actor.GetPosition2()
+
+    @position2.setter
+    def _set_position2(self, position2=(100, 100, 0)):
+        """ Set text actor position2.
+
+        Parameters
+        ----------
+        position : (float, float)
+            The new position. (x, y) in pixels.
+        """
+        self.actor.SetPosition2(*position2)
+        if self._background is not None:
+            self._background.SetPosition2(*self.actor.GetPosition2())
 
     def _get_size(self):
         if self._background is not None:
