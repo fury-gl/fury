d71397cd5d3ea0f6fe8230ad00e5ee07f9af57ff
Nibba2018 <sohambiswas41@gmail.com>
2020-06-05 21:09:34 +0530

    Simplified ComboBox tutorial

--- docs/tutorials/02_ui/viz_combobox.py
+++ docs/tutorials/02_ui/viz_combobox.py
@@ -5,56 +5,24 @@ ComboBox
 ========
 
 This example shows how to use the UI API. We will demonstrate how to
-create ComboBoxes for selecting actors and thier respective colors.
+create ComboBoxes for selecting colors for a label.
 
 First, some imports.
 """
-from fury import ui, window, actor
-import numpy as np
-
-########################################################################
-# Setting up actors.
-# ===============================
-#
-# Adding all actors to the scene.
-#
-# Firstly, we define some common actor properties.
-
-centers = np.random.rand(1, 3)
-dirs = np.random.rand(1, 3)
-heights = np.random.rand(5)
-
-chosen_actor = None
-chosen_color = (1, 1, 1)
-
-########################################################################
-# Now we create a cube.
-
-cube = actor.cube(centers, dirs, (1, 1, 1), heights=heights)
-cube.SetVisibility(False)
+from fury import ui, window
 
 #########################################################################
-# Next we create a sphere.
+# First, we create a label.
 
-sphere = actor.sphere(centers, window.colors.coral)
-sphere.SetVisibility(False)
-
-########################################################################
-# Next a cylinder
-
-cylinder = actor.cylinder(centers, dirs, (1, 1, 1), heights=heights)
-cylinder.SetVisibility(False)
+label = ui.TextBlock2D(
+    position=(200, 300), font_size=40, color=(1, 0.5, 0),
+    justification="center", vertical_justification="top",
+    text="FURY rocks!!!"
+)
 
 ########################################################################
-# Now we create two dictionaries, one to map Actor names with their
-# respective actor instances and the other to store the RGB values of
-# 7 different colors.
-
-actors = {
-    "Cube": cube,
-    "Sphere": sphere,
-    "Cylinder": cylinder
-}
+# Now we create a dictionary to store colors as its key and their
+# RGB values as its value.
 
 colors = {
     "Violet": (0.6, 0, 0.8),
@@ -63,65 +31,41 @@ colors = {
     "Green": (0, 1, 0),
     "Yellow": (1, 1, 0),
     "Orange": (1, 0.5, 0),
-    "Red": (255, 0, 0)
+    "Red": (1, 0, 0)
 }
 
 ########################################################################
 # ComboBox
 # ===================
 #
-# Now we create two ComboBox UI components.
-# One for selecting the actors and the other for selecting its color.
+# Now we create a ComboBox UI component for selecting colors.
 
-actor_combobox = ui.ComboBox2D(items=list(actors.keys()),
-                               position=(50, 0), size=(300, 200))
 color_combobox = ui.ComboBox2D(items=list(colors.keys()),
-                               position=(450, 0), size=(300, 200))
+                               placeholder="Choose Text Color",
+                               position=(75, 50), size=(250, 150))
 
 ########################################################################
 # Callbacks
 # ==================================
 #
-# Now we create callbacks for setting the chosen actor and color.
-
-
-def change_actor(combobox):
-
-    global chosen_actor, chosen_color
-
-    # Identify the option using `combobox.selected_text`
-    chosen_actor = actors[combobox.selected_text]
-
-    for actr in combobox.items:
-        if actr != combobox.selected_text:
-            actors[actr].SetVisibility(False)
-
-    chosen_actor.SetVisibility(True)
-    chosen_actor.GetProperty().SetColor(*chosen_color)
+# Now we create a callback for setting the chosen color.
 
 
 def change_color(combobox):
-
-    global chosen_actor, chosen_color
-
-    if chosen_actor is not None:
-        chosen_color = colors[combobox.selected_text]
-        chosen_actor.GetProperty().SetColor(*chosen_color)
+    label.color = colors[combobox.selected_text]
 
 
-actor_combobox.on_change = change_actor
 color_combobox.on_change = change_color
 
 ###############################################################################
 # Show Manager
 # ==================================
 #
-# Now that all the elements have been initialised, we add them to the show
-# manager.
+# Now we add label and combobox to the scene.
 
-current_size = (800, 800)
+current_size = (400, 400)
 showm = window.ShowManager(size=current_size, title="ComboBox UI Example")
-showm.scene.add(cube, sphere, cylinder, actor_combobox, color_combobox)
+showm.scene.add(label, color_combobox)
 
 ###############################################################################
 # Set camera for better visualization
