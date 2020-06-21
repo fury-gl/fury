1543b8462a6f65b713ab6fc167afc1f3a813dd17
Nasim Anousheh <nasimanousheh@gmail.com>
2020-06-21 10:43:54 -0400

    create boundary condition

--- docs/examples/collision-particles.py
+++ docs/examples/collision-particles.py
@@ -14,25 +14,24 @@ from numba import numpy_support
 from vtk.util import numpy_support
 import itertools
 
-num_particles = 10
-box_lx = box_ly = 10
-box_lz =10
-
+num_particles = 20
+box_lx = 100
+box_ly = 100
+box_lz = 100
 global force, velocity, xyz
 force = np.random.rand(num_particles, 3)
-# force = np.ones((10, 1))
 velocity = np.random.rand(num_particles, 3)
-xyz = 10 * np.random.rand(num_particles, 3)
+xyz =  np.random.rand(num_particles, 3)
 colors = np.random.rand(num_particles, 3)
-radii = np.random.rand(num_particles)
+radii = 10 * np.random.rand(num_particles)
 
 scene = window.Scene()
-box_centers = np.array([[0, 0, 0.]])
+box_centers = np.array([[box_lx * 0.5, box_ly * 0.5, box_lz * 0.5]])
 box_directions = np.array([[0, 1, 0]])
 box_colors = np.array([[1, 0, 0, 0.2]])
 box_actor = actor.box(box_centers, box_directions, box_colors,
-                    size=(box_lx, box_ly, box_lz),
-                    heights=2, vertices=None, faces=None)
+                      size=(box_lx, box_ly, box_lz),
+                      heights=1, vertices=None, faces=None)
 box_actor.GetProperty().SetRepresentationToWireframe()
 box_actor.GetProperty().SetLineWidth(10)
 
@@ -75,14 +74,21 @@ no_vertices_per_sphere = len(all_vertices)/num_particles
 dt = 0.5
 
 steps = 100
-
-
 def timer_callback(_obj, _event):
     global force, velocity, xyz
     cnt = next(counter)
     tb.message = "Let's count up to 100 and exit :" + str(cnt)
     velocity = velocity + force * (0.5 * dt)
     xyz = xyz + velocity * dt
+    xyz[:, 0] = np.where(xyz[:, 0] >= (box_lx-radii),  xyz[:, 0] - (box_lx - radii), xyz[:, 0])
+    xyz[:, 0] = np.where(xyz[:, 0] <= radii,  xyz[:, 0] + radii, xyz[:, 0])
+
+    xyz[:, 1] = np.where(xyz[:, 1] >= (box_ly-radii),  xyz[:, 1] - (box_ly - radii), xyz[:, 1])
+    xyz[:, 1] = np.where(xyz[:, 1] <= radii,  xyz[:, 1] + radii, xyz[:, 1])
+
+    xyz[:, 2] = np.where(xyz[:, 2] >= (box_lz-radii),  xyz[:, 2] - (box_lz - radii), xyz[:, 2])
+    xyz[:, 2] = np.where(xyz[:, 2] <= radii,  xyz[:, 2] + radii, xyz[:, 2])
+
     all_vertices[:] = initial_vertices + \
         np.repeat(xyz, no_vertices_per_sphere, axis=0)
     set_vertices(sphere_actor, all_vertices)
@@ -93,5 +99,5 @@ def timer_callback(_obj, _event):
 
 
 scene.add(tb)
-showm.add_timer_callback(True, 200, timer_callback)
+showm.add_timer_callback(True, 100, timer_callback)
 showm.start()
