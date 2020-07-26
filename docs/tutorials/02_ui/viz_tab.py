b1cdbd439a54a7d0c9582f0d298dd13c92f0fdf9
Nibba2018 <sohambiswas41@gmail.com>
2020-07-26 18:32:54 +0530

    Added second tab

--- docs/tutorials/02_ui/viz_tab.py
+++ docs/tutorials/02_ui/viz_tab.py
@@ -7,7 +7,7 @@ This example shows how to use the Tab UI. We will demonstrate how to
 create Tabs for:
 
 1. Slider controls for a Cube
-2. Checkboxes for Cube and Sphere
+2. Checkboxes for Cylinder and Sphere
 3. Color combobox for Fury.
 
 First, some imports.
@@ -73,6 +73,45 @@ tab_ui.tabs[0].title = "Sliders"
 tab_ui.tabs[0].add_element(ring_slider, (0.3, 0.3))
 tab_ui.tabs[0].add_element(line_slider_x, (0.0, 0.0))
 tab_ui.tabs[0].add_element(line_slider_y, (0.0, 0.1))
+
+###############################################################################
+# CheckBoxes For Cylinder and Sphere
+# ==============================
+#
+# Now we prepare content for second tab.
+
+cylinder = actor.cylinder(centers=np.array([[0, 0, 0]]),
+                          directions=np.array([[0, 1, 0]]),
+                          colors=np.array([[0, 1, 1]]))
+
+sphere = actor.sphere(centers=np.array([[0, 0, 0]]),
+                      colors=(1, 1, 0))
+
+figure_dict = {'cylinder': cylinder, 'sphere': sphere}
+checkbox = ui.Checkbox(labels=["Cylinder", "Sphere"])
+
+# Get difference between two lists.
+def sym_diff(l1, l2):
+    return list(set(l1).symmetric_difference(set(l2)))
+
+# Set Visiblity of the figures
+def set_figure_visiblity(checkboxes):
+    checked = checkboxes.checked_labels
+    unchecked = sym_diff(list(figure_dict), checked)
+
+    for visible in checked:
+        figure_dict[visible].SetVisibility(True)
+
+    for invisible in unchecked:
+        figure_dict[invisible].SetVisibility(False)
+
+check_box.on_change = set_figure_visiblity
+
+###############################################################################
+# After defining content, we define properties for the tab.
+
+tab_ui.tabs[1].title = "Checkbox"
+tab_ui.tabs[1].add_element(checkbox, (0.2, 0.2))
 
 ###############################################################################
 # Next we prepare the scene and render it with the help of show manager.
