dae296072d31f8b42b74e56cfbe4104f9ad599b2
Nibba2018 <sohambiswas41@gmail.com>
2020-08-09 17:58:33 +0530

    rope simulation

--- docs/tutorials/05_physics/viz_pendulum.py
+++ docs/tutorials/05_physics/viz_pendulum.py
@@ -11,7 +11,7 @@ p.setGravity(0, 0, -10, physicsClientId=client)
 ###### Creating ceiling Plane
 ceil_actor = actor.box(centers=np.array([[0, 0, 0]]),
                          directions=[0,0,0],
-                         size=(5, 5, 0.2) ,
+                         scale=(5, 5, 0.2) ,
                          colors=(255, 255, 255))
 ceil_coll = p.createCollisionShape(p.GEOM_BOX,
                                    halfExtents=[2.5, 2.5, 0.1]) # half of the actual size.
@@ -20,81 +20,65 @@ ceil = p.createMultiBody(baseMass=0,
                           basePosition=[0, 0, -0.2],
                           baseOrientation=[ 0, 0, 0, 1 ])
 
-def sync_actor(actor, multibody):
-    pos, orn = p.getBasePositionAndOrientation(multibody)
-    actor.SetPosition(*pos)
-    orn_deg = np.degrees(p.getEulerFromQuaternion(orn))
-    actor.SetOrientation(*orn_deg)
-    # actor.RotateWXYZ(*orn)
-
 ############ Creating String
-sphereRadius = 0.25
-segment_size = (0.1, 0.1, 0.25)
-###### Creating BALL
-# Ball actor
-ball_actor = actor.sphere(centers = [[0, 0, 0]],
-                    colors=np.array([1,0,0]),
-                    radii=0.25)
 
-# Creating a Multibody which will be tracked by pybullet.
-useMaximalCoordinates = False
-# colBoxId = p.createCollisionShapeArray([p.GEOM_BOX, p.GEOM_SPHERE],radii=[sphereRadius+0.03,sphereRadius+0.03], halfExtents=[[sphereRadius,sphereRadius,sphereRadius],[sphereRadius,sphereRadius,sphereRadius]])
-sphere = p.createCollisionShape(p.GEOM_SPHERE,
-                                  radius=sphereRadius)
-colBoxId = p.createCollisionShape(p.GEOM_BOX,
-                                  halfExtents=segment_size)
+# Parameters
+n_links = 30
+dx_link = 0.02
+link_mass = 0.005
+base_mass = 0.1
+radii = 0.005
 
-pendulum_actors = []
+joint_friction = 0.0005
 
-mass = 1
+link_shape = p.createCollisionShape(p.GEOM_CYLINDER,
+                                    radius=radii,
+                                    height=dx_link,
+                                    collisionFramePosition=[0, 0, -dx_link/2])
+
+base_shape = p.createCollisionShape(p.GEOM_BOX,
+                                    halfExtents=[0.01, 0.01, 0.01])
+
 visualShapeId = -1
 
-link_Masses = np.zeros(7)
-link_Masses[:] = 0.5
+link_Masses = np.zeros(n_links)
+link_Masses[:] = link_mass
 
-linkCollisionShapeIndices = np.zeros(7)
-linkCollisionShapeIndices[:] = np.array(colBoxId)
-linkVisualShapeIndices = -1 * np.ones(7)
-linkPositions = np.zeros((7, 3))
-linkPositions[:] = np.array([0, 0, segment_size[2] * 2.0 + 0.01])
-linkOrientations = np.zeros((7, 4))
+linkCollisionShapeIndices = np.zeros(n_links)
+linkCollisionShapeIndices[:] = np.array(link_shape)
+linkVisualShapeIndices = -1 * np.ones(n_links)
+linkPositions = np.zeros((n_links, 3))
+linkPositions[:] = np.array([0, 0, -dx_link])
+linkOrientations = np.zeros((n_links, 4))
 linkOrientations[:] = np.array([0, 0, 0, 1])
-linkInertialFramePositions = np.zeros((7, 3))
-linkInertialFrameOrientations = np.zeros((7, 4))
+linkInertialFramePositions = np.zeros((n_links, 3))
+linkInertialFrameOrientations = np.zeros((n_links, 4))
 linkInertialFrameOrientations[:] = np.array([0, 0, 0, 1])
-indices = np.arange(7)
-jointTypes = np.zeros(7)
+indices = np.arange(n_links)
+jointTypes = np.zeros(n_links)
 jointTypes[:] = np.array(p.JOINT_SPHERICAL)
-axis = np.zeros((7, 3))
-axis[:] = np.array([0, 0, 1])
+axis = np.zeros((n_links, 3))
+axis[:] = np.array([1, 0, 0])
 
-linkDirections = np.zeros((7, 3))
-linkDirections[:] = np.array([1.57, 0, 0])
+linkDirections = np.zeros((n_links, 3))
+linkDirections[:] = np.array([1, 1, 1])
 
-linkSizes = np.zeros((7, 3))
-linkSizes[:] = np.array(segment_size)*2
+link_radii = np.zeros(n_links)
+link_radii[:] = radii
 
-segment_actor = actor.box(centers=linkPositions,
-                          directions=linkDirections,
-                          colors=np.random.rand(7, 3)*255,
-                          scale=linkSizes)
+link_heights = np.zeros(n_links)
+link_heights[:] = dx_link
 
-# print(link_Masses.shape)
-# print(linkCollisionShapeIndices.shape)
-# print(linkVisualShapeIndices.shape)
-# print(linkPositions.shape)
-# print(linkOrientations.shape)
-# print(linkInertialFrameOrientations.shape)
-# print(linkInertialFramePositions.shape)
-# print(indices.shape)
-# print(jointTypes.shape)
-# print(axis.shape)
-# print()
+rope_actor = actor.cylinder(centers=linkPositions,
+                            directions=linkDirections,
+                            colors=np.random.rand(n_links, 3),
+                            radius=radii,
+                            heights=link_heights)
 
-basePosition = [0, 0, 1]
+basePosition = [0, 0, 2]
 baseOrientation = [0, 0, 0, 1]
-pendulum = p.createMultiBody(mass,
-                              sphere,
+rope = p.createMultiBody(base_mass,
+                              base_shape,
                               visualShapeId,
                               basePosition,
                               baseOrientation,
@@ -112,22 +96,41 @@ pendulum = p.createMultiBody(mass,
 
 # remove stiffness in motors, add friction force
 
-friction_vec = [0.0005]*3   # same all axis
+friction_vec = [joint_friction]*3   # same all axis
 control_mode = p.POSITION_CONTROL   # set pos control mode
-for j in range(p.getNumJoints(pendulum)):
-    p.setJointMotorControlMultiDof(pendulum,j,control_mode,
+for j in range(p.getNumJoints(rope)):
+    p.setJointMotorControlMultiDof(rope,j,control_mode,
                                     targetPosition=[0,0,0,1],
                                     targetVelocity=[0,0,0],
                                     positionGain=0,
                                     velocityGain=1,
                                     force=friction_vec)
 
-scene = window.Scene()
-scene.add(actor.axes())
-scene.add(segment_actor)
-scene.add(ball_actor)
-scene.add(ceil_actor)
+# fixed constrain to keep root cube in place
+root_robe_c = p.createConstraint(rope, -1, -1, -1,
+                                 p.JOINT_FIXED, [0, 0, 0],
+                                 [0, 0, 0], [0, 0, 2])
 
+# some traj to inject motion
+amplitude_x = 0.3
+amplitude_y = 0.0
+freq = 0.6
+
+# manually simulate joint damping
+Damping = 0.001
+
+
+base_actor = actor.box(centers=np.array([[0, 0, 0]]),
+                       directions=np.array([[0, 0, 0]]),
+                       scale=(0.02, 0.02, 0.02),
+                       colors=np.array([[1, 0, 0]]))
+
+scene = window.Scene()
+scene.background((1, 1, 1))
+scene.add(actor.axes())
+scene.add(rope_actor)
+scene.add(ceil_actor)
+scene.add(base_actor)
 
 
 # Create show manager.
@@ -146,15 +149,10 @@ def sync_actor(actor, multibody):
     actor.SetPosition(*pos)
     orn_deg = np.degrees(p.getEulerFromQuaternion(orn))
     actor.SetOrientation(*orn_deg)
-    # actor.RotateWXYZ(*orn)
 
 sync_actor(ceil_actor, ceil)
 
-# print(pendulum_actors)
-# for joint in range(p.getNumJoints(pendulum)):
-#     print(p.getJointInfo(pendulum, joint))
-
-vertices = utils.vertices_from_actor(segment_actor)
+vertices = utils.vertices_from_actor(rope_actor)
 num_vertices = vertices.shape[0]
 num_objects = linkPositions.shape[0]
 sec = np.int(num_vertices / num_objects)
@@ -175,16 +173,33 @@ def sync_joints(actor_list, multibody):
         linkPositions[joint] = pos
         linkOrientations[joint] = orn
 
+
+t = 0.0
+freq_sim = 240
 def timer_callback(_obj, _event):
     cnt = next(counter)
+    global t
     showm.render()
+
+    t += 1./freq_sim
+
+    # some trajectory
+    ux = amplitude_x*np.sin(2*np.pi*freq*t)
+    uy = amplitude_y*np.cos(2*np.pi*freq*t)
+
+    # move base arround
+    pivot = [ux, uy, 2]
+    orn = p.getQuaternionFromEuler([0, 0, 0])
+    p.changeConstraint(root_robe_c, pivot, jointChildFrameOrientation=orn, maxForce=500)
+
 
     # Set position and orientation of the ball.
     # sync_actor(ceil_actor, ceil)
-    for i in range(p.getNumJoints(pendulum)):
-        sync_actor(ball_actor, sphere)
-        sync_joints(pendulum_actors, pendulum)
-        utils.update_actor(segment_actor)
+    for i in range(p.getNumJoints(rope)):
+        # sync_actor(ball_actor, sphere)
+        sync_actor(base_actor, rope)
+        sync_joints(rope_actor, rope)
+        utils.update_actor(rope_actor)
 
     # Simulate a step.
     p.stepSimulation()
