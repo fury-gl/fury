b706359d4d287cd1771f4594c76a5934d0b941c1
Nasim Anousheh <nasimanousheh@gmail.com>
2020-09-29 12:07:40 -0400

    Fixed avoid_collision

--- fury/swarm.py
+++ fury/swarm.py
@@ -23,7 +23,7 @@ class GlobalMemory(object):
         self.tm_step = 10
 
         # Initial parameters for particles
-        self.num_particles = 40
+        self.num_particles = 150
         self.height_cones = 1
         self.turnfactor = 1
         self.vel = np.array([0, 0, 0.])
@@ -33,9 +33,9 @@ class GlobalMemory(object):
         self.vertices = None
 
         # Initial parameters for box
-        self.box_lx = 40
-        self.box_ly = 40
-        self.box_lz = 40
+        self.box_lx = 100
+        self.box_ly = 100
+        self.box_lz = 100
         self.box_centers = np.array([[0, 0, 0]])
         self.box_directions = np.array([[0, 1, 0]])
         self.box_colors = np.array([[255, 255, 255]])
@@ -49,11 +49,12 @@ class GlobalMemory(object):
 
         # Initial parameters for attractors
         self.num_leaders = 1
-        self.radii_leaders = 1
+        self.radii_leaders = 2
         self.pos_leaders = np.array([self.box_lx, self.box_ly, self.box_lz]) * (np.random.rand(self.num_leaders, 3) - 0.5) * 0.6
-        self.vel_leaders = np.random.rand(self.num_leaders, 3)
+        self.vel_leaders = np.array([[0, 0, 0]])
+        # self.vel_leaders = np.random.rand(self.num_leaders, 3)
         self.color_leaders = np.random.rand(self.num_leaders, 3)
-
+        # self.directions_leader = self.vel_leaders.copy()
 
 def vec2vec_rotmat(u, v):
     r""" rotation matrix from 2 unit vectors
@@ -197,39 +198,36 @@ def boids_rules(gm, vertices, vcolors):
         separation = np.array([0, 0, 0.])
         alignment = gm.vel[i].copy()
         avoid_collision = np.array([0, 0, 0.])
-        avoid_obstacles = np.array([0, 0, 0.])
         follow_attractor = np.array([0, 0, 0.])
         distance_particle_leader = np.array([0, 0, 0.])
-        separation_test = np.array([0, 0, 0.])
-        num_avoid_obstacles = 0
-        for a in range(gm.num_obstacles):
-            distance_particle_obstacle = np.linalg.norm(gm.pos[i] -
-                                                        gm.pos_obstacles[a])
-            if distance_particle_obstacle <= (gm.radii_obstacles +
-                                              gm.height_cones):
-                diff = gm.pos[i] - gm.pos_obstacles[a]
-                inv_sqr_magnitude = 1 / ((np.linalg.norm(diff) -
-                                         (gm.radii_obstacles +
-                                          gm.height_cones)) ** 2)
-                avoid_obstacles += gm.vel[i] + (0.5 * inv_sqr_magnitude * diff)
-                num_avoid_obstacles += 1
+
+        # for a in range(gm.num_obstacles):
+        #     distance_particle_obstacle = np.linalg.norm(gm.pos[i] -
+        #                                                 gm.pos_obstacles[a])
+        #     if distance_particle_obstacle <= (gm.radii_obstacles +
+        #                                       gm.height_cones):
+        #         diff = gm.pos[i] - gm.pos_obstacles[a]
+        #         inv_sqr_magnitude = 1 / ((np.linalg.norm(diff) -
+        #                                  (gm.radii_obstacles +
+        #                                   gm.height_cones)) ** 2)
+        #         avoid_obstacles += gm.vel[i] + (0.5 * inv_sqr_magnitude * diff)
+        #         num_avoid_obstacles += 1
 
         for k in range(gm.num_leaders):
             distance_particle_leader = np.linalg.norm(gm.pos[i] -
                                                        gm.pos_leaders[k])
-            if distance_particle_leader < 3:
-                separation_test += 3 #gm.pos[i] - gm.pos_leaders[k]
-                continue
+            if distance_particle_leader <= 40:
+                follow_attractor = (gm.pos_leaders[k] - gm.pos[i]) #/np.linalg.norm(gm.pos_leaders[k] - gm.pos[i])
 
-            if distance_particle_leader < 7:
-                follow_attractor += (gm.pos_leaders[k] - gm.pos[i]) #/np.linalg.norm(gm.pos_leaders[k] - gm.pos[i])
-                    # num_avoid_leaders += 1
         for j in range(gm.num_particles):
             if (i == j):
                 continue
-            distance_particle_particle = np.linalg.norm(gm.pos[i] - gm.pos[j])
-            if distance_particle_particle <= gm.height_cones:
-                avoid_collision += gm.pos[i] - gm.pos[j]
+            distance_particle_particle =  np.linalg.norm(gm.pos[i] - gm.pos[j])
+            if distance_particle_particle <= (gm.height_cones * 2):
+                diff = gm.pos[i] - gm.pos[j]
+                inv_sqr_magnitude = 1 / ((np.linalg.norm(diff) - gm.height_cones) ** 2)
+                avoid_collision = avoid_collision + (inv_sqr_magnitude * diff)
+
             if distance_particle_particle <= 7:
                 vcolors[i * sec: i * sec + sec] = \
                     vcolors[j * sec: j * sec + sec]
@@ -238,12 +236,6 @@ def boids_rules(gm, vertices, vcolors):
                 separation += gm.pos[i] - gm.pos[j]
                 neighborCount += 1  # number of neighbors in sphere
 
-        # if num_avoid_leaders > 0:
-        #     follow_attractor = follow_attractor / num_avoid_leaders
-        #     separation_test = separation_test / num_avoid_leaders
-
-        # if num_avoid_obstacles > 0:
-        #     avoid_obstacles = avoid_obstacles / num_avoid_obstacles
         if neighborCount > 0:
             cohesion = cohesion / (neighborCount)
             cohesion = (cohesion - gm.pos[i])
@@ -251,11 +243,18 @@ def boids_rules(gm, vertices, vcolors):
             alignment = alignment - gm.vel[i]
             separation = separation / neighborCount
 
-            # size_attractor = np.linalg.norm(alignment)
-            # print(size_attractor)
-            gm.vel[i] += avoid_collision + separation + alignment + cohesion + avoid_obstacles + follow_attractor + separation_test
+            # if alignment > 20:
+            #     alignment = alignment / np.linalg.norm(alignment) *20
+
+            gm.vel[i] += avoid_collision + separation + alignment + cohesion + follow_attractor
         else:
-            gm.vel[i] += gm.vel[i].copy()
+            gm.vel[i] += gm.vel[i].copy() + avoid_collision + follow_attractor
+
+    # gm.vel[:, 0] = np.where((gm.vel[:, 0] >= gm.vel_leaders[:, 0]), (gm.vel[:, 0] / speed * max_speed), gm.vel[:, 0])
+    # gm.vel[:, 1] = np.where((gm.vel[:, 1] >= gm.vel_leaders[:, 1]), (gm.vel[:, 1] / speed * max_speed), gm.vel[:, 1])
+    # gm.vel[:, 2] = np.where((gm.vel[:, 2] >= gm.vel_leaders[:, 2]), (gm.vel[:, 2] / speed * max_speed), gm.vel[:, 2])
+
+
 
 
 def collision_obstacle_leader_walls(gm):
