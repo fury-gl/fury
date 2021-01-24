bdef5015247842d810d8c42600fb9c31b10e9ef0
SunTzunami <sun.tsunami248@gmail.com>
2021-01-24 23:26:39 +0530

    added a few comments and updated some code

--- docs/examples/2Dwavefunc_animated.py
+++ docs/examples/2Dwavefunc_animated.py
@@ -5,13 +5,13 @@ Animating a time-varying 2D wave function
 
 This is a simple demonstration of how one can visualize
 time-varying 2D wave functions using FURY.
-An example of a 2D time varying wave function-
-z = F(x, y, t) = 0.24*sin(10x)*cos(10x)*cos(t)
+2D time varying wave function used in this animation-
+z = F(x, y, t) = 0.24*sin(10x)*cos(10y)*cos(t)
 
-Other examples-
+Try these in update_coordinates function-
 #Z = (X**2 - Y**2)/(X**2 + Y**2)**0.5*np.cos(t)
 #Z = (X**2 + Y**2 )*np.sin(t)
-#Z = np.sin(X**2)*np.cos(Y**2)*np.sin(t) #flapping wings
+#Z = np.sin(X**2)*np.cos(Y**2)*np.sin(t) look like flapping wings
 #Z = np.sin(X**2-Y**2)*np.sin(t)
 
 """
@@ -26,8 +26,8 @@ import itertools
 ###############################################################################
 # The following function updates and returns the coordinates of the points
 # which are being used to plot the 2d function. Kindly note that only the z
-# coordinate is being modified time as only the z coordinate is a function of
-# time.
+# coordinate is being modified with time as only the z coordinate is a
+# function of time.
 
 def update_coordinates(lower_xbound=-1, upper_xbound=1, lower_ybound=-1,
                 upper_ybound=1, npoints=100, t=0):
@@ -36,6 +36,7 @@ def update_coordinates(lower_xbound=-1, upper_xbound=1, lower_ybound=-1,
     X, Y = np.meshgrid(x, y)
     X = X.reshape(-1)
     Y = Y.reshape(-1)
+    # Z is the function F i.e. F(x, y, t)
     Z = 0.34*np.sin(X*10)*np.cos(10*Y)*np.cos(t)
     xyz = np.array([(a, b, c) for (a, b, c) in zip(X, Y, Z)])
     return xyz
@@ -69,9 +70,9 @@ showm = window.ShowManager(scene,
 #          and upper bounds that'll be used to plot the function.
 #          Alternatively, it can also be thought of as the square root of the
 #          total number of points that'll be used to plot the function.
-#          For higher quality graphing, keep the number high but do note
+#          For higher quality graphing, keep the number high but kindly note
 #          that higher values for npoints significantly slows down the
-#          animation (default = 100)
+#          animation; (default = 100)
 
 time = 0
 dt = 0.12
@@ -92,7 +93,7 @@ xyz = update_coordinates(lower_xbound, upper_xbound, lower_ybound, upper_ybound,
 # colors and radius are used to manipulate the color and radius of the points
 # respectively (default radius = 0.05)
 # Ideally, radius should be kept somewhat low and npoints should be large.
-colors = window.colors.orange
+colors = window.colors.yellow
 radius = 0.05
 
 ###############################################################################
@@ -106,8 +107,10 @@ scene.add(point_actor)
 # Initializing text box to display the function which is being plotted.
 tb = ui.TextBlock2D(bold=True, position=(150, 90))
 tb.message = "z = F(x, y, t) = 0.24*sin(x)*cos(y)*cos(t)"
+scene.add(tb)
 
-
+###############################################################################
+# Initializing showm
 showm.initialize()
 counter = itertools.count()
 
@@ -124,8 +127,8 @@ initial_vertices = vertices.copy() - \
 end = 500
 
 ###############################################################################
-# coordinates are changed everytime timer_callback is called by using the
-# update_coordinates function. The function is rendered here.
+# coordinates to be plotted are changed everytime timer_callback is called by
+# using the update_coordinates function. The 2D function is rendered here.
 def timer_callback(_obj, _event):
     global xyz
     global time
@@ -139,12 +142,16 @@ def timer_callback(_obj, _event):
     scene.reset_clipping_range()
     showm.render()
 
+# to end the animation
     if cnt == end:
         showm.exit()
 
-scene.add(tb)
-showm.add_timer_callback(True, 5, timer_callback)
+###############################################################################
+# Adding the timer_callback to showm.
+showm.add_timer_callback(True, 50, timer_callback)
 
 interactive = True
 if interactive:
     showm.start()
+
+window.record(showm.scene, size=(600, 600), out_path="2Dwave_func.png")
