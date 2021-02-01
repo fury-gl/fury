89ccf586bd7bb19d7cb8e4c8f31f80f64e278d0d
SunTzunami <sun.tsunami248@gmail.com>
2021-02-01 23:21:23 +0530

    updated the code to use surface actor instead of point actor

--- docs/examples/2Dwavefunc_animated.py
+++ docs/examples/2Dwavefunc_animated.py
@@ -8,10 +8,9 @@ time-varying 2D wave functions using FURY.
 2D time varying wave function used in this animation-
 z = F(x, y, t) = 0.24*sin(10x)*cos(10y)*cos(t)
 
-Try these in update_coordinates function-
-#Z = (X**2 - Y**2)/(X**2 + Y**2)**0.5*np.cos(t)
-#Z = (X**2 + Y**2 )*np.sin(t)
-#Z = np.sin(X**2)*np.cos(Y**2)*np.sin(t) look like flapping wings
+Can also try plotting these in update_coordinates function-
+#Z = (X**2 - Y**2)/(X**2 + Y**2)**0.5*np.cos(np.pi*X-rate*incre)
+#Z = np.sin(X**2)*np.cos(Y**2-rate*incre)#*np.sin(rate*incre)
 #Z = np.sin(X**2-Y**2)*np.sin(t)
 
 """
@@ -19,35 +18,46 @@ Try these in update_coordinates function-
 ###############################################################################
 # Importing necessary modules
 
-from fury import window, actor, ui, utils
+from fury import window, actor, ui, utils, colormap
 import numpy as np
 import itertools
+
+###############################################################################
+# The following function is used to scale the Z values(height) between 0-1
+# which will then be mapped in RGB colors according to colormap
+def scale(arr):
+    arr = np.array(arr)
+    maxima = np.max(arr)
+    minima = np.min(arr)
+    return (arr - maxima)/(maxima - minima)
 
 ###############################################################################
 # The following function updates and returns the coordinates of the points
 # which are being used to plot the 2d function. Kindly note that only the z
 # coordinate is being modified with time as only the z coordinate is a
-# function of time.
-
+# function of time
 
 def update_coordinates(lower_xbound=-1, upper_xbound=1, lower_ybound=-1,
-                       upper_ybound=1, npoints=100, t=0):
+                       upper_ybound=1, npoints=100, time=0, incre=0):
     x = np.linspace(lower_xbound, upper_xbound, npoints)
     y = np.linspace(lower_ybound, upper_ybound, npoints)
     X, Y = np.meshgrid(x, y)
     X = X.reshape(-1)
     Y = Y.reshape(-1)
+    # user can manipulate rate to change the speed of the waves
+    rate = 0.03
     # Z is the function F i.e. F(x, y, t)
-    Z = 0.24*np.sin(X*10)*np.cos(10*Y)*np.cos(t)
+    Z = 0.34*np.sin(2*np.pi*(X))*np.cos(2*np.pi*(Y-rate*incre))*np.cos(time)
+    v = scale(Z)
+    colors = colormap.create_colormap(v, name='PuBu') #PuBu, hot
     xyz = np.array([(a, b, c) for (a, b, c) in zip(X, Y, Z)])
-    return xyz
+    return xyz, colors
 
 ###############################################################################
-# Creating a scene object and configuring the camera's position.
-
+# Creating a scene object and configuring the camera's position
 
 scene = window.Scene()
-scene.zoom(9.5)
+scene.zoom(10.5)
 scene.set_camera(position=(60, 40, 30), focal_point=(0.0, 0.0, 0.0),
                  view_up=(0.0, 0.0, 1.0))
 showm = window.ShowManager(scene,
@@ -68,47 +78,36 @@ showm = window.ShowManager(scene,
 #               (default = -1)
 # upper_ybound: Upper bound of the y values in which the function is plotted
 #                (default = 1)
-# npoints: It's basically the number of equidistant values between lower
-#          and upper bounds that'll be used to plot the function.
-#          Alternatively, it can also be thought of as the square root of the
-#          total number of points that'll be used to plot the function.
-#          For higher quality graphing, keep the number high but kindly note
-#          that higher values for npoints significantly slows down the
-#          animation; (default = 100)
+# npoints: For high quality rendering, keep the number high but kindly note
+#          that higher values for npoints slows down the animation
+#          (default = 200)
 
 time = 0
-dt = 0.1
+dt = 0
 lower_xbound = -1
 upper_xbound = 1
 lower_ybound = -1
 upper_ybound = 1
-npoints = 100
+npoints = 200
 
 
 ###############################################################################
 # xyz are the coordinates of the points that'll be used to plot the function
-xyz = update_coordinates(lower_xbound, upper_xbound, lower_ybound,
-                         upper_ybound, npoints, t=time)
-
-
-###############################################################################
-# colors and radius are used to manipulate the color and radius of the points
-# respectively (default radius = 0.05)
-# Ideally, radius should be kept somewhat low and npoints should be large.
-colors = window.colors.yellow
-radius = 0.05
+# colors refers to the colormap that'll be used
+xyz, colors = update_coordinates(lower_xbound, upper_xbound, lower_ybound,
+                         upper_ybound, npoints, time=time)
 
 ###############################################################################
-# Initializing point and axes actors and adding them to the scene.
-point_actor = actor.point(xyz, colors, point_radius=radius)
+# Initializing point and axes actors and adding them to the scene
+point_actor = actor.surface(xyz, colors= colors)
 axes_actor = actor.axes(scale=(1.5, 1.5, 1.5))
 scene.add(axes_actor)
 scene.add(point_actor)
 
 ###############################################################################
-# Initializing text box to print the 2D function which is being rendered.
+# Initializing text box to print the 2D function which is being rendered
 tb = ui.TextBlock2D(bold=True, position=(150, 60))
-tb.message = "z = F(x, y, t) = 0.24*sin(x)*cos(y)*cos(t)"
+tb.message = "z = F(x, y, t) = 0.24*sin(x)*cos(y)"#*cos(t)"
 scene.add(tb)
 
 ###############################################################################
@@ -126,20 +125,20 @@ initial_vertices = vertices.copy() - \
 
 ###############################################################################
 # end is used to decide when to end the animation
-end = 500
+end = 2000
 
 ###############################################################################
 # Coordinates to be plotted are changed everytime timer_callback is called by
-# using the update_coordinates function. The 2D function is rendered here.
-
+# using the update_coordinates function. The 2D function is rendered here
 
 def timer_callback(_obj, _event):
-    global xyz
-    global time
+    global xyz, time, lower_xbound, upper_xbound, lower_ybound, upper_ybound, colors
     time += dt
     cnt = next(counter)
-    xyz = update_coordinates(lower_xbound, upper_xbound, lower_ybound,
-                             upper_ybound, npoints, t=time)
+    #lower_ybound+=dz
+    #upper_ybound+=dz
+    xyz, colors[:] = update_coordinates(lower_xbound, upper_xbound, lower_ybound,
+                             upper_ybound, npoints, time=time, incre=cnt)
     vertices[:] = initial_vertices + \
         np.repeat(xyz, no_vertices_per_point, axis=0)
     utils.update_actor(point_actor)
