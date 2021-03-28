5bf00f5b84d1d19a55e91d6ed9048cc05a109c0a
antrikshmisri <antrikshmisri@gmail.com>
2021-03-28 20:27:09 +0530

    feat: added interactive flag

--- docs/tutorials/02_ui/viz_card.py
+++ docs/tutorials/02_ui/viz_card.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 ===============
-Buttons & Text
+Card
 ===============
 
 This example shows how to use the UI API. We will demonstrate how to create
@@ -9,7 +9,6 @@ panel having multiple cards on them.
 
 First, some imports.
 """
-from numpy.core.fromnumeric import size
 from fury import ui, window
 from fury.data import read_viz_icons, fetch_viz_icons
 
@@ -36,13 +35,15 @@ second_img_path = read_viz_icons(fname="circle-right.png")
 
 # Now we create two cards with these images , with image scale set to 0.5
 
-first_card = ui.Card2D(image_path=first_img_path, title="First Card",
-                       body="First Card Body", image_scale=0.5,
-                       size=(400, 400))
+first_card = ui.Card2D(image_path=first_img_path, title_text="First Card",
+                       body_text="First Card Body", image_scale=0.5,
+                       size=(400, 400), bg_color=(0.3, 0.1, 0.9),
+                       bg_opacity=0.8)
 
-second_card = ui.Card2D(image_path=second_img_path, title="Second Card",
-                        body="Second Card Body", image_scale=0.5,
-                        size=(400, 400))
+second_card = ui.Card2D(image_path=second_img_path, title_text="Second Card",
+                        body_text="Second Card Body", image_scale=0.5,
+                        size=(400, 400), bg_color=(0.9, 0.2, 0.1),
+                        bg_opacity=0.8)
 
 panel.add_element(first_card, (0, 0.25))
 panel.add_element(second_card, (0.556, 0.25))
@@ -56,4 +57,11 @@ show_manager = window.ShowManager(size=current_size,
                                   title="FURY Card Example")
 
 show_manager.scene.add(panel)
-show_manager.start()
+
+# To interact with the UI, set interactive = True
+interactive = False
+
+if interactive:
+    show_manager.start()
+
+window.record(show_manager.scene, out_path="card_ui.png", size=(1000, 1000))
