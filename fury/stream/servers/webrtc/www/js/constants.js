504e17fe9aeafac6dcaf6334679e981c21ecd3d8
devmessias <messias.physics@gmail.com>
2021-06-03 11:51:25 -0300

    improves the organization, add iframe mode
    iframe mode, background color and interaction can
    be defined through url parameters
    
    Example:
    http://localhost:8000/?interaction=1&iframe=0&background=black

--- fury/stream/servers/webrtc/www/js/constants.js
+++ fury/stream/servers/webrtc/www/js/constants.js
@@ -1,1 +1,8 @@
-export const millisecMouseMove = 80
+export const millisecMouseMove = 80;
+export const urlServer = `${location.protocol}//${location.hostname}${
+  location.port ? ":" + location.port : ""
+}/`;
+export const interactionInterval = 200;
+export const videoEl = document.getElementById("video");
+
+export const urlParams = new URLSearchParams(window.location.search);
--- fury/stream/servers/webrtc/www/js/main.js
+++ fury/stream/servers/webrtc/www/js/main.js
@@ -1,40 +1,51 @@
 import { startWebRTC } from "/js/webrtc.js";
-import { millisecMouseMove } from "/js/constants.js";
-
-let interactionInterval = 200; // for throttling
-let mouseLeftReleased = true;
-let mouseOutVideo = false;
-let mouseX = 0;
-let mouseY = 0;
-let ctrlKey = 0;
-let shiftKey = 0;
-let urlServer = "";
-const videoEl = document.getElementById("video");
+import {
+  millisecMouseMove,
+  urlServer,
+  interactionInterval,
+  videoEl,
+  urlParams,
+} from "/js/constants.js";
 
 document.addEventListener("DOMContentLoaded", (event) => {
   document.getElementById("startBtn").addEventListener("click", (e) => {
     startWebRTC();
   });
-  let urlServer = `${location.protocol}//${location.hostname}${
-    location.port ? ":" + location.port : ""
-  }/`
-})
+  const interaction = urlParams.get("interaction");
+  const runningOnIframe = urlParams.get("iframe");
+  const backgroundColor = urlParams.get('background')
+  if (interaction === null || interaction == 1) addInteraction(videoEl);
+  const videoClass =
+    runningOnIframe == null || runningOnIframe == 1
+      ? "videoIframeMode"
+      : "videoNormalMode";
+  videoEl.className = videoClass
+  if (backgroundColor != null)
+    document.body.style.backgroundColor = backgroundColor;
+});
 
-let enableCallWheel = true;
-let currentWheelEventTotalDeltaY = 0;
-videoEl.addEventListener("wheel", (event) => {
-  currentWheelEventTotalDeltaY+=event.deltaY;
-  if (!enableCallWheel) return;
-  const data = { deltaY: currentWheelEventTotalDeltaY };
-  currentWheelEventTotalDeltaY=0;
+function addInteraction(videoEl) {
+  let mouseLeftReleased = true;
+  let mouseOutVideo = false;
+  let mouseX = 0;
+  let mouseY = 0;
+  let ctrlKey = 0;
+  let shiftKey = 0;
+  let enableCallWheel = true;
+  let currentWheelEventTotalDeltaY = 0;
+  videoEl.addEventListener("wheel", (event) => {
+    currentWheelEventTotalDeltaY += event.deltaY;
+    if (!enableCallWheel) return;
+    const data = { deltaY: currentWheelEventTotalDeltaY };
+    currentWheelEventTotalDeltaY = 0;
 
-  fetch(`${urlServer}mouse_weel`, {
-    method: "POST", // or 'PUT'
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
+    fetch(`${urlServer}mouse_weel`, {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
     // .then((response) => response.json())
     // .then((data) => {
     //   // console.log("Success:", data)
@@ -42,136 +53,137 @@ videoEl.addEventListener("wheel", (event) => {
     // .catch((error) => {
     //   console.error("Error:", error);
     // });
-  enableCallWheel = false;
-  setTimeout(() => enableCallWheel = true, interactionInterval);
-});
+    enableCallWheel = false;
+    setTimeout(() => (enableCallWheel = true), interactionInterval);
+  });
 
-videoEl.addEventListener("mousemove", (event) => {
-  mouseOutVideo = false;
-  ctrlKey = event.ctrlKey ? 1 : 0;
-  shiftKey = event.shiftKey ? 1 : 0;
-  const width = videoEl.offsetWidth;
-  const height = videoEl.offsetHeight;
-  mouseX = event.offsetX / width;
-  mouseY = event.offsetY / height;
-});
+  videoEl.addEventListener("mousemove", (event) => {
+    mouseOutVideo = false;
+    ctrlKey = event.ctrlKey ? 1 : 0;
+    shiftKey = event.shiftKey ? 1 : 0;
+    const width = videoEl.offsetWidth;
+    const height = videoEl.offsetHeight;
+    mouseX = event.offsetX / width;
+    mouseY = event.offsetY / height;
+  });
 
-videoEl.addEventListener("mouseleave", (e) => {
-  mouseOutVideo = false;
-});
-// videoEl.addEventListener("mousemove", (e) => {
-//   if (mouseLeftReleased) return
-//   mouseX = e.offsetX
-//   mouseY = e.offsetY
-//   const width = videoEl.offsetWidth
-//   const height = videoEl.offsetHeight
-//   let x = mouseX/height//width
-//   let y = mouseY/width///height
-//   const data = { x: x, y: y }
-//   console.log(x*100, y*100)
-//   fetch("http://localhost:8000/mouse_move", {
-//     method: "POST", // or 'PUT'
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(data),
-//   })
-//     .then((response) => response.json())
-//     .then((data) => {
-//       // console.log("Success:", data)
-//     })
-//     .catch((error) => {
-//       console.error("Error:", error)
-//     })
-// })
+  videoEl.addEventListener("mouseleave", (e) => {
+    mouseOutVideo = false;
+  });
+  // videoEl.addEventListener("mousemove", (e) => {
+  //   if (mouseLeftReleased) return
+  //   mouseX = e.offsetX
+  //   mouseY = e.offsetY
+  //   const width = videoEl.offsetWidth
+  //   const height = videoEl.offsetHeight
+  //   let x = mouseX/height//width
+  //   let y = mouseY/width///height
+  //   const data = { x: x, y: y }
+  //   console.log(x*100, y*100)
+  //   fetch("http://localhost:8000/mouse_move", {
+  //     method: "POST", // or 'PUT'
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(data),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       // console.log("Success:", data)
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error:", error)
+  //     })
+  // })
 
-const mouseMoveCallback = () => {
-  if (mouseLeftReleased || mouseOutVideo) return;
-  const data = { x: mouseX, y: mouseY, ctrlKey: ctrlKey, shiftKey: shiftKey };
-  fetch(`${urlServer}mouse_move`, {
-    method: "POST", // or 'PUT'
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // console.log("Success:", data)
+  const mouseMoveCallback = () => {
+    if (mouseLeftReleased || mouseOutVideo) return;
+    const data = { x: mouseX, y: mouseY, ctrlKey: ctrlKey, shiftKey: shiftKey };
+    fetch(`${urlServer}mouse_move`, {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-};
-const timerMouseMove = setInterval(mouseMoveCallback, millisecMouseMove);
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log("Success:", data)
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+  const timerMouseMove = setInterval(mouseMoveCallback, millisecMouseMove);
 
-videoEl.addEventListener("mousedown", (e) => {
-  mouseLeftReleased = false;
-});
+  videoEl.addEventListener("mousedown", (e) => {
+    mouseLeftReleased = false;
+  });
 
-window.addEventListener("mouseup", (e) => {
-  mouseLeftReleased = true;
-  fetch(`${urlServer}mouse_left_click`, {
-    method: "POST", // or 'PUT'
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ on: 0 }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // console.log("Success:", data)
+  window.addEventListener("mouseup", (e) => {
+    mouseLeftReleased = true;
+    fetch(`${urlServer}mouse_left_click`, {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ on: 0 }),
     })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-});
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log("Success:", data)
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
 
-// document.addEventListener("keydown", function (event) {
-//   ctrlKey = event.ctrlKey ? 1:0
-//   shiftKey =event.shiftKey ? 1:0
-// if (!event.ctrlKey) {
+  // document.addEventListener("keydown", function (event) {
+  //   ctrlKey = event.ctrlKey ? 1:0
+  //   shiftKey =event.shiftKey ? 1:0
+  // if (!event.ctrlKey) {
 
-//   fetch("http://localhost:8000/ctrl_key", {
-//     method: "POST", // or 'PUT'
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({'on': 1}),
-//   })
-//     .then((response) => response.json())
-//     .then((data) => {
-//       // console.log("Success:", data)
-//     })
-//     .catch((error) => {
-//       console.error("Error:", error)
-//     })
-// }
-// })
-// document.addEventListener("keyup", function (event) {
-//   ctrlKey = event.ctrlKey ? 1:0
-//   shiftKey =event.shiftKey ? 1:0
+  //   fetch("http://localhost:8000/ctrl_key", {
+  //     method: "POST", // or 'PUT'
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({'on': 1}),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       // console.log("Success:", data)
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error:", error)
+  //     })
+  // }
+  // })
+  // document.addEventListener("keyup", function (event) {
+  //   ctrlKey = event.ctrlKey ? 1:0
+  //   shiftKey =event.shiftKey ? 1:0
 
-// if (event.ctrlKey) {
-//   console.log('ctrl up')
-//   fetch("http://localhost:8000/ctrl_key", {
-//     method: "POST", // or 'PUT'
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({'on': 0}),
-//   })
-//     .then((response) => response.json())
-//     .then((data) => {
-//       // console.log("Success:", data)
-//     })
-//     .catch((error) => {
-//       console.error("Error:", error)
-//     })
-// }
-// })
+  // if (event.ctrlKey) {
+  //   console.log('ctrl up')
+  //   fetch("http://localhost:8000/ctrl_key", {
+  //     method: "POST", // or 'PUT'
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({'on': 0}),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       // console.log("Success:", data)
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error:", error)
+  //     })
+  // }
+  // })
 
-// document.getElementById('startBtn').addEventListener('wheel', (event)=>
-// {
-//     console.log(event.deltaY)
-// })
+  // document.getElementById('startBtn').addEventListener('wheel', (event)=>
+  // {
+  //     console.log(event.deltaY)
+  // })
+}
