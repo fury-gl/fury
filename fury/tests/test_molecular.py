87ce86a1808bc5b8f0e33e468c7abab351235551
Sajag Swami <sun.tsunami248@gmail.com>
2021-07-21 23:55:41 +0530

    updated the test

--- fury/tests/test_molecular.py
+++ fury/tests/test_molecular.py
@@ -6,7 +6,7 @@ from fury import molecular
 def test_periodic_table():
     table = molecular.PeriodicTable()
     npt.assert_equal(table.atomic_number('C'), 6)
-    npt.assert_equal(table.element_name(7), 'N')
+    npt.assert_equal(table.element_name(7), 'Nitrogen')
     npt.assert_equal(table.atomic_symbol(8), 'O')
     npt.assert_almost_equal(table.atomic_radius(1, 'VDW'), 1.2)
     npt.assert_almost_equal(table.atomic_radius(1, 'Covalent'), 0.32)
