262497198a8494bdfcca7dcb16e7708d32decf94
Serge Koudoro <skab12@gmail.com>
2021-07-26 10:55:37 +0200

    create dummy object

--- fury/ui/tests/test_helpers.py
+++ fury/ui/tests/test_helpers.py
@@ -1,7 +1,6 @@
 """Test helpers fonction ."""
 
-from fury.ui.containers import GridUI
-from fury.actor import Container
+from fury.ui.core import UI
 import numpy.testing as npt
 
 from fury import window, ui
@@ -51,10 +50,44 @@ def test_clip_overflow():
                       text, rectangle.size[0], 'middle')
 
 
+class DummyActor:
+    def __init__(self, act):
+        self._act = act
+
+    @property
+    def act(self):
+        return self._act
+
+    def add_to_scene(self, ren):
+        """ Adds the items of this container to a given scene. """
+        return ren
+
+
+class DummyUI(UI):
+    def __init__(self, act):
+        super(DummyUI, self).__init__()
+        self.act = act
+
+    def _setup(self):
+        pass
+
+    def _get_actors(self):
+        return []
+
+    def _add_to_scene(self, scene):
+        return scene
+
+    def _get_size(self):
+        return (5, 5)
+
+    def _set_position(self, coords):
+        return coords
+
+
 def test_is_ui():
     panel = ui.Panel2D(position=(0, 0), size=(100, 100))
-    grid = GridUI(actors=[])
-    container = Container()
+    grid = DummyUI(act=[])
+    container = DummyActor(act="act")
 
     npt.assert_equal(True, is_ui(panel))
     npt.assert_equal(False, is_ui(container))
