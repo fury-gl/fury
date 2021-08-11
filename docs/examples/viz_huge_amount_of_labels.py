a8028cca3eed195e134f7358e59da5be14bf2d53
devmessias <messias.physics@gmail.com>
2021-08-11 15:46:58 -0300

    a simple example using the billboard labels

---
+++ docs/examples/viz_huge_amount_of_labels.py
@@ -0,0 +1,119 @@
+"""
+============================================================
+This examples shows how to visualize a large amount of labels
+=============================================================
+
+The goal of this example is to show how to visualize a large amount of labels
+using the interdisciplinary map of journal networks.
+
+"""
+
+###############################################################################
+# First, let's import some useful functions
+
+from os.path import join as pjoin
+from fury import actor, window, colormap as cmap
+from fury.actor_text import bitmap_labels
+import numpy as np
+
+###############################################################################
+# Then let's download some available datasets.
+
+from fury.data.fetcher import fetch_viz_wiki_nw
+
+files, folder = fetch_viz_wiki_nw()
+categories_file, edges_file, positions_file = sorted(files.keys())
+
+###############################################################################
+# We read our datasets
+
+positions = np.loadtxt(pjoin(folder, positions_file))
+categories = np.loadtxt(pjoin(folder, categories_file), dtype=str)
+edges = np.loadtxt(pjoin(folder, edges_file), dtype=int)
+
+###############################################################################
+# We attribute a color to each category of our dataset which correspond to our
+# nodes colors.
+
+category2index = {category: i
+                  for i, category in enumerate(np.unique(categories))}
+
+index2category = np.unique(categories)
+
+categoryColors = cmap.distinguishable_colormap(nb_colors=len(index2category))
+
+colors = np.array([categoryColors[category2index[category]]
+                   for category in categories])
+
+###############################################################################
+# We define our node size
+
+radii = 1 + np.random.rand(len(positions))
+
+###############################################################################
+# Lets create our edges now. They will indicate a citation between two nodes.
+# OF course, the colors of each edges will be an interpolation between the two
+# node that it connects.
+
+edgesPositions = []
+edgesColors = []
+for source, target in edges:
+    edgesPositions.append(np.array([positions[source], positions[target]]))
+    edgesColors.append(np.array([colors[source], colors[target]]))
+
+edgesPositions = np.array(edgesPositions)
+edgesColors = np.average(np.array(edgesColors), axis=1)
+
+###############################################################################
+# Our data preparation is ready, it is time to visualize them all. We start to
+# build 2 actors that we represent our data : sphere_actor for the nodes and
+# lines_actor for the edges.
+
+sphere_actor = actor.markers(
+    centers=positions,
+    colors=colors,
+    scales=radii*0.1,
+)
+
+lines_actor = actor.line(
+    edgesPositions,
+    colors=edgesColors,
+    opacity=0.1,
+)
+
+###############################################################################
+#
+#
+
+labels = [
+    f'{category} journal {i}' for i, category in enumerate(categories)
+]
+my_text_actor = bitmap_labels(
+    positions, labels,
+    y_offset_ratio=2,
+    align='center', scales=.1)
+
+###############################################################################
+# All actors need to be added in a scene, so we build one and add our
+# lines_actor and sphere_actor.
+
+scene = window.Scene()
+
+scene.add(lines_actor)
+scene.add(sphere_actor)
+scene.add(my_text_actor)
+
+###############################################################################
+# The final step ! Visualize and save the result of our creation! Please,
+# switch interactive variable to True if you want to visualize it.
+
+interactive = False
+
+if interactive:
+    window.show(scene, size=(600, 600))
+
+window.record(scene, out_path='journal_networks.png', size=(600, 600))
+
+###############################################################################
+# This example can be improved by adding some interactivy with slider,
+# picking, etc. Play with it, improve it!
