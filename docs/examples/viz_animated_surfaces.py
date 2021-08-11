27b13af18781e79b194f9b15a7d1a7f5624684b5
Sajag Swami <sun.tsunami248@gmail.com>
2021-08-11 19:19:01 +0530

    use utility function, other minor changes

--- docs/examples/viz_animated_surfaces.py
+++ docs/examples/viz_animated_surfaces.py
@@ -1,10 +1,10 @@
 """
 ===============================================
-Animating a time-varying 2D wave function
+Animated 2D functions
 ===============================================
 
-This is a simple demonstration of how one can visualize
-time-varying 2D wave functions using FURY.
+This is a simple demonstration of how one can
+animate 2D functions using FURY.
 """
 
 ###############################################################################
@@ -16,13 +16,14 @@ import itertools
 
 
 ###############################################################################
-# The following function is used to create and/or update the coordinates of the
-# points which are being used to plot the wave.
+# The following function is used to create and update the coordinates of the
+# points which are being used to plot the surface. It's also used to create
+# and update the colormap being used to color the surface.
 # Kindly note that only the z coordinate is being modified with time as only
 # the z coordinate is a function of time.
 
 
-def update_coordinates(x, y, equation, cmap_name='viridis'):
+def update_surface(x, y, equation, cmap_name='viridis'):
 
     # z is the function F i.e. F(x, y, t)
     z = eval(equation)
@@ -42,8 +43,8 @@ def update_coordinates(x, y, equation, cmap_name='viridis'):
 #       initial value of the time variable i.e. value of the time variable at
 #       the beginning of the program; (default = 0)
 # dt: float
-#     amount by which the "time" variable is incremented for every iteration
-#     of timer_callback function; (default = 0.1)
+#     amount by which ``time`` variable is incremented for every iteration
+#     of timer_callback function (default = 0.1)
 # lower_xbound: float
 #               lower bound of the x values in which the function is plotted
 #               (default = -1)
@@ -80,21 +81,20 @@ x = x.reshape(-1)
 y = y.reshape(-1)
 
 ###############################################################################
-# Initializing wave_actors and adjusting lighting options to make the waves
-# look more aesthetically pleasant.
+# Function used to create surface obtained from 2D equation.
 
 
-def wave(x, y, equation, colormap_name):
-    xyz, colors = update_coordinates(x, y, equation=equation,
+def create_surface(x, y, equation, colormap_name):
+    xyz, colors = update_surface(x, y, equation=equation,
                                      cmap_name=colormap_name)
-    wave_actor = actor.surface(xyz, colors=colors)
-    wave_actor.equation = equation
-    wave_actor.cmap_name = colormap_name
-    wave_actor.vertices = utils.vertices_from_actor(wave_actor)
-    wave_actor.no_vertices_per_point = len(wave_actor.vertices)/npoints**2
-    wave_actor.initial_vertices = wave_actor.vertices.copy() - \
-        np.repeat(xyz, wave_actor.no_vertices_per_point, axis=0)
-    return wave_actor
+    surf = actor.surface(xyz, colors=colors)
+    surf.equation = equation
+    surf.cmap_name = colormap_name
+    surf.vertices = utils.vertices_from_actor(surf)
+    surf.no_vertices_per_point = len(surf.vertices)/npoints**2
+    surf.initial_vertices = surf.vertices.copy() - \
+        np.repeat(xyz, surf.no_vertices_per_point, axis=0)
+    return surf
 
 
 ###############################################################################
@@ -112,12 +112,11 @@ equations = [eq1, eq2, eq3, eq4]
 cmap_names = ['hot', 'plasma', 'viridis', 'ocean']
 
 ###############################################################################
-# Creating a list of wave actors.
-waves = []
+# Creating a list of surfaces.
+surfaces = []
 for i in range(4):
-    waves.append(wave(x, y, equation=equations[i],
+    surfaces.append(create_surface(x, y, equation=equations[i],
                  colormap_name=cmap_names[i]))
-actors = waves
 
 
 ###############################################################################
@@ -129,8 +128,7 @@ scene.set_camera(position=(4.45, -21, 12), focal_point=(4.45, 0.0, 0.0),
 showm = window.ShowManager(scene, size=(600, 600))
 
 ###############################################################################
-# Creating an axes actor(for reference) and a grid to store multiple functions.
-# Adding them both to the scene.
+# Creating a grid to interact with surfaces individually.
 
 # To store the function names
 text = []
@@ -139,13 +137,14 @@ for i in range(4):
     t_actor.SetCamera(scene.camera())
     text.append(t_actor)
 
-# grid
-grid_ui = ui.GridUI(actors=actors, captions=text,
+grid_ui = ui.GridUI(actors=surfaces, captions=text,
                     caption_offset=(-0.7, -2.5, 0), dim=(1, 4),
                     cell_padding=2,
                     aspect_ratio=1,
                     rotation_axis=(0, 1, 0))
 showm.scene.add(grid_ui)
+
+# Adding an axes actor to the first surface.
 showm.scene.add(actor.axes())
 
 
@@ -167,22 +166,21 @@ end = 200
 
 ###############################################################################
 # Coordinates to be plotted are changed everytime timer_callback is called by
-# using the update_coordinates function. The 2D functions are rendered here
+# using the ``update_surface`` function. The 2D functions are rendered here.
 
 def timer_callback(_obj, _event):
     global xyz, time
     time += dt
     cnt = next(counter)
 
-    # updating the colors and vertices of the triangles used to form the waves
-    for wave in waves:
-        xyz, colors = update_coordinates(x, y, equation=wave.equation,
-                                         cmap_name=wave.cmap_name)
-        wave.GetMapper().GetInput().GetPointData().\
-            SetScalars(utils.numpy_to_vtk_colors(255*colors))
-        wave.vertices[:] = wave.initial_vertices + \
-            np.repeat(xyz, wave.no_vertices_per_point, axis=0)
-        utils.update_actor(wave)
+    # updating the colors and vertices of the triangles used to form the surfaces
+    for surf in surfaces:
+        xyz, colors = update_surface(x, y, equation=surf.equation,
+                                         cmap_name=surf.cmap_name)
+        utils.update_surface_actor_colors(surf, colors)
+        surf.vertices[:] = surf.initial_vertices + \
+            np.repeat(xyz, surf.no_vertices_per_point, axis=0)
+        utils.update_actor(surf)
 
     showm.render()
     # to end the animation
@@ -194,7 +192,7 @@ def timer_callback(_obj, _event):
 # Run every 30 milliseconds
 showm.add_timer_callback(True, 30, timer_callback)
 
-interactive = False
+interactive = True
 if interactive:
     showm.start()
 
