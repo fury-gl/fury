c95048bf96cf5dfa50809d7f2a4422925f0524b4
Javier Guaje <jrguajeg@iu.edu>
2021-08-24 12:52:34 -0400

    Added 2nd dataset. Added normals calculation. Added DBSCAN clustering. Added RANSAC plane detection.

--- docs/experimental/viz_basic_xyz_pcl.py
+++ docs/experimental/viz_basic_xyz_pcl.py
@@ -2,16 +2,59 @@ from fury import actor, window
 from docs.experimental.tmp_pcl_fetcher import read_viz_dataset
 
 
+import matplotlib.pyplot as plt
 import numpy as np
+import open3d as o3d
 import vtk
 
 
-pcl_fname = read_viz_dataset('sample.xyz')
+#pcl_fname = read_viz_dataset('sample.xyz')
+pcl_fname = read_viz_dataset('NZ19_Wellington_voxel-center_2.xyz')
 
-point_cloud = np.loadtxt(pcl_fname, skiprows=1)
+#point_cloud = np.loadtxt(pcl_fname, skiprows=1)
+point_cloud = np.loadtxt(pcl_fname, delimiter=';', skiprows=1)
 
 xyz = point_cloud[:, :3]
-rgb = point_cloud[:, 3:]
+
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector(xyz)
+
+#rgb = point_cloud[:, 3:]
+rgb = np.ones((len(xyz), 3)) * 255
+#pcd.colors = o3d.utility.Vector3dVector(rgb / 255)
+
+# Normals estimation
+pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(
+    radius=.1, max_nn=30))
+
+normals = np.asarray(pcd.normals)
+rgb = (normals + 1) / 2 * 255
+#pcd.colors = o3d.utility.Vector3dVector((normals + 1) / 2)
+
+"""
+plane_model, inliers = pcd.segment_plane(distance_threshold=.1, ransac_n=3,
+                                         num_iterations=1000)
+inlier_cloud = pcd.select_by_index(inliers)
+outlier_cloud = pcd.select_by_index(inliers, invert=True)
+inlier_cloud.paint_uniform_color([1, 0, 0])
+outlier_cloud.paint_uniform_color([.5, .5, .5])
+o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
+"""
+
+"""
+# Clustering with DBSCAN
+with o3d.utility.VerbosityContextManager(
+        o3d.utility.VerbosityLevel.Debug) as cm:
+    labels = np.array(pcd.cluster_dbscan(eps=.027, min_points=10,
+                                         print_progress=True))
+
+max_label = labels.max()
+cols = plt.get_cmap('tab20')(labels / (max_label if max_label > 0 else 1))
+cols[labels < 0] = 0
+
+rgb = cols[:, :3] * 255
+#pcd.colors = o3d.utility.Vector3dVector(cols[:, :3])
+"""
 
 ## VTK
 # Create the geometry of a point (the coordinate)
@@ -50,8 +93,17 @@ pcl_actor.SetMapper(mapper)
 #pcl_actor.GetProperty().SetPointSize(10)
 
 scene = window.Scene()
+scene.background((1, 1, 1))
 
 #scene.add(actor.dots(xyz))
 scene.add(pcl_actor)
 
-window.show(scene)
+scene.roll(-145)
+scene.pitch(70)
+scene.reset_camera()
+scene.reset_clipping_range()
+#scene.zoom(2)
+
+window.show(scene, reset_camera=False)
+#o3d.visualization.draw_geometries([pcd])
+#o3d.visualization.draw_geometries([pcd], point_show_normal=True)
