72aca3ebb91f0d2354e0f5dd5d1dd5c0a47d9999
Javier Guaje <jrguajeg@iu.edu>
2021-08-25 12:59:12 -0400

    Added RANSAC and DBSCAN segmentations.

--- docs/experimental/viz_basic_xyz_pcl.py
+++ docs/experimental/viz_basic_xyz_pcl.py
@@ -9,18 +9,22 @@ import vtk
 
 
 #pcl_fname = read_viz_dataset('sample.xyz')
-pcl_fname = read_viz_dataset('NZ19_Wellington_voxel-center_2.xyz')
+#pcl_fname = read_viz_dataset('NZ19_Wellington_voxel-center_2.xyz')
+pcl_fname = read_viz_dataset('TLS_kitchen.ply')
 
 #point_cloud = np.loadtxt(pcl_fname, skiprows=1)
-point_cloud = np.loadtxt(pcl_fname, delimiter=';', skiprows=1)
+#point_cloud = np.loadtxt(pcl_fname, delimiter=';', skiprows=1)
+pcd = o3d.io.read_point_cloud(pcl_fname)
 
-xyz = point_cloud[:, :3]
+#xyz = point_cloud[:, :3]
+xyz = np.asarray(pcd.points)
 
-pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(xyz)
+#pcd = o3d.geometry.PointCloud()
+#pcd.points = o3d.utility.Vector3dVector(xyz)
 
 #rgb = point_cloud[:, 3:]
-rgb = np.ones((len(xyz), 3)) * 255
+#rgb = np.ones((len(xyz), 3)) * 255
+rgb = np.ones((len(xyz), 3)) * 40
 #pcd.colors = o3d.utility.Vector3dVector(rgb / 255)
 
 # Normals estimation
@@ -32,28 +36,83 @@ rgb = (normals + 1) / 2 * 255
 #pcd.colors = o3d.utility.Vector3dVector((normals + 1) / 2)
 
 """
-plane_model, inliers = pcd.segment_plane(distance_threshold=.1, ransac_n=3,
+plane_model, inliers = pcd.segment_plane(distance_threshold=.01, ransac_n=3,
                                          num_iterations=1000)
-inlier_cloud = pcd.select_by_index(inliers)
-outlier_cloud = pcd.select_by_index(inliers, invert=True)
-inlier_cloud.paint_uniform_color([1, 0, 0])
-outlier_cloud.paint_uniform_color([.5, .5, .5])
-o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
+#inlier_cloud = pcd.select_by_index(inliers)
+#outlier_cloud = pcd.select_by_index(inliers, invert=True)
+#inlier_cloud.paint_uniform_color([1, 0, 0])
+#outlier_cloud.paint_uniform_color([.5, .5, .5])
+#o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
+rgb = np.ones((len(xyz), 3)) * 40
+rgb[inliers, :] = [255, 0, 0]
 """
 
 """
 # Clustering with DBSCAN
 with o3d.utility.VerbosityContextManager(
         o3d.utility.VerbosityLevel.Debug) as cm:
-    labels = np.array(pcd.cluster_dbscan(eps=.027, min_points=10,
+    labels = np.array(pcd.cluster_dbscan(eps=.05, min_points=10,
                                          print_progress=True))
+#labels = np.array(pcd.cluster_dbscan(eps=.05, min_points=10))
 
 max_label = labels.max()
 cols = plt.get_cmap('tab20')(labels / (max_label if max_label > 0 else 1))
 cols[labels < 0] = 0
-
 rgb = cols[:, :3] * 255
-#pcd.colors = o3d.utility.Vector3dVector(cols[:, :3])
+pcd.colors = o3d.utility.Vector3dVector(cols[:, :3])
+"""
+
+segment_models = {}
+segments = {}
+
+max_plane_idx = 20
+
+cmap = plt.get_cmap('tab20')
+ransac_thr = .01
+
+rest = pcd
+xyz = []
+rgb = []
+for i in range(max_plane_idx):
+    color = list(cmap(i)[:3])
+    segment_models[i], inliers = rest.segment_plane(
+        distance_threshold=ransac_thr, ransac_n=3, num_iterations=1000)
+    inlier_cloud = rest.select_by_index(inliers)
+    pnts = np.asarray(inlier_cloud.points)
+    ransac_num_pnts = len(pnts)
+    labels = np.array(inlier_cloud.cluster_dbscan(eps=ransac_thr * 10,
+                                                  min_points=10))
+    clusters_ids = np.unique(labels)
+    ppc = [len(np.where(labels == j)[0]) for j in clusters_ids]
+    biggest_cluster = clusters_ids[np.argwhere(ppc == np.max(ppc))[0][0]]
+    #segments[i] = inlier_cloud
+    #segments[i].paint_uniform_color(color)
+    rest = rest.select_by_index(inliers, invert=True)
+    rest += inlier_cloud.select_by_index(list(np.where(
+        labels != biggest_cluster)[0]))
+    inlier_cloud = inlier_cloud.select_by_index(list(np.where(
+        labels == biggest_cluster)[0]))
+    pnts = np.asarray(inlier_cloud.points)
+    cols = np.repeat(np.array([color]), len(pnts), axis=0) * 255
+    xyz.extend(pnts)
+    rgb.extend(cols)
+    print('{:02d}/{} done. {: 7d} points found by RANSAC. '
+          '{: 7d} points retained by DBSCAN.'.format(i + 1, max_plane_idx,
+                                                 ransac_num_pnts, len(pnts)))
+labels = np.array(rest.cluster_dbscan(eps=.05, min_points=5))
+max_label = labels.max()
+pnts = np.asarray(rest.points)
+#cols = np.ones((len(pnts), 3)) * 40
+cols = cmap(labels / max_label if max_label > 0 else 1)
+cols[labels < 0] = 0
+cols = cols[:, :3] * 255
+xyz.extend(pnts)
+rgb.extend(cols)
+xyz = np.asarray(xyz)
+rgb = np.asarray(rgb)
+"""
+o3d.visualization.draw_geometries([segments[i] for i in range(
+    max_plane_idx)] + [rest])
 """
 
 ## VTK
@@ -98,11 +157,17 @@ scene.background((1, 1, 1))
 #scene.add(actor.dots(xyz))
 scene.add(pcl_actor)
 
-scene.roll(-145)
-scene.pitch(70)
+#scene.roll(-145)
+#scene.pitch(70)
+scene.roll(-160)
+scene.pitch(85)
+
 scene.reset_camera()
 scene.reset_clipping_range()
+
 #scene.zoom(2)
+#scene.zoom(2.1)
+scene.zoom(2.4)
 
 window.show(scene, reset_camera=False)
 #o3d.visualization.draw_geometries([pcd])
