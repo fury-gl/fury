ba7089b0574f25fe86165b6f65fbfe51f982b611
ganimtron-10 <praneethshetty10@gmail.com>
2021-09-14 09:36:47 +0530

    updating tests

--- docs/examples/viz_spinbox.py
+++ docs/examples/viz_spinbox.py
@@ -21,12 +21,12 @@ fetch_viz_icons()
 ###############################################################################
 # Let's create a Cone.
 
-# cone = actor.cone(np.random.rand(1,3), np.random.rand(1,3), (1, 1, 1), np.random.rand(1))
+cone = actor.cone(np.random.rand(1,3), np.random.rand(1,3), (1, 1, 1), np.random.rand(1))
 
 ###############################################################################
 #Creating the SpinBox UI.
 
-spinbox = ui.SpinBox(position=(400,400),size=(300,100),min_val=0,max_val=360,initial_val=180,step=10)
+spinbox = ui.SpinBox(position=(200,100),size=(300,100),min_val=0,max_val=360,initial_val=180,step=10)
 
 ###############################################################################
 # Now that all the elements have been initialised, we add them to the show
@@ -36,23 +36,25 @@ current_size = (800, 800)
 show_manager = window.ShowManager(size=current_size,
                                   title="FURY SpinBox Example")
 
-# show_manager.scene.add(cone)
+show_manager.scene.add(cone)
 show_manager.scene.add(spinbox)
 
 ###############################################################################
 # Using the on_change hook to rotate the scene.
 
-# def rotate_cone(spinbox):
-#     show_manager.scene.azimuth(spinbox.value)
+def rotate_cone(spinbox):
+    show_manager.scene.azimuth(spinbox.value)
 
 
-# spinbox.on_change = rotate_cone
+spinbox.on_change = rotate_cone
 
+###############################################################################
+# Starting the ShowManager.
 
-interactive = True
+interactive = False
 
 if interactive:
     show_manager.start()
 
-# window.record(show_manager.scene, size=current_size,
-#               out_path="viz_spinbox.png")
+window.record(show_manager.scene, size=current_size,
+              out_path="viz_spinbox.png")
