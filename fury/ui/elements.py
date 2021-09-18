77550357984b34e2e632c421c99045288abcf40f
ganimtron-10 <praneethshetty10@gmail.com>
2021-09-18 13:03:10 +0530

    pep fixs

--- fury/ui/elements.py
+++ fury/ui/elements.py
@@ -4158,7 +4158,8 @@ class SpinBox(UI):
         """
         self.panel = Panel2D(size=self.panel_size, color=self.panel_color)
 
-        self.textbox = TextBox2D(width=self.textbox_width, height=self.textbox_height)
+        self.textbox = TextBox2D(width=self.textbox_width,
+                                 height=self.textbox_height)
         self.textbox.set_message(str(self.value))
         self.increment_button = Button2D(
             icon_fnames=[("up", read_viz_icons(fname="circle-up.png"))])
