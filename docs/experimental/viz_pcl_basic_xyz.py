d113ae2d5df61409d8ddfa63aa62e6cab481503c
Javier Guaje <jrguajeg@iu.edu>
2021-10-01 19:39:48 -0400

    Added normals to sculpture viz.

--- docs/experimental/viz_pcl_basic_xyz.py
+++ docs/experimental/viz_pcl_basic_xyz.py
@@ -1,6 +1,6 @@
 from docs.experimental.tmp_pcl_fetcher import read_viz_dataset
 from fury import window
-from vtk.util import numpy_support
+from fury.utils import set_polydata_colors, set_polydata_normals
 
 
 import numpy as np
@@ -23,47 +23,40 @@ pcd.colors = o3d.utility.Vector3dVector(rgb / 255)
 # Normals estimation
 pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(
     radius=.1, max_nn=30))
+#pcd.orient_normals_towards_camera_location(
+#    camera_location=scene.get_camera()[0])
+pcd.orient_normals_consistent_tangent_plane(10)
 
-ijk = np.asarray(pcd.normals)
-rgb = (ijk + 1) / 2 * 255
-#pcd.colors = o3d.utility.Vector3dVector((ijk + 1) / 2)
+normals = np.asarray(pcd.normals)
+#rgb = (normals + 1) / 2 * 255
+#pcd.colors = o3d.utility.Vector3dVector((normals + 1) / 2)
 
 ## VTK
 # Create the geometry of a point (the coordinate)
 points = vtk.vtkPoints()
 # Create the topology of the point (a vertex)
 vertices = vtk.vtkCellArray()
-# Setup colors
-colors = vtk.vtkUnsignedCharArray()
-colors.SetNumberOfComponents(3)
-colors.SetName('colors')
-# Setup normals
-#normals = vtk.vtkUnsignedCharArray()
-#normals.SetNumberOfComponents(3)
 # Add points
 for i in range(0, len(xyz)):
     p = xyz[i]
     id = points.InsertNextPoint(p)
     vertices.InsertNextCell(1)
     vertices.InsertCellPoint(id)
-    colors.InsertNextTuple3(rgb[i][0], rgb[i][1], rgb[i][2])
-    #normals.InsertNextTuple3(ijk[i][0], ijk[i][1], ijk[i][2])
 # Create a polydata object
-point = vtk.vtkPolyData()
+polydata = vtk.vtkPolyData()
 # Set the points and vertices we created as the geometry and topology of the
 # polydata
-point.SetPoints(points)
-point.SetVerts(vertices)
-point.GetPointData().SetScalars(colors)
-#normals = numpy_support.numpy_to_vtk(ijk, deep=True)
-#point.GetPointData().SetNormals(normals)
-point.Modified()
+polydata.SetPoints(points)
+polydata.SetVerts(vertices)
+set_polydata_colors(polydata, rgb)
+set_polydata_normals(polydata, normals)
+polydata.Modified()
 # Visualize
 mapper = vtk.vtkPolyDataMapper()
 if vtk.VTK_MAJOR_VERSION <= 5:
-    mapper.SetInput(point)
+    mapper.SetInput(polydata)
 else:
-    mapper.SetInputData(point)
+    mapper.SetInputData(polydata)
 ## ACTOR
 # Create an actor
 pcl_actor = vtk.vtkActor()
