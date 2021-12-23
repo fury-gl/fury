8a174227e5eb353cea71ebe88d6a90a418e58ce3
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2021-12-23 14:48:28 -0500

    Completed large investigation phase. We need to replace record with snapshot.

--- fury/ui/tests/test_containers.py
+++ fury/ui/tests/test_containers.py
@@ -24,73 +24,7 @@ def test_wrong_interactor_style():
     npt.assert_raises(TypeError, panel.add_to_scene, dummy_scene)
 
 
-def test_ui_tab_ui(interactive=False):
-    filename = "test_ui_tab_ui"
-    recording_filename = pjoin(DATA_DIR, filename + ".log.gz")
-    expected_events_counts_filename = pjoin(DATA_DIR, filename + ".json")
-
-    tab_ui = ui.TabUI(position=(50, 50), size=(300, 300), nb_tabs=3,
-                      draggable=True)
-
-    tab_ui.tabs[0].title = "Tab 1"
-    tab_ui.tabs[1].title = "Tab 2"
-    tab_ui.tabs[2].title = "Tab 3"
-
-    tab_ui.add_element(0, ui.Checkbox(["Option 1", "Option 2"]), (0.5, 0.5))
-    tab_ui.add_element(1, ui.LineSlider2D(), (0.0, 0.5))
-    tab_ui.add_element(2, ui.TextBlock2D(), (0.5, 0.5))
-
-    with npt.assert_raises(IndexError):
-        tab_ui.add_element(3, ui.TextBlock2D(), (0.5, 0.5, 0.5))
-
-    with npt.assert_raises(IndexError):
-        tab_ui.remove_element(3, ui.TextBlock2D())
-
-    with npt.assert_raises(IndexError):
-        tab_ui.update_element(3, ui.TextBlock2D(), (0.5, 0.5, 0.5))
-
-    npt.assert_equal("Tab 1", tab_ui.tabs[0].title)
-    npt.assert_equal("Tab 2", tab_ui.tabs[1].title)
-    npt.assert_equal("Tab 3", tab_ui.tabs[2].title)
-
-    npt.assert_equal(3, tab_ui.nb_tabs)
-
-    collapses = itertools.count()
-    changes = itertools.count()
-
-    def collapse(tab_ui):
-        if tab_ui.collapsed:
-            next(collapses)
-
-    def tab_change(tab_ui):
-        next(changes)
-
-    tab_ui.on_change = tab_change
-    tab_ui.on_collapse = collapse
-
-    event_counter = EventCounter()
-    event_counter.monitor(tab_ui)
-
-    current_size = (800, 800)
-    show_manager = window.ShowManager(
-        size=current_size, title="Tab UI Test")
-    show_manager.scene.add(tab_ui)
-
-    if interactive:
-        show_manager.record_events_to_file(recording_filename)
-        print(list(event_counter.events_counts.items()))
-        event_counter.save(expected_events_counts_filename)
-    else:
-        show_manager.play_events_from_file(recording_filename)
-        expected = EventCounter.load(expected_events_counts_filename)
-        event_counter.check_counts(expected)
-
-    npt.assert_equal(0, tab_ui.active_tab_idx)
-    npt.assert_equal(11, next(changes))
-    npt.assert_equal(5, next(collapses))
-
-
-def test_grid_ui(interactive=False):
+def test_grid_ui1(interactive=False):
     vol1 = np.zeros((100, 100, 100))
     vol1[25:75, 25:75, 25:75] = 100
 
@@ -176,10 +110,74 @@ def test_grid_ui(interactive=False):
     arr = window.snapshot(scene)
     report = window.analyze_snapshot(arr)
     npt.assert_equal(report.objects > 9, True)
+    print(report.objects)
+
+
+def test_grid_ui2(interactive=False):
+
+    vol1 = np.zeros((100, 100, 100))
+    vol1[25:75, 25:75, 25:75] = 100
+
+    colors = distinguishable_colormap(nb_colors=3)
+    contour_actor1 = actor.contour_from_roi(vol1, np.eye(4),
+                                            colors[0], 1.)
+
+    vol2 = np.zeros((100, 100, 100))
+    vol2[25:75, 25:75, 25:75] = 100
+
+    contour_actor2 = actor.contour_from_roi(vol2, np.eye(4),
+                                            colors[1], 1.)
+
+    vol3 = np.zeros((100, 100, 100))
+    vol3[25:75, 25:75, 25:75] = 100
+
+    contour_actor3 = actor.contour_from_roi(vol3, np.eye(4),
+                                            colors[2], 1.)
+
+    scene = window.Scene()
+    actors = []
+    texts = []
+
+    actors.append(contour_actor1)
+    text_actor1 = actor.text_3d('cube 1', justification='center')
+    texts.append(text_actor1)
+
+    actors.append(contour_actor2)
+    text_actor2 = actor.text_3d('cube 2', justification='center')
+    texts.append(text_actor2)
+
+    actors.append(contour_actor3)
+    text_actor3 = actor.text_3d('cube 3', justification='center')
+    texts.append(text_actor3)
+
+    actors.append(shallow_copy(contour_actor1))
+    text_actor1 = actor.text_3d('cube 4', justification='center')
+    texts.append(text_actor1)
+
+    actors.append(shallow_copy(contour_actor2))
+    text_actor2 = actor.text_3d('cube 5', justification='center')
+    texts.append(text_actor2)
+
+    actors.append(shallow_copy(contour_actor3))
+    text_actor3 = actor.text_3d('cube 6', justification='center')
+    texts.append(text_actor3)
+
+    actors.append(shallow_copy(contour_actor1))
+    text_actor1 = actor.text_3d('cube 7', justification='center')
+    texts.append(text_actor1)
+
+    actors.append(shallow_copy(contour_actor2))
+    text_actor2 = actor.text_3d('cube 8', justification='center')
+    texts.append(text_actor2)
+
+    actors.append(shallow_copy(contour_actor3))
+    text_actor3 = actor.text_3d('cube 9', justification='center')
+    texts.append(text_actor3)
+
 
     # this needs to happen automatically when start() ends.
-    for act in actors:
-        act.RemoveAllObservers()
+    # for act in actors:
+    #     act.RemoveAllObservers()
 
     filename = "test_grid_ui"
     recording_filename = pjoin(DATA_DIR, filename + ".log.gz")
@@ -235,3 +233,77 @@ def test_ui_image_container_2d(interactive=False):
     show_manager.scene.add(image_test)
     if interactive:
         show_manager.start()
+
+
+def test_ui_tab_ui(interactive=False):
+    filename = "test_ui_tab_ui"
+    recording_filename = pjoin(DATA_DIR, filename + ".log.gz")
+    expected_events_counts_filename = pjoin(DATA_DIR, filename + ".json")
+
+    tab_ui = ui.TabUI(position=(50, 50), size=(300, 300), nb_tabs=3,
+                      draggable=True)
+
+    tab_ui.tabs[0].title = "Tab 1"
+    tab_ui.tabs[1].title = "Tab 2"
+    tab_ui.tabs[2].title = "Tab 3"
+
+    tab_ui.add_element(0, ui.Checkbox(["Option 1", "Option 2"]), (0.5, 0.5))
+    tab_ui.add_element(1, ui.LineSlider2D(), (0.0, 0.5))
+    tab_ui.add_element(2, ui.TextBlock2D(), (0.5, 0.5))
+
+    with npt.assert_raises(IndexError):
+        tab_ui.add_element(3, ui.TextBlock2D(), (0.5, 0.5, 0.5))
+
+    with npt.assert_raises(IndexError):
+        tab_ui.remove_element(3, ui.TextBlock2D())
+
+    with npt.assert_raises(IndexError):
+        tab_ui.update_element(3, ui.TextBlock2D(), (0.5, 0.5, 0.5))
+
+    npt.assert_equal("Tab 1", tab_ui.tabs[0].title)
+    npt.assert_equal("Tab 2", tab_ui.tabs[1].title)
+    npt.assert_equal("Tab 3", tab_ui.tabs[2].title)
+
+    npt.assert_equal(3, tab_ui.nb_tabs)
+
+    collapses = itertools.count()
+    changes = itertools.count()
+
+    def collapse(tab_ui):
+        if tab_ui.collapsed:
+            next(collapses)
+
+    def tab_change(tab_ui):
+        next(changes)
+
+    tab_ui.on_change = tab_change
+    tab_ui.on_collapse = collapse
+
+    event_counter = EventCounter()
+    event_counter.monitor(tab_ui)
+
+    current_size = (800, 800)
+    show_manager = window.ShowManager(
+        size=current_size, title="Tab UI Test")
+    show_manager.scene.add(tab_ui)
+
+    if interactive:
+        show_manager.record_events_to_file(recording_filename)
+        print(list(event_counter.events_counts.items()))
+        event_counter.save(expected_events_counts_filename)
+    else:
+        show_manager.play_events_from_file(recording_filename)
+        expected = EventCounter.load(expected_events_counts_filename)
+        event_counter.check_counts(expected)
+
+    npt.assert_equal(0, tab_ui.active_tab_idx)
+    npt.assert_equal(11, next(changes))
+    npt.assert_equal(5, next(collapses))
+
+
+# test_wrong_interactor_style()
+# test_grid_ui()
+# test_grid_ui2()
+# test_ui_tab_ui()
+
+
