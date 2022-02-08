f18dcbc9d119b74fc2877de729494ab5aa9fe704
sreekar chigurupati <chigurupatisreekar@gmail.com>
2022-02-08 17:56:25 -0500

    added non working index and css files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -169,6 +169,10 @@ html_theme_options = {
 
 }
 
+html_additional_pages = {
+    'index': 'index.html'
+    }
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
