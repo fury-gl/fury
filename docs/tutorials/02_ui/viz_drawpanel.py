6c9cd13c653d80780319bcdcdf2e190532aaeb63
Praneeth Shetty <praneethshetty10@gmail.com>
2022-06-09 11:33:46 +0530

    adding tutorial

---
+++ docs/tutorials/02_ui/viz_drawpanel.py
@@ -0,0 +1,27 @@
+"""
+=========
+DrawPanel
+=========
+
+This example shows how to use the DrawPanel UI. We will demonstrate how to
+create Various shapes and transform them.
+
+First, some imports.
+"""
+from fury import ui, window
+
+#########################################################################
+# We then create a DrawPanel Object.
+
+drawing_canvas = ui.DrawPanel()
+
+###############################################################################
+# Show Manager
+# ==================================
+#
+# Now we add DrawPanel to the scene.
+
+current_size = (410, 410)
+showm = window.ShowManager(size=current_size, title="DrawPanel UI Example")
+showm.scene.add(drawing_canvas)
+showm.start()
