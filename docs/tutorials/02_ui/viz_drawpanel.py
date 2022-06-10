23059cf2417b83676771c7a7334f9cad7f76f851
Praneeth Shetty <praneethshetty10@gmail.com>
2022-06-10 20:09:45 +0530

    updating tutorial

--- docs/tutorials/02_ui/viz_drawpanel.py
+++ docs/tutorials/02_ui/viz_drawpanel.py
@@ -13,7 +13,7 @@ from fury import ui, window
 #########################################################################
 # We then create a DrawPanel Object.
 
-drawing_canvas = ui.DrawPanel()
+drawing_canvas = ui.DrawPanel(size=(550,550),position=(25,25))
 
 ###############################################################################
 # Show Manager
@@ -21,7 +21,7 @@ drawing_canvas = ui.DrawPanel()
 #
 # Now we add DrawPanel to the scene.
 
-current_size = (410, 410)
+current_size = (600, 600)
 showm = window.ShowManager(size=current_size, title="DrawPanel UI Example")
 showm.scene.add(drawing_canvas)
 showm.start()
