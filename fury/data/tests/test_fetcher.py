def111a9487b844f6ac17a384028b3c26972632b
xtanion <anandshivam54321@gmail.com>
2022-06-26 00:55:25 +0530

    added tests for `list_gltf_sample_models`

--- fury/data/tests/test_fetcher.py
+++ fury/data/tests/test_fetcher.py
@@ -1,14 +1,18 @@
 import os
 from os.path import join as pjoin
 import json
+from urllib.request import urlopen
 import numpy.testing as npt
 from aiohttp import InvalidURL
-from fury.data import (fetch_gltf, read_viz_gltf)
+from fury.data import (fetch_gltf, read_viz_gltf, list_gltf_sample_models)
 
 if 'FURY_HOME' in os.environ:
     fury_home = os.environ['FURY_HOME']
 else:
     fury_home = pjoin(os.path.expanduser('~'), '.fury')
+
+GLTF_DATA_URL = \
+    "https://api.github.com/repos/KhronosGroup/glTF-Sample-Models/contents/2.0/"  # noqa
 
 
 def tests_fetch_gltf():
@@ -34,8 +38,6 @@ def tests_fetch_gltf():
 
     filenames, path = fetch_gltf('Box', 'glTF-Binary')
     npt.assert_equal(len(filenames), 1)
-    print(filenames)
-    print(os.listdir(path))
     npt.assert_equal(os.listdir(path), filenames)
 
     filename = read_viz_gltf('Box', 'glTF-Binary')
@@ -46,3 +48,23 @@ def tests_fetch_gltf():
         gltf = json.loads(f.read())
     validate_gltf = gltf.get('asset')
     npt.assert_equal(validate_gltf['version'], str(2.0))
+
+
+def test_list_gltf_sample_models():
+    gltf_path = pjoin(fury_home, 'glTF')
+    list_json = pjoin(gltf_path, 'list.json')
+    if not os.path.exists(list_json):
+        json_data = urlopen(f'{GLTF_DATA_URL}').read()
+        with open(list_json, 'wb') as f:
+            f.write(json_data)
+
+    with open(list_json, 'r') as r:
+        data = json.load(r)
+    model_names = [model['name'] for model in data if model['size'] == 0]
+    fetch_names = list_gltf_sample_models()
+    npt.assert_equal(len(fetch_names), len(model_names))
+    npt.assert_array_equal(model_names, fetch_names)
+
+    default_list = ['BoxTextured', 'Duck', 'CesiumMilkTruck', 'CesiumMan']
+    result = [model in fetch_names for model in default_list]
+    npt.assert_equal(result, [True, True, True, True])
