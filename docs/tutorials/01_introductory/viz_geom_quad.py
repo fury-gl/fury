960c65a92bcf96ac0bc7bfc2af65931dd2d7c004
m-agour <mo.aggour@gmail.com>
2022-07-17 08:02:51 +0200

    Simplified the example

--- docs/tutorials/01_introductory/viz_geom_quad.py
+++ docs/tutorials/01_introductory/viz_geom_quad.py
@@ -1,24 +1,13 @@
-
 import numpy as np
 from fury import window, actor
 
-
-# centers = np.random.random([6000, 3]) * 300
-centers = np.array([[50, 50, 50], [0, 0, 75]])
-# colors = np.random.random([6000, 3])
-colors = np.random.random([2, 3])
+centers = np.random.random([500, 3]) * 100
+colors = np.random.random([500, 3])
 
 scene = window.Scene()
 showm = window.ShowManager(scene, size=(1000, 768))
 
-geom_squares = actor.geom_quad(centers, colors=colors)
+geom_squares = actor.billboard_gs(centers, colors=colors)
+scene.add(geom_squares)
 
-
-squares = actor.square(centers, colors=colors)
-# geom_a = actor.geom_actor(centers, colors)
-# prim_sphere_actor2 = actor.sphere(colors, colors=colors, radii=0.1)
-scene.add(geom_squares, squares)
-
-
-print(geom_squares)
 showm.start()
