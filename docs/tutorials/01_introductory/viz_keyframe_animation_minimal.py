23e8e9f5b3e8a7d6cb0ce6281cbe24eb35e03de2
m-agour <mo.aggour@gmail.com>
2022-07-18 09:37:12 +0200

    Modified tutorial

--- docs/tutorials/01_introductory/viz_keyframe_animation_minimal.py
+++ docs/tutorials/01_introductory/viz_keyframe_animation_minimal.py
@@ -20,14 +20,13 @@ showm.initialize()
 
 boxes = actor.arrow(np.array([[0, 0, 0]]), (0, 0, 0), (1, 0, 1), scales=6)
 
-
 # Creating a timeline to animate the actor
 timeline = Timeline(playback_panel=Timeline)
 # Adding the sphere actor to the timeline
 # This could've been done during initialization.
 timeline.add_actor(boxes)
 
-# Adding some keyframes
+# Adding some position keyframes
 timeline.set_position(0, np.array([0, 0, 0]))
 timeline.set_position(2, np.array([10, 10, 10]))
 timeline.set_position(5, np.array([-10, 16, 0]))
@@ -35,16 +34,20 @@ timeline.set_position(9, np.array([10, 0, 20]))
 
 # change the position interpolator to Cubic spline interpolator.
 timeline.set_position_interpolator(CubicSplineInterpolator)
+
+# Adding some rotation keyframes.
 timeline.set_rotation(0, np.array([160, 50, 20]))
 timeline.set_rotation(4, np.array([60, 160, 0]))
 timeline.set_rotation(8, np.array([0, -180, 90]))
 
+# Setting the rotation interpolator to Slerp
 timeline.set_rotation_interpolator(Slerp)
 
 # Main timeline to control all the timelines
 scene.camera().SetPosition(0, 0, 90)
 
 # Adding timelines to the main Timeline
+scene.add(timeline)
 
 
 # making a function to update the animation and render the scene
@@ -52,8 +55,6 @@ def timer_callback(_obj, _event):
     timeline.update_animation()
     showm.render()
 
-
-scene.add(timeline)
 
 # Adding the callback function that updates the animation
 showm.add_timer_callback(True, 10, timer_callback)
