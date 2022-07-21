6c072e7f49e7c2267e15e000e58de104835b2727
Serge Koudoro <skab12@gmail.com>
2022-07-21 23:27:23 +0200

    use loop

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -18,12 +18,31 @@ if [ "$COVERAGE" == "1" ] || [ "$COVERAGE" = true ]; then
     cp ../.coveragerc .;
     cp ../.codecov.yml .;
     # Run the tests and check for test coverage.
-    coverage run -m pytest -svv --verbose --durations=10 --pyargs fury   # Need to --doctest-modules flag
+    # coverage run -m pytest -svv --verbose --durations=10 --pyargs fury   # Need to --doctest-modules flag
+    for file in `find ../fury -name 'test_*.py' -print`;
+    do
+        coverage run -m -p pytest -svv $file;
+        retVal=$?
+        if [ $retVal -ne 0 ]; then
+            echo "THE CURRENT ERROR CODE IS $retVal";
+            error_code=1
+        fi
+    done
+    coverage combine .
     coverage report -m  # Generate test coverage report.
-    echo "START UPLOAD COVERAGE"
-    codecov    # Upload the report to codecov.
+    codecov  # Upload the report to codecov.
 else
-    pytest -svv --verbose --durations=10 --pyargs fury # Need to --doctest-modules flag
+    # Threads issue so we run test on individual file
+    # pytest -svv --verbose --durations=10 --pyargs fury # Need to --doctest-modules flag
+    for file in `find ../fury -name 'test_*.py' -print`;
+    do
+      pytest -svv $file;
+      retVal=$?
+      if [ $retVal -ne 0 ]; then
+      echo "THE CURRENT ERROR CODE IS $retVal";
+      error_code=1
+      fi
+    done
 fi
 
 set +ex
