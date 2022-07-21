cac1baba9df1f97637d68722430436e462525a74
Serge Koudoro <skab12@gmail.com>
2022-07-21 19:10:37 +0200

    fix install_dependencies syntax

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -14,18 +14,12 @@ echo "Install Dependencies"
 if [ "$INSTALL_TYPE" == "conda" ]; then
 	conda install -yq --name venv --file requirements/default.txt
     conda install -yq --name venv --file requirements/test.txt
-    if [[ "$DEPENDS" == "OPTIONAL_DEPS" ]]; then
-        conda install -yq --name venv --file requirements/optional.txt
-    fi
-    if [[ "${BUILD_DOCS}" == "1" ]]; then
-        conda install -yq --name venv --file requirements/docs.txt
-    fi
+    if [ "$DEPENDS" == "OPTIONAL_DEPS" ]; then conda install -yq --name venv --file requirements/optional.txt; fi
+    if [ "$BUILD_DOCS" == "1" ]; then conda install -yq --name venv --file requirements/docs.txt; fi
 else
     PIPI="pip install --timeout=60 "
 
-    if [ "$USE_PRE" == "1" ] || [ "$USE_PRE" = true ]; then
-        PIPI="$PIPI --extra-index-url=$PRE_WHEELS --pre";
-    fi
+    if [ "$USE_PRE" == "1" ] || [ "$USE_PRE" = true ]; then PIPI="$PIPI --extra-index-url=$PRE_WHEELS --pre"; fi
 
     $PIPI pytest
     $PIPI numpy
@@ -33,8 +27,8 @@ else
 	$PIPI -r requirements/default.txt
     $PIPI -r requirements/test.txt
 
-    if [[ "$DEPENDS" == "OPTIONAL_DEPS" ]]; then $PIPI -r requirements/optional.txt fi
-    if [[ "${BUILD_DOCS}" == "1" ]]; then $PIPI -r requirements/docs.txt fi
+    if [ "$DEPENDS" == "OPTIONAL_DEPS" ]; then $PIPI -r requirements/optional.txt; fi
+    if [ "$BUILD_DOCS" == "1" ]; then $PIPI -r requirements/docs.txt; fi
     if [ "$COVERAGE" == "1" ] || [ "$COVERAGE" = true ]; then pip install coverage coveralls codecov; fi
 fi
 
