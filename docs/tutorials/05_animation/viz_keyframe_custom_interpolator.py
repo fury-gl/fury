0e8bf0eb9006d48ab912c746f61219483b85864b
m-agour <mo.aggour@gmail.com>
2022-07-31 01:41:14 +0200

    updated tutorial

--- docs/tutorials/05_animation/viz_keyframe_custom_interpolator.py
+++ docs/tutorials/05_animation/viz_keyframe_custom_interpolator.py
@@ -9,16 +9,16 @@ Keyframe animation using custom interpolator.
 import numpy as np
 from fury import actor, window
 from fury.animation.timeline import Timeline
-
+from fury.animation import helpers
 
 ###############################################################################
 # Implementing a custom interpolator
 # ===============================================
 #
-# A keyframe interpolator must have a method `interpolate` which take the time
+# A keyframe interpolator function must return a function which take the time
 # as an argument and returns a value.
-# It's recommended to inherit from `Interpolator` class, which has some useful
-# methods dealing with timestamps.
+# It's recommended to import `fury.animation.helpers`, which has some useful
+# functions that would help to implement the interpolator.
 
 ###############################################################################
 # In glTF, animations using cubic spline interpolator needs at least two
@@ -38,18 +38,24 @@ from fury.animation.timeline import Timeline
 # >>>            (-2 * t3 + 3 * t2) * nextPoint +
 # >>>            (t3 - t2) * nextTangent
 #
-# First we create a class that must take a dict object that contains the
+# First we create a function that must take a dict object that contains the
 # animation keyframes when initialized as follows:
 #
-# >>> class cubic_spline_interpolator(Interpolator):
-# >>>     def __init__(self, keyframes):
-# >>>         super(cubic_spline_interpolator, self).__init__(keyframes)
+# >>> def tan_cubic_spline_interpolator(keyframes):
+# >>>     ...
+# >>>     def interpolate(t):
+# >>>           return interpolated_value
+# >>>     return interpolator
 #
 # Note: Also any other additional arguments are ok, see `spline_interpolator`
-# Second step is to implement the `interpolate` method (must have the name
-# `interpolate`) that only takes the current time as input.
+# Second step is to implement the `interpolate` closure that only takes the
+# current time as input.
+
 
 def tan_cubic_spline_interpolator(keyframes):
+    # First we must get ordered timestamps array:
+    timestamps = helpers.get_timestamps_from_keyframes(keyframes)
+
     # keyframes should be on the following form:
     # {
     # 1: {'value': ndarray, 'in_tangent': ndarray, 'out_tangent': ndarray},
@@ -68,36 +74,39 @@ def tan_cubic_spline_interpolator(keyframes):
             data['in_tangent'] = np.zeros_like(value)
 
     def interpolate(t):
-        # `get_neighbour_timestamps` method takes time as input and returns
-        # the surrounding timestamps.
-        t0, t1 = self.get_neighbour_timestamps(t)
+        # `get_previous_timestamp`and `get_next_timestamp` functions take
+        # timestamps array and current time as inputs and returns the
+        # surrounding timestamps.
+        t0 = helpers.get_previous_timestamp(timestamps, t)
+        t1 = helpers.get_next_timestamp(timestamps, t)
 
-        # `get_time_tau` static method takes current time and surrounding
+        # `get_time_tau` function takes current time and surrounding
         # timestamps and returns a value from 0 to 1
-        dt = self.get_time_tau(t, t0, t1)
+        dt = helpers.get_time_tau(t, t0, t1)
 
         time_delta = t1 - t0
 
         # to get a keyframe data at a specific timestamp, use
-        # `self.keyframes.get(t0)`. This keyframe data contains `value` and any
+        # `keyframes.get(t0)`. This keyframe data contains `value` and any
         # other data set as a custom argument using keyframe setters.
         # for example:
         # >>> timeline = Timeline()
         # >>> timeline.set_position(0, np.array([1, 1, 1]),
         # >>>                       custom_field=np.array([2, 3, 1]))
-        # In this case `self.keyframes.get(0)` would return:
+        # In this case `keyframes.get(0)` would return:
         # {'value': array(1, 1, 1), 'custom_field': array(2, 3, 1)}
         #
         # now we continue with the cubic spline equation.
-        p0 = self.keyframes.get(t0).get('value')
-        tan_0 = self.keyframes.get(t0).get('out_tangent') * time_delta
-        p1 = self.keyframes.get(t1).get('value')
-        tan_1 = self.keyframes.get(t1).get('in_tangent') * time_delta
+        p0 = keyframes.get(t0).get('value')
+        tan_0 = keyframes.get(t0).get('out_tangent') * time_delta
+        p1 = keyframes.get(t1).get('value')
+        tan_1 = keyframes.get(t1).get('in_tangent') * time_delta
         # cubic spline equation using tangents
         t2 = dt * dt
         t3 = t2 * dt
         return (2 * t3 - 3 * t2 + 1) * p0 + (t3 - 2 * t2 + dt) * tan_0 + (
                 -2 * t3 + 3 * t2) * p1 + (t3 - t2) * tan_1
+    return interpolate
 
 
 scene = window.Scene()
@@ -154,7 +163,7 @@ def timer_callback(_obj, _event):
 # Adding the callback function that updates the animation
 showm.add_timer_callback(True, 10, timer_callback)
 
-interactive = False
+interactive = 1
 
 if interactive:
     showm.start()
