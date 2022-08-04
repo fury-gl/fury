cef39614d0f6558544c15ae583a60458172cea8d
m-agour <mo.aggour@gmail.com>
2022-08-04 02:25:29 +0200

    Shouldn't reverse list

--- fury/animation/helpers.py
+++ fury/animation/helpers.py
@@ -44,7 +44,7 @@ def get_next_timestamp(timestamps, current_time, include_first=False):
     float or int
         The next timestamp
     """
-    for timestamp in timestamps[::-1] if include_first else timestamps[1:]:
+    for timestamp in timestamps[:] if include_first else timestamps[1:]:
         if timestamp > current_time:
             return timestamp
     return timestamps[-1]
