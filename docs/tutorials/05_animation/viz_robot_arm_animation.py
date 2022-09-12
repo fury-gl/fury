a416081083381c76bdef522a471964820ee4b154
Mohamed Agour <mo.aggour@gmail.com>
2022-09-12 02:52:27 +0200

    modified robot arm tutorial

--- docs/tutorials/05_animation/viz_robot_arm_animation.py
+++ docs/tutorials/05_animation/viz_robot_arm_animation.py
@@ -10,29 +10,13 @@ import numpy as np
 from fury import actor, window
 from fury.animation.timeline import Timeline
 from fury.lib import Actor
-from fury.utils import vertices_from_actor, update_actor
+from fury.utils import vertices_from_actor, update_actor, set_actor_origin
 
 scene = window.Scene()
 
 showm = window.ShowManager(scene,
                            size=(900, 768), reset_camera=False,
                            order_transparent=True)
-showm.initialize()
-
-
-def set_actor_center(actor, center):
-    """ Change the center of an actor to a custom position.
-
-    Parameters
-    ----------
-    actor: Actor
-        The actor object.
-    center: ndarray
-        The new center.
-    """
-    vertices = vertices_from_actor(actor)
-    vertices[:] -= center
-    update_actor(actor)
 
 
 ###############################################################################
@@ -53,9 +37,11 @@ end = actor.cone(np.array([[0, 0, 0]]), np.array([[1, 0, 0]]),
                  np.array([[1, 0, 0]]), heights=2.2, resolution=6)
 
 ###############################################################################
-# Setting the center of both shafts to the beginning.
-set_actor_center(main_arm, np.array([-6, 0, 0]))
-set_actor_center(sub_arm, np.array([-4, 0, 0]))
+# Setting the origin or rotation of both shafts to the beginning.
+# Length of main arm is 12 so the beginning would be at -6 in x direction.
+set_actor_origin(main_arm, np.array([-6, 0, 0]))
+# Length of the sub arm is 8 so the beginning would be at -4 in x direction.
+set_actor_origin(sub_arm, np.array([-4, 0, 0]))
 
 ###############################################################################
 # Creating a timeline to animate the actor
@@ -65,26 +51,12 @@ tl_grand_child = Timeline(end)
 
 ###############################################################################
 # Adding Timelines in hierarchical order
-tl_main.add(tl_child)
-tl_child.add(tl_grand_child)
 tl_main.add_child_timeline(tl_child)
 tl_child.add_child_timeline(tl_grand_child)
 
 
 ###############################################################################
 # Creating Arm joints time dependent animation functions.
-
-def main_pos(t):
-    return np.array([np.sin(t), np.cos(t) * np.sin(t), 0]) * 30
-
-
-def child_pos(t):
-    return np.array([np.cos(t * 20), np.cos(t * 20), np.cos(t * 20)]) * 3
-
-
-def grand_child_pos(t):
-    return np.array([np.sin(t * 20), 0, 0]) * 3
-
 
 def rot_main_arm(t):
     return np.array([np.sin(t/2) * 180, np.cos(t/2) * 180, 0])
@@ -100,10 +72,20 @@ def rot_drill(t):
 
 ###############################################################################
 # Setting timelines (joints) relative position
+# 1- Placing the main arm on the cube static base.
 tl_main.set_position(0, np.array([0, 1.3, 0]))
+
+###############################################################################
+# 2- Translating the timeline containing the sub arm to the end of the first
+# arm.
 tl_child.set_position(0, np.array([12, 0, 0]))
+
+###############################################################################
+# 3- Translating the timeline containing the drill to the end of the sub arm.
 tl_grand_child.set_position(0, np.array([8, 0, 0]))
 
+###############################################################################
+# Setting rotation time-based evaluators
 tl_main.set_rotation_interpolator(rot_main_arm, is_evaluator=True)
 tl_child.set_rotation_interpolator(rot_sub_arm, is_evaluator=True)
 tl_grand_child.set_rotation_interpolator(rot_drill, is_evaluator=True)
@@ -128,7 +110,7 @@ def timer_callback(_obj, _event):
 # Adding the callback function that updates the animation.
 showm.add_timer_callback(True, 10, timer_callback)
 
-interactive = 1
+interactive = False
 
 if interactive:
     showm.start()
