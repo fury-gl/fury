84c113ffcf6a1b7c3859ecec4b1b7153fe46d6e5
maharshigor <gor.maharshi@gmail.com>
2022-12-22 15:42:12 -0500

    final code shifts

--- docs/source/_static/js/popup.js
+++ docs/source/_static/js/popup.js
@@ -52,34 +52,34 @@ const TUTORIALS = {
         fileName: 'mathematics',
         title: 'mathematics',
         link: ''
-    },
-
+    }
 }
 
+// checking is mobile
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
 
-
-function createTutorialPopup(tutorial) {
-    return `<div id="tutorial-popup" class="tutorial-popup">
+function createTutorialPopup(tutorial, fromTop) {
+    return `<div id="tutorial-popup" class="tutorial-popup ${fromTop ? 'tutorial-popup-top': 'tutorial-popup-bottom'}">
         <div class="gallery-box-title">${tutorial.title}</div>
         <img src="_static/images/sc-dom/popup/${tutorial.fileName}.gif" alt="">
-    </div>`
+    </div>`;
 }
 
 // open a popup
 function showPopup(el, popupType = POPUP_TYPE.TUTORIAL) {
     
-    let popupBounds = calculatePositionForPopup(el.getBoundingClientRect())
+    let popupBounds = calculatePositionForPopup(el.getBoundingClientRect());
 
     // Injecting the element
     if (popupType === POPUP_TYPE.TUTORIAL) {
         
         // Checking for already active popup to make sure only one popup present
         if (ACTIVE_POPUP.TUTORIAL) {
-            removePopup()
+            removePopup();
         }
         
-        $('.scientific-domains').append(createTutorialPopup(TUTORIALS[el.id]));
-        ACTIVE_POPUP.TUTORIAL = true
+        $('.scientific-domains').append(createTutorialPopup(TUTORIALS[el.id], popupBounds.fromTop));
+        ACTIVE_POPUP.TUTORIAL = true;
 
         // Adding position
         $('#tutorial-popup').css({
@@ -90,7 +90,7 @@ function showPopup(el, popupType = POPUP_TYPE.TUTORIAL) {
 
         // Close the popup when exited from the popup
         $('#tutorial-popup').hover(() => {}, function () {
-            removePopup()
+            removePopup();
         });
         
     }
@@ -100,39 +100,44 @@ function calculatePositionForPopup(elBounds, popupWidth = 350, popupHeight = 300
 
     let popupBounds = {
         height: popupHeight,
-        width: popupWidth
+        width: popupWidth,
+        fromTop: true
     }
 
     if (elBounds.top < 308) {
         // Open the popup on bottom
+        popupBounds.top = elBounds.bottom + 8;
+        popupBounds.bottom = elBounds.bottom + popupHeight + 8;
+        popupBounds.fromTop = false;
     } else {
-        popupBounds.top = elBounds.top - popupHeight - 8
-        popupBounds.bottom = elBounds.top - 8
+        popupBounds.top = elBounds.top - popupHeight - 8;
+        popupBounds.bottom = elBounds.top - 8;
     }
 
-    popupBounds.left = elBounds.left
-    popupBounds.right = elBounds.left + popupWidth
+    popupBounds.left = elBounds.left;
+    popupBounds.right = elBounds.left + popupWidth;
 
-
-    return popupBounds
+    return popupBounds;
 }
 
 // remove the popup
 function removePopup(popupType = POPUP_TYPE.TUTORIAL) {
     $(`#${popupType}-popup`).remove();
-    ACTIVE_POPUP.TUTORIAL = false
+    ACTIVE_POPUP.TUTORIAL = false;
 }
 
 // Open the popup on cell-layout hover
 $('img.cell-layout').hover(function () {
         // over,
-        showPopup(this);
+        if (!isMobile) {
+            showPopup(this);
+        }
     }, () => {}
 );
 
 // Close the popup when exited from the popup
 $('#tutorial-popup').hover(() => {}, function () {
-        removePopup()
+        removePopup();
     }
 );
 
@@ -141,7 +146,7 @@ $(document).click(function (e) {
     e.preventDefault();
     // We can append more of these to close the popups
     if($(e.target).closest('#tutorial-popup').length == 0) {
-        removePopup()
+        removePopup();
     }
 });
 
