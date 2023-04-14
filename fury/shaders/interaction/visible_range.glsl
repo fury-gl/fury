f15ecf567e540a5ba1f71c9e73795fa519a766b2
Javier Guaje <jrguajeg@iu.edu>
2023-04-14 18:21:04 -0400

    Changed center type to int.

--- fury/shaders/interaction/visible_range.glsl
+++ fury/shaders/interaction/visible_range.glsl
@@ -1,4 +1,4 @@
-bool inVisibleRange(vec3 center)
+bool inVisibleRange(ivec3 center)
 {
     bool xVal = lowRanges.x <= center.x && center.x <= highRanges.x;
     bool yVal = lowRanges.y <= center.y && center.y <= highRanges.y;
