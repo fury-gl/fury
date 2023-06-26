ceb076fbcddf8ae615c24802e7fe946c53d115e9
Joao Victor Dell Agli <joaovdafloriano@usp.br>
2023-06-25 23:10:56 -0300

    Render structuring and cleanup

--- docs/experimental/framebuffer_example.py
+++ docs/experimental/framebuffer_example.py
@@ -83,81 +83,30 @@ billboard_vert_impl =  """/* Billboard  vertex shader implementation */
                         out_tex = in_tex;
                         gl_Position = MCDCMatrix * vec4(vertexPositionMC, 1.);"""
 
-billboard_frag =   """#ifdef GL_FRAGMENT_PRECISION_MEDIUM 
-                        precision mediump float;
-                    #else
-                        precision lowp float;
-                        precision lowp int;
-                    #endif
-                    
+billboard_frag_decl ="""                   
                     varying vec2 out_tex;
                     
-                    uniform float t;
-                    uniform vec2 res;
-                    uniform sampler2D textureSampler;
+                    uniform sampler2D screenTexture;"""
                     
                     
-                    void main(){
-                    
-                        vec3 color = vec3(gl_FragCoord.xy/res, 1.0);
-                        vec4 texture = texture(textureSampler, out_tex);
-                    
-                        // gl_FragColor = vec4(color, 1.0);\
-                        gl_FragColor = vec4(exp(-color.x*color.x), exp(-color.y*color.y), exp(-color.z*color.z), 1.0);\
-                        // gl_FragColor = vec4(abs(sin(t)), abs(cos(t)), 1.0, 1.0);\
-                    
-                    
-                    }"""
-
-vert_decl = """/* Billboard  vertex shader declaration */ 
-            in vec3 center; 
-            in vec2 in_tex; 
-            out vec3 centerVertexMCVSOutput; 
-            out vec3 normalizedVertexMCVSOutput; 
-            varying vec2 out_tex;"""
-
-vert_impl = """/* Billboard  vertex shader implementation */ 
-            centerVertexMCVSOutput = center; 
-            normalizedVertexMCVSOutput = vertexMC.xyz - center; // 1st Norm. [-scale, scale] 
-            float scalingFactor = 1. / abs(normalizedVertexMCVSOutput.x); 
-            float size = abs(normalizedVertexMCVSOutput.x) * 2; 
-            normalizedVertexMCVSOutput *= scalingFactor; // 2nd Norm. [-1, 1] 
-            vec2 billboardSize = vec2(size, size); // Fixes the scaling issue 
-            vec3 cameraRightMC = vec3(MCVCMatrix[0][0], MCVCMatrix[1][0], MCVCMatrix[2][0]); 
-            vec3 cameraUpMC = vec3(MCVCMatrix[0][1], MCVCMatrix[1][1], MCVCMatrix[2][1]); 
-            vec3 vertexPositionMC = center + 
-                cameraRightMC * billboardSize.x * normalizedVertexMCVSOutput.x + 
-                cameraUpMC * billboardSize.y * normalizedVertexMCVSOutput.y; 
-            out_tex = in_tex; 
-            gl_Position = MCDCMatrix * vec4(vertexPositionMC, 1.);"""
+billboard_frag_impl ="""                   
+                        vec3 texture = texture(screenTexture, out_tex);
+                        vec3 color = vec3(1.0, 0.0, 0.0);
+                        
+                        fragOutput0 = vec4(color, 1.0);"""
 
 
-frag = """#ifdef GL_FRAGMENT_PRECISION_MEDIUM 
-    precision mediump float; 
-#else 
-    precision lowp float; 
-    precision lowp int; 
-#endif 
+frag_decl = """varying vec2 out_tex; 
+               uniform vec2 res0;  
+               uniform vec3 point0;"""
 
-varying vec2 out_tex; 
-uniform float t; 
-uniform vec2 res; 
-layout(binding = 0) uniform sampler2D textureSampler; 
-uniform vec3 point; 
+kde_func = """float kde(vec3 point, vec3 coord, float sigma){ 
+                return exp(-pow(length(abs(point - coord)), 2.0)/(2.0*sigma*sigma)); 
+              } """
 
-float kde(vec3 point, vec3 coord, float sigma){ 
-    return exp(-pow(length(abs(point - coord)), 2.0)/(2.0*sigma*sigma)); 
-} 
-
-void main(){ 
-    
-    vec3 color = vec3(gl_FragCoord.xy/res, 1.0); 
-    vec3 texture = texture(textureSampler, out_tex); 
-    vec3 k = vec3(1.0, kde(point, vec3(gl_FragCoord.xy/res, 0.0), 10.0), 1.0); 
-    
-    // gl_FragColor = vec4(color, 1.0); 
-    gl_FragColor = vec4((k + texture)*0.5, 1.0); 
-    // gl_FragColor = vec4(abs(sin(t)), abs(cos(t)), 1.0, 1.0);}"""
+frag_impl = """
+               vec3 k = vec3(kde(point0, vec3(gl_FragCoord.xy/res0, 0.0), 10.0)); 
+               fragOutput0 = vec4(k, 0.5);"""
 
 
 width, height = (1920, 1080)
@@ -188,13 +137,17 @@ scale = np.array([[width, height, 0.0]])
 
 # Actor setup
 billboard = actor.billboard(np.array([[0.0, 0.0, 0.0]]), (1.0, 0.0, 0.0), scales=scale)
+
+frag_decl = shaders.compose_shader((frag_decl, kde_func))
+
+# Adding first the kde rendering shaders
 actor.shader_to_actor(billboard, "vertex", billboard_vert_impl, billboard_vert_decl)
-actor.replace_shader_in_actor(billboard, "fragment", billboard_frag)
-shader_custom_uniforms(billboard, "fragment").SetUniform2f("res", [width, height])
+actor.shader_to_actor(billboard, "fragment", frag_impl, frag_decl)
 
 # billboard_tex = np.array([[0.0, 0.0], [0.0, 1.0], [1.0, 1.0], [1.0, 0.0]])
 billboard_tex = np.array([[-1.0, -1.0], [-1.0, 1.0], [1.0, 1.0], [1.0, -1.0]])
 actor.attribute_to_actor(billboard, billboard_tex, "in_tex")
+
 
 scene.GetRenderWindow().DebugOn()
 scene.GetRenderWindow().GlobalWarningDisplayOn()
@@ -226,21 +179,23 @@ points = np.random.rand(10, 3)
 
 FBO = vtk.vtkOpenGLFramebufferObject()
 
-
 manager.window.SetOffScreenRendering(True)
-manager.initialize()
-# manager.start()
+manager.initialize() # sets everything for rendering
 
 FBO.DebugOn()
 FBO.GlobalWarningDisplayOn()
 
 FBO.SetContext(manager.window) # Sets the context for the FBO. 
-FBO.PopulateFramebuffer(width, height, True, 1, vtk.VTK_UNSIGNED_CHAR, False, 24, 0)
+FBO.PopulateFramebuffer(width, height, True, 1, vtk.VTK_FLOAT, False, 24, 0)
+
 
 # Checking FBO status
 print("FBO of index:", FBO.GetFBOIndex()) 
 print("Number of color attachments:", FBO.GetNumberOfColorAttachments())
 
+
+
+# RENDER TIME
 
 # Below, how to render things with a FBO according to VTK's website
 # FBO.SaveCurrentBindingsAndBuffers()
@@ -250,3 +205,38 @@ print("Number of color attachments:", FBO.GetNumberOfColorAttachments())
 # FBO.Start(width, height)
 # # Render
 # FBO.RestorePreviousBindingsAndBuffers()
+
+shaders.shader_apply_effects(manager.window, billboard, window.gl_disable_depth)
+shaders.shader_apply_effects(manager.window, billboard, window.gl_set_normal_blending)
+
+
+FBO.SaveCurrentBindingsAndBuffers()
+FBO.Bind()
+FBO.ActivateBuffer(0)
+
+print("FBO Start:", FBO.Start(width, height))
+
+# Render every point existing inside loop
+for i in range(n_points):
+    shader_custom_uniforms(billboard, "fragment").SetUniform3f("point", points[i, :])
+    shader_custom_uniforms(billboard, "fragment").SetUniform2f("res", [width, height])
+    manager.render()
+FBO.RestorePreviousBindingsAndBuffers()
+
+# Below, testing to see if the prior rendering works by downloading the color attachment
+color_texture = FBO.GetColorAttachmentAsTextureObject() # Gets the color texture for further rendering
+buffer = color_texture.Download().MapBuffer()
+print("Buffer")
+print(buffer)
+print()
+
+# WIP below
+manager.window.SetOffScreenRendering(False) # Then, after rendering everything in the offscreen FBO, time to render it to a simple billboard
+actor.shader_to_actor(billboard, "fragment", billboard_frag_impl, billboard_frag_decl) # attatch simple billboard texture rendering shaders
+
+shaders.shader_apply_effects(manager.window, billboard, window.gl_disable_blend)
+
+interactive = False
+
+if interactive:
+    manager.start()
