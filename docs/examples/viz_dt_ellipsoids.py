adc83421deb075c55479e0d9abe4a7dc7d571890
Tania Castillo <31288525+tvcastillod@users.noreply.github.com>
2023-07-24 02:09:03 -0500

    updated second part of tutorial

--- docs/examples/viz_dt_ellipsoids.py
+++ docs/examples/viz_dt_ellipsoids.py
@@ -3,9 +3,10 @@
 Display Tensor Ellipsoids for DTI using tensor_slicer vs ellipsoid actor
 ===============================================================================
 This tutorial is intended to show two ways of displaying diffusion tensor
-ellipsoids for DTI visualization. The first is using the usual tensor_slicer
-that allows us to slice many tensors as ellipsoids. The second is the generic
-ellipsoid actor that can be used to display different amount of ellipsoids.
+ellipsoids for DTI visualization. The first is using the usual
+``tensor_slicer`` that allows us to slice many tensors as ellipsoids. The
+second is the generic ``ellipsoid`` actor that can be used to display different
+amount of ellipsoids.
 
 We start by importing the necessary modules:
 """
@@ -48,6 +49,7 @@ whole_brain_evals, _ = load_nifti(read_viz_dmri('whole_brain_evals.nii.gz'))
 # and 724 vertices.
 
 vertices, faces = prim_sphere('repulsion100', True)
+
 
 ###############################################################################
 # As we need to provide a sphere object we create a class Sphere to which we
@@ -100,32 +102,40 @@ window.record(scene)
 
 scene.clear()
 
+
 ###############################################################################
 # Using ellipsoid actor
 # =====================
-# In order to use the ellipsoid actor to display the same tensor slice we need
-# to set additional parameters. First, we define the centers which corresponds
-# to ellipsoids positions.
+# In order to use the ``ellipsoid`` actor to display the same tensor slice we
+# need to set additional parameters. For this purpose, we define a helper
+# function to facilitate the correct setting of the parameters before passing
+# them to the actor.
 
-valid_mask = np.abs(slice_evecs).max(axis=(-2, -1)) > 0
-indices = np.nonzero(valid_mask)
-centers = np.asarray(indices).T
+def get_params(evecs, evals):
+    # We define the centers which corresponds to the ellipsoids positions.
+    valid_mask = np.abs(evecs).max(axis=(-2, -1)) > 0
+    indices = np.nonzero(valid_mask)
+    centers = np.asarray(indices).T
+
+    # We need to pass the data of the axes and lengths of the ellipsoid as a
+    # ndarray, so it is necessary to rearrange the data of the eigenvectors and
+    # eigenvalues.
+    fevecs = evecs[indices]
+    fevals = evals[indices]
+
+    # We need to define the colors of the ellipsoids following the default
+    # coloring in tensor_slicer that is uses _color_fa that is a way to map
+    # colors to each tensor based on the fractional anisotropy (FA) of each
+    # diffusion tensor.
+    colors = _color_fa(_fa(fevals), fevecs)
+
+    return centers, fevecs, fevals, colors
 
 ###############################################################################
-# Second, we need to pass the data of the axes and lengths of the ellipsoid as
-# a ndarray, so it is necessary to rearrange the data of the eigenvectors and
-# eigenvalues.
+# With this we now have the values we need to define the centers, axes,
+# lengths, and colors of the ellipsoids.
 
-evecs = slice_evecs[indices]
-evals = slice_evals[indices]
-
-###############################################################################
-# Third, we need to define the colors of the ellipsoids following the default
-# coloring in ``tensor_slicer`` that is using ``_color_fa`` that is a way to
-# map colors to each tensor based on the fractional anisotropy (FA) of each
-# diffusion tensor.
-
-colors = _color_fa(_fa(evals), evecs)
+centers, evecs, evals, colors = get_params(slice_evecs, slice_evals)
 
 ###############################################################################
 # Now, we can use the ``ellipsoid`` actor to create the tensor ellipsoids as
@@ -145,10 +155,10 @@ scene.clear()
 
 ###############################################################################
 # Thus, one can see that the same result is obtained, however there is a
-# difference in the visual quality and this is because the ellipsoid actor uses
-# raymarching technique, so the objects that are generated are smoother since
-# they are not made with polygons but defined by an SDF function. Next we can
-# see in more detail the tensor ellipsoids generated.
+# difference in the visual quality and this is because the ``ellipsoid`` actor
+# uses raymarching technique, so the objects that are generated are smoother
+# since they are not made with polygons but defined by an SDF function. Next we
+# can see in more detail the tensor ellipsoids generated.
 #
 
 ###############################################################################
@@ -165,68 +175,53 @@ data_shape = roi_evals.shape[:3]
 tensor_roi.display_extent(
     0, data_shape[0], 0, data_shape[1], 0, data_shape[2])
 
-scene = window.Scene()
 scene.add(tensor_roi)
+scene.azimuth(87)
 
 if interactive:
     window.show(scene)
 
+window.record(scene, size=(600, 600), out_path='tensor_roi_100.png')
+window.record(scene)
+
 scene.clear()
 
+###############################################################################
 # We can do it also with a sphere of 200 vertices, but if we try to do it with
 # one of 724 the visualization can no longer be rendered. In contrast, we can
-# visualize the ROI with the ellipsoid actor without compromising the quality
-# of the visualization.
+# visualize the ROI with the ``ellipsoid`` actor without compromising the
+# quality of the visualization.
 
-'''
-valid_mask = np.abs(roi_evecs).max(axis=(-2, -1)) > 0
-indices = np.nonzero(valid_mask)
+centers, evecs, evals, colors = get_params(roi_evecs, roi_evals)
 
-centers = np.asarray(indices).T
-
-num_centers = centers.shape[0]
-
-dofs_vecs = roi_evecs[indices]
-dofs_vals = roi_evals[indices]
-
-colors = [_color_fa(_fa(dofs_vals[i, :]), dofs_vecs[i, ...])
-          for i in range(num_centers)]
-colors = np.asarray(colors)
-
-tensors = actor.ellipsoid(
-    centers, colors=colors, axes=dofs_vecs, lengths=dofs_vals, scales=.6)
+tensors = actor.ellipsoid(centers=centers, colors=colors, axes=evecs,
+                          lengths=evals, scales=.6)
 scene.add(tensors)
 
 if interactive:
     window.show(scene)
 
-scene.clear()
-'''
+window.record(scene, size=(600, 600), out_path='tensor_roi_sdf.png')
+window.record(scene)
 
+scene.clear()
+
+###############################################################################
 # In fact, although with a low performance, this actor allows us to visualize
 # the whole brain, which contains a much larger amount of data.
 
-'''
-valid_mask = np.abs(whole_brain_evecs).max(axis=(-2, -1)) > 0
-indices = np.nonzero(valid_mask)
+centers, evecs, evals, colors = get_params(whole_brain_evecs,
+                                           whole_brain_evals)
 
-centers = np.asarray(indices).T
-
-num_centers = centers.shape[0]
-
-dofs_vecs = whole_brain_evecs[indices]
-dofs_vals = whole_brain_evals[indices]
-
-colors = [_color_fa(_fa(dofs_vals[i, :]), dofs_vecs[i, ...])
-          for i in range(num_centers)]
-colors = np.asarray(colors)
-
-tensors = actor.ellipsoid(
-    centers, colors=colors, axes=dofs_vecs, lengths=dofs_vals, scales=.6)
+tensors = actor.ellipsoid(centers=centers, colors=colors, axes=evecs,
+                          lengths=evals, scales=.6)
 scene.add(tensors)
+scene.azimuth(-89)
 
 if interactive:
     window.show(scene)
 
+window.record(scene, size=(600, 600), out_path='tensor_whole_brain_sdf.png')
+window.record(scene)
+
 scene.clear()
-'''
