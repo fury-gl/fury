18bda117a21c887718f6933bc8d6b618ff7f0b3c
Joao Victor Dell Agli <joaovdafloriano@usp.br>
2023-08-10 16:49:18 -0300

    feat: Hardcoded fullscreen billboard

--- docs/experimental/viz_billboards.py
+++ docs/experimental/viz_billboards.py
@@ -4,9 +4,10 @@ from fury.actor import billboard
 from fury.window import Scene, ShowManager
 
 width, height = (1350, 800)
+cam_pos = np.array([0.0, 0.0, -1.0])
 
 scene = Scene()
-scene.set_camera(position=(-6, 5, -10),
+scene.set_camera(position=(cam_pos[0], cam_pos[1], cam_pos[2]),
                  focal_point=(0.0,
                               0.0,
                               0.0),
@@ -20,9 +21,27 @@ manager = ShowManager(
 
 manager.initialize()
 
-scale = 3.4*np.array([[width/height, 1.0, 0.0]])
+center = np.array([[0.0, 0.0, 20.0]])
 
-bill = billboard(np.array([[0.0, 0.0, 0.0]]), scales=scale,colors = (1.0, 0.0, 0.0))
+
+#hardcoding the billboard to be fullscreen and centered
+scale_factor_2 = np.abs(np.linalg.norm(center[0] - cam_pos))*np.sin(np.deg2rad(scene.camera().GetViewAngle()/2.0))
+print(scale_factor_2)
+
+scale = scale_factor_2*np.array([[width/height, 1.0, 0.0]])
+
+bill = billboard(center, scales=scale, colors = (1.0, 0.0, 0.0))
+
+def callback(obj = None, event = None):
+    pos, fp, vu = manager.scene.get_camera()
+    scale_factor_2 = np.abs(np.linalg.norm(center[0] - np.array(pos)))*np.sin(np.deg2rad(scene.camera().GetViewAngle()/2.0))
+    scale = scale_factor_2*np.array([[width/height, 1.0, 0.0]])
+    bill.SetScale(scale[0, 0], scale[0, 1], scale[0, 2])
+    bill.Modified()
+
+callback()
+
 manager.scene.add(bill)
+manager.add_iren_callback(callback, "RenderEvent")
 
 manager.start()
