1f455c0cc5d61e218e23ce6dc22087d9421bef4b
Javier Guaje <jrguajeg@iu.edu>
2023-10-31 19:37:30 -0400

    Restored texture_coefficients.py

--- docs/experimental/SH-ODF experimental/texture_coefficients.py
+++ docs/experimental/SH-ODF experimental/texture_coefficients.py
@@ -1,20 +1,14 @@
 """
-This script includes TEXTURE experimentation for passing SH coefficients
+This spript includes TEXTURE experimentation for passing SH coeffients
 """
+import numpy as np
 import os
 
-import numpy as np
-from dipy.data import get_sphere
-from dipy.reconst.shm import sh_to_sf
+from fury.lib import Texture, FloatArray
 
 from fury import actor, window
-from fury.lib import FloatArray, Texture
-from fury.shaders import (
-    attribute_to_actor,
-    compose_shader,
-    import_fury_shader,
-    shader_to_actor,
-)
+from fury.shaders import (attribute_to_actor, compose_shader,
+                          import_fury_shader, shader_to_actor)
 from fury.utils import numpy_to_vtk_image_data, set_polydata_tcoords
 
 
@@ -67,15 +61,11 @@ if __name__ == '__main__':
 
     uv_vals = np.array(
         [
-            [0, 2 / 3], [0, 1], [1, 1], [1, 2 / 3],
-            [0, 2 / 3], [0, 1], [1, 1], [1, 2 / 3],  # glyph1
-            [0, 1 / 3], [0, 2 / 3], [1, 2 / 3], [1, 1 / 3],
-            [0, 1 / 3], [0, 2 / 3], [1, 2 / 3], [1, 1 / 3],  # glyph2
-            [0, 0], [0, 1 / 3], [1, 1 / 3], [1, 0],
-            [0, 0], [0, 1 / 3], [1, 1 / 3], [1, 0]  # glyph3
+            [0, 2 / 3], [0, 1], [1, 1], [1, 2 / 3], [0, 2 / 3], [0, 1], [1, 1], [1, 2 / 3], #glyph1
+            [0, 1 / 3], [0, 2 / 3], [1, 2 / 3], [1, 1 / 3], [0, 1 / 3], [0, 2 / 3], [1, 2 / 3], [1, 1 / 3], #glyph2
+            [0, 0], [0, 1 / 3], [1, 1 / 3], [1, 0], [0, 0], [0, 1 / 3], [1, 1 / 3], [1, 0], #glyph3
         ]
     )
-    # fmt: on
 
     num_pnts = uv_vals.shape[0]
 
@@ -84,7 +74,7 @@ if __name__ == '__main__':
     t_coords.SetNumberOfTuples(num_pnts)
     [t_coords.SetTuple(i, uv_vals[i]) for i in range(num_pnts)]
 
-    set_polydata_tcoords(odf_actor_pd, t_coords)
+    set_polydata_tcoords(actor_box, t_coords)
 
     arr = (
             np.array(
@@ -170,52 +160,58 @@ if __name__ == '__main__':
         {
             float p0 = 0., p1 = 0., p2 = 0.;
 
-        for (int k = l; k >= 0; k--)
+            for (int k = l; k >= 0; k--)
+            {
+                float k1 = float(k + 1);
+                float m1 = float(2 * m) + k1;
+                float m2 = float(2 * (m + k) + 1);
+
+                p2 = p1;
+                p1 = p0;
+
+                p0 = 0.;
+                if (l == m + k)
+                    p0 = 1.;
+
+                float u0 = sqrt(
+                    (m2 * (m2 + 2.0)) /
+                    (k1 * m1)
+                );
+
+                float u1 = sqrt(
+                    (k1 * m1 * (m2 + 4.0)) / 
+                    ((k1 + 1.0) * (m1 + 1.0) * m2)
+                );
+
+                p0 += p1 * u0 * x;
+                p0 -= u1 * p2;
+            }
+
+            for (int k = 1; k <= m; k++)
+            {
+                p0 *= sqrt(
+                    (1.0 - 0.5/float(k)) * (1.0 - x) * (1.0 + x)
+                );
+            }
+
+            p0 *= sqrt((0.5 * float(m) + 0.25)/PI);
+
+            return p0;
+        }
+
+        float SH( in int l, in int m, in vec3 s ) 
         {
-            float k1 = float(k + 1);
-            float m1 = float(2 * m) + k1;
-            float m2 = float(2 * (m + k) + 1);
+            vec3 ns = normalize(s);
 
-            p2 = p1;
-            p1 = p0;
-            p0 = 0;
+            if (m < 0) {
+                m = -m;
+            }
 
-            if (l == m + k)
-                p0 = 1;
+            // spherical coordinates
+            float thetax = ns.y;
+            float phi = atan(ns.z, ns.x)+PI/2.;
 
-            float u0 = sqrt((m2 * (m2 + 2)) / (k1 * m1));
-
-            float u1 = sqrt((k1 * m1 * (m2 + 4)) / ((k1 + 1) * (m1 + 1) * m2));
-
-            p0 += p1 * u0 * x;
-            p0 -= u1 * p2;
-        }
-
-        for (int k = 1; k <= m; k++)
-        {
-            p0 *= sqrt((1 - .5 / float(k)) * (1 - x) * (1 + x));
-        }
-
-        p0 *= sqrt((.5 * float(m) + .25) / PI);
-
-        return p0;
-    }
-    """
-
-    spherical_harmonics = """
-    float SH(in int l, in int m, in vec3 s)
-    {
-        vec3 ns = normalize(s);
-
-        if (m < 0) {
-            m = -m;
-        }
-
-        // spherical coordinates
-        float thetax = ns.y;
-        float phi = atan(ns.z, ns.x) + PI / 2.;
-
-        float pl = Pgn(l, m, thetax);
+            float pl = Pgn(l, m, thetax);
 
             float r = pow(-1.0, float(m)) * cos(float(m) * phi) * pl;
             if (m != 0) {
@@ -223,7 +219,7 @@ if __name__ == '__main__':
             }
             return r;
         }
-
+        
         float coef_norm( in float coef)
         {
             float min = 0;
@@ -241,59 +237,59 @@ if __name__ == '__main__':
             p = p - centerMCVSOutput;
             vec3 p00 = p;
 
-        float r, d; vec3 n, s, res;
+            float r, d; vec3 n, s, res;
 
             #define SHAPE (vec3(d-abs(r), sign(r),d))
             //#define SHAPE (vec3(d-0.35, -1.0+2.0*clamp(0.5 + 16.0*r,0.0,1.0),d))
             d=length(p00);
-            n=p00/d;
+            n=p00/d;     
             // ================================================================
             float i = 1/(k*2);
             float c = texture(texture0, vec2(i, tcoordVCVSOutput.y)).x;
             r = coef_norm(c)*SH(0, 0, n);
-
+            
             c = texture(texture0, vec2(i+1/k, tcoordVCVSOutput.y)).x;
             r += coef_norm(c)*SH(2, -2, n);
-
+            
             c = texture(texture0, vec2(i+2/k, tcoordVCVSOutput.y)).x;
             r += coef_norm(c)*SH(2, -1, n);
-
+            
             c = texture(texture0, vec2(i+3/k, tcoordVCVSOutput.y)).x;
             r += coef_norm(c)*SH(2, 0, n);
-
+            
             c = texture(texture0, vec2(i+4/k, tcoordVCVSOutput.y)).x;
             r += coef_norm(c)*SH(2, 1, n);
-
+            
             c = texture(texture0, vec2(i+5/k, tcoordVCVSOutput.y)).x;
             r += coef_norm(c)*SH(2, 2, n);
-
+            
             c = texture(texture0, vec2(i+6/k, tcoordVCVSOutput.y)).x;
             r += coef_norm(c)*SH(4, -4, n);
-
+            
             c = texture(texture0, vec2(i+7/k, tcoordVCVSOutput.y)).x;
             r += coef_norm(c)*SH(4, -3, n);
-
+            
             c = texture(texture0, vec2(i+8/k, tcoordVCVSOutput.y)).x;
             r += coef_norm(c)*SH(4, -2, n);
-
+            
             c = texture(texture0, vec2(i+9/k, tcoordVCVSOutput.y)).x;
             r += coef_norm(c)*SH(4, -1, n);
-
+            
             c = texture(texture0, vec2(i+10/k, tcoordVCVSOutput.y)).x;
             r += coef_norm(c)*SH(4, 0, n);
-
+            
             c = texture(texture0, vec2(i+11/k, tcoordVCVSOutput.y)).x;
             r += coef_norm(c)*SH(4, 1, n);
-
+            
             c = texture(texture0, vec2(i+12/k, tcoordVCVSOutput.y)).x;
             r += coef_norm(c)*SH(4, 2, n);
-
+            
             c = texture(texture0, vec2(i+13/k, tcoordVCVSOutput.y)).x;
             r += coef_norm(c)*SH(4, 3, n);
-
+            
             c = texture(texture0, vec2(i+14/k, tcoordVCVSOutput.y)).x;
             r += coef_norm(c)*SH(4, 4, n);
-
+            
             r *= scaleVSOutput;
             // ================================================================
             s = SHAPE; res = s;
@@ -353,8 +349,8 @@ if __name__ == '__main__':
     coeffs_1 = \
         """
         float coeffs[15] = float[15](0.2820735, 0.15236554, -0.04038717,
-        -0.11270988, -0.04532376, 0.14921817, 0.00257928,
-        0.0040734, -0.05313807, 0.03486542, 0.04083064, 0.02105767,
+        -0.11270988, -0.04532376, 0.14921817, 0.00257928, 
+        0.0040734, -0.05313807, 0.03486542, 0.04083064, 0.02105767, 
         -0.04389586, -0.04302812, 0.1048641);
         """
 
@@ -397,53 +393,46 @@ if __name__ == '__main__':
             }
         """
 
-    central_diffs_normals = """
-    vec3 centralDiffsNormals(in vec3 pos)
-    {
-        //vec2 e = vec2(1.0,-1.0)*0.5773*0.0005;
-        vec2 e = vec2(.001, -1);
-        return normalize(
-            e.xyy * map(pos + e.xyy).x + e.yyx * map(pos + e.yyx).x +
-            e.yxy * map(pos + e.yxy).x + e.xxx * map(pos + e.xxx).x );
-    }
-    """
-
-    """
-    central_diffs_normals = import_fury_shader(
-        os.path.join("sdf", "central_diffs.frag")
-    )
-    """
-
-    cast_ray = """
-    vec3 castRay(in vec3 ro, vec3 rd)
-    {
-        vec3 res = vec3(1e10, -1, 1);
-
-        float maxd = 4;
-        float h = 1;
-        float t = 0;
-        vec2  m = vec2(-1);
-
-        for(int i = 0; i < 2000; i++)
+    central_diffs_normal = \
+        """
+        vec3 centralDiffsNormals(in vec3 pos)
         {
-            if(h < .01 || t > maxd)
-                break;
-            vec3 res = map(ro + rd * t);
-            h = res.x;
-            m = res.yz;
-            t += h * .1;
+            //vec2 e = vec2(1.0,-1.0)*0.5773*0.0005;
+            vec2 e = vec2(0.001, -1.0);
+            return normalize( e.xyy*map( pos + e.xyy ).x +
+                              e.yyx*map( pos + e.yyx ).x +
+                              e.yxy*map( pos + e.yxy ).x +
+                              e.xxx*map( pos + e.xxx ).x );
+        }
+        """
+
+    cast_ray = \
+        """
+        vec3 castRay(in vec3 ro, vec3 rd)
+        {
+            vec3 res = vec3(1e10,-1.0, 1.0);
+
+            float maxd = 4.0;
+            float h = 1.0;
+            float t = 0.0;
+            vec2  m = vec2(-1.0);
+            for( int i=0; i<2000; i++ )
+            {
+                if( h<0.01||t>maxd ) break;
+                vec3 res = map( ro+rd*t );
+                h = res.x;
+                m = res.yz;
+                t += h*0.1;
+            }
+            if( t<maxd && t<res.x ) res=vec3(t,m);
+
+            return res;
         }
 
-        if(t < maxd && t < res.x)
-            res = vec3(t, m);
+        """
 
-        return res;
-    }
-    """
-
-    blinn_phong_model = import_fury_shader(
-        os.path.join("lighting", "blinn_phong_model.frag")
-    )
+    blinn_phong_model = import_fury_shader(os.path.join(
+        'lighting', 'blinn_phong_model.frag'))
 
     fs_dec = compose_shader([fs_vars_dec, sdf_map, map_function_tex,
                              central_diffs_normal, cast_ray, blinn_phong_model])
@@ -469,38 +458,38 @@ if __name__ == '__main__':
     shader_to_actor(box_actor_uniform_3, 'fragment', decl_code=fs_dec_2)
     shader_to_actor(box_actor_template, 'fragment', decl_code=fs_dec_t3)
 
-    sdf_frag_impl = """
-    vec3 pnt = vertexMCVSOutput.xyz;
+    sdf_frag_impl = \
+        """
+        vec3 pnt = vertexMCVSOutput.xyz;
 
-    // Ray Origin
-    // Camera position in world space
-    vec3 ro = (-MCVCMatrix[3] * MCVCMatrix).xyz;
+        // Ray Origin
+        // Camera position in world space
+        vec3 ro = (-MCVCMatrix[3] * MCVCMatrix).xyz;
 
-    // Ray Direction
-    vec3 rd = normalize(pnt - ro);
+        // Ray Direction
+        vec3 rd = normalize(pnt - ro);
 
-    // Light Direction
-    vec3 ld = normalize(ro - pnt);
+        // Light Direction
+        vec3 ld = normalize(ro - pnt);
 
-    ro += pnt - ro;
+        ro += pnt - ro;
 
-    vec3 t = castRay(ro, rd);
+        vec3 t = castRay(ro, rd);
 
-    if(t.y > -.5)
-    {
-        vec3 pos = ro + t.y * rd;
+        if(t.y > -0.5 )
+        {
+            vec3 pos = ro + t.y * rd;
 
-        vec3 normal = centralDiffsNormals(pos);
+            vec3 normal = centralDiffsNormals(pos);
 
-        float occ = clamp(2 * t.z, 0, 1);
-        //float sss = pow(clamp(1 + dot(normal, rd), 0, 1), 1);
-        float sss = clamp(1 + dot(normal, rd), 0, 1);
+            float occ = clamp( 2.0*t.z, 0.0, 1.0 );
+            float sss = pow( clamp( 1.0 + dot(normal, rd), 0.0, 1.0 ), 1.0 );
 
-        // lights
-        vec3 lin  = 2.5 * occ * vec3(1) * (.6 + .4 * normal.y);
-        lin += 1 * sss * vec3(1, .95, .7) * occ;
+            // lights
+            vec3 lin  = 2.5*occ*vec3(1.0,1.0,1.0)*(0.6+0.4*normal.y);
+            lin += 1.0*sss*vec3(1.0,0.95,0.70)*occ;	
 
-        vec3 mater = .5 * mix(vec3(1, 1, 0), vec3(1), t.y);
+            vec3 mater = 0.5*mix( vec3(1.0,1.0,0.0), vec3(1.0,1.0,1.0), t.y); 	
 
             // ================================================================
             fragOutput0 = vec4( vec3(1,1,0)*lin, 1.0);
@@ -528,25 +517,28 @@ if __name__ == '__main__':
     show_manager.scene.add(box_actor_uniform_3)
     show_manager.scene.add(box_actor_template)
 
-    sphere = get_sphere("repulsion724")
+    from dipy.reconst.shm import sh_to_sf
+    from dipy.data import get_sphere
 
-    # sh_basis = "descoteaux07"
-    sh_basis = "tournier07"
+    sphere = get_sphere('repulsion724')
+
+    sh_basis = 'descoteaux07'
     sh_order = 4
+    tensor_sh = coeffs = np.array(
+        [[[[0.2820735, 0.15236554, -0.04038717, -0.11270988, -0.04532376,
+            0.14921817, 0.00257928, 0.0040734, -0.05313807, 0.03486542,
+            0.04083064, 0.02105767, -0.04389586, -0.04302812, 0.1048641]]],
+         [[[0.28549338, 0.0978267, -0.11544838, 0.12525354, -0.00126003,
+            0.00320594, 0.04744155, -0.07141446, 0.03211689, 0.04711322,
+            0.08064896, 0.00154299, 0.00086506, 0.00162543, -0.00444893]]],
+         [[[0.28208936, -0.13133252, -0.04701012, -0.06303016, -0.0468775,
+            0.02348355, 0.03991898, 0.02587433, 0.02645416, 0.00668765,
+            0.00890633, 0.02189304, 0.00387415, 0.01665629, -0.01427194]]]])
+    tensor_sf = sh_to_sf(tensor_sh, sh_order=4, basis_type='descoteaux07',
+                         sphere=sphere)
 
-    sh = np.zeros((3, 1, 1, 15))
-    sh[0, 0, 0, :] = coeffs[0, :]
-    sh[1, 0, 0, :] = coeffs[1, :]
-    sh[2, 0, 0, :] = coeffs[2, :]
+    odf_actor = actor.odf_slicer(tensor_sf, sphere=sphere, scale=0.5,
+                                 colormap='plasma')
+    show_manager.scene.add(odf_actor)
 
-    tensor_sf = sh_to_sf(
-        sh, sh_order=sh_order, basis_type=sh_basis, sphere=sphere
-    )
-
-    odf_slicer_actor = actor.odf_slicer(
-        tensor_sf, sphere=sphere, scale=0.5, colormap="plasma"
-    )
-
-    show_man.scene.add(odf_slicer_actor)
-
-    show_man.start()
+    show_manager.start()
