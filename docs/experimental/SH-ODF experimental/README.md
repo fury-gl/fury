6b682d3fe99243661d97b8c2a88975fb207f250e
Javier Guaje <jrguajeg@iu.edu>
2024-02-09 12:55:02 -0500

    Added ray_traced_4.0.py as an analogous version of ray_traced_2.0.py. Updated README.

--- docs/experimental/SH-ODF experimental/README.md
+++ docs/experimental/SH-ODF experimental/README.md
@@ -15,7 +15,7 @@ Germany. https://creativecommons.org/publicdomain/zero/1.0/
 
 The original paper implementation can be found in:
  - [ray_traced_1.0.py](ray_traced_1.0.py) for a single glyph
-  - [ray_traced_2.0.py](ray_traced_2.0.py) for multiple glyphs.
+ - [ray_traced_2.0.py](ray_traced_2.0.py) for multiple glyphs.
 
 > **Note:** We keep these files as they are for comparison purposes.
 
@@ -23,8 +23,14 @@ The original paper implementation can be found in:
 
 To better understand the base approach and being able to build on top on it we need a simplified yet functional version of it.
 
-- [ray_traced_3.0.py](ray_traced_3.0.py) simplifies the illumination model getting rid of additional parameters and adding compatibility with VTK's default lighting model. Here is a comparison between the simplified version and the original one:
+ - [ray_traced_3.0.py](ray_traced_3.0.py) simplifies the illumination model getting rid of additional parameters and adding compatibility with VTK's default lighting model. Here is a comparison between the simplified version and the original one:
 
 | BRDF lighting ([ray_traced_1.0.py](ray_traced_1.0.py)) | Blinn-Phong lighting ([ray_traced_3.0.py](ray_traced_3.0.py)) |
 |---|---|
 |<img width="350" src="https://github.com/tvcastillod/fury/assets/9929496/5b9d2a1e-9c14-4496-86d1-7d5484e8e038">|<img width="350" src="https://github.com/tvcastillod/fury/assets/9929496/c1f766c7-58d9-419f-b79a-7ae69df18493">|
+
+ - [ray_traced_4.0.py](ray_traced_4.0.py) is an analogous version of [ray_traced_2.0.py](ray_traced_2.0.py) but with the simplified illumination model introduced in [ray_traced_3.0.py](ray_traced_3.0.py). Here is a comparison between the simplified version and the original one:
+
+| BRDF lighting ([ray_traced_2.0.py](ray_traced_2.0.py)) | Blinn-Phong lighting ([ray_traced_4.0.py](ray_traced_4.0.py)) |
+|---|---|
+|<img width="350" src="https://github.com/tvcastillod/fury/assets/9929496/ab4af59e-2cd5-429a-9616-3511e0857a18">|<img width="350" src="https://github.com/tvcastillod/fury/assets/9929496/0509a424-b925-4f03-b862-f24e7bd17c2b">|
--- docs/experimental/SH-ODF experimental/ray_traced_4.0.py
+++ docs/experimental/SH-ODF experimental/ray_traced_4.0.py
@@ -1,5 +1,5 @@
 """
-Fury's simplified version of the script ray_traced_1.0.py.
+Fury's simplified version of the script ray_traced_2.0.py.
  - Simplified color calculation.
  - Simplified lighting.
 """
@@ -9,44 +9,128 @@ import os
 import numpy as np
 
 from fury import actor, window
+from fury.lib import FloatArray, Texture
 from fury.shaders import (
     attribute_to_actor,
     compose_shader,
     import_fury_shader,
     shader_to_actor,
 )
+from fury.utils import numpy_to_vtk_image_data, set_polydata_tcoords
+
+
+def uv_calculations(n):
+    uvs = []
+    for i in range(0, n):
+        a = (n - (i + 1)) / n
+        b = (n - i) / n
+        # glyph_coord [0, a], [0, b], [1, b], [1, a]
+        uvs.extend(
+            [
+                [0.1, a + 0.1],
+                [0.1, b - 0.1],
+                [0.9, b - 0.1],
+                [0.9, a + 0.1],
+                [0.1, a + 0.1],
+                [0.1, b - 0.1],
+                [0.9, b - 0.1],
+                [0.9, a + 0.1],
+            ]
+        )
+    return uvs
+
 
 if __name__ == "__main__":
     show_man = window.ShowManager(size=(1280, 720))
     show_man.scene.background((1, 1, 1))
 
-    centers = np.array([[0, 0, 0]])
-    scales = np.array([4])
+    # fmt: off
+    coeffs = np.array([
+        [
+            0.2820735, 0.15236554, -0.04038717, -0.11270988, -0.04532376,
+            0.14921817, 0.00257928, 0.0040734, -0.05313807, 0.03486542,
+            0.04083064, 0.02105767, -0.04389586, -0.04302812, 0.1048641
+        ],
+        [
+            0.28549338, 0.0978267, -0.11544838, 0.12525354, -0.00126003,
+            0.00320594, 0.04744155, -0.07141446, 0.03211689, 0.04711322,
+            0.08064896, 0.00154299, 0.00086506, 0.00162543, -0.00444893
+        ],
+        [
+            0.28208936, -0.13133252, -0.04701012, -0.06303016, -0.0468775,
+            0.02348355, 0.03991898, 0.02587433, 0.02645416, 0.00668765,
+            0.00890633, 0.02189304, 0.00387415, 0.01665629, -0.01427194
+        ]
+    ])
+    # fmt: on
 
-    odf_actor = actor.box(centers=centers, scales=scales)
+    centers = np.array([[0, -1, 0], [1, -1, 0], [2, -1, 0]])
+
+    odf_actor = actor.box(centers=centers, scales=1.0)
 
     big_centers = np.repeat(centers, 8, axis=0)
     attribute_to_actor(odf_actor, big_centers, "center")
 
-    big_scales = np.repeat(scales, 8, axis=0)
-    attribute_to_actor(odf_actor, big_scales, "scale")
+    minmax = np.array([coeffs.min(axis=1), coeffs.max(axis=1)]).T
+    big_minmax = np.repeat(minmax, 8, axis=0)
+    attribute_to_actor(odf_actor, big_minmax, "minmax")
+
+    odf_actor_pd = odf_actor.GetMapper().GetInput()
+
+    # fmt: off
+    uv_vals = np.array(uv_calculations(3))
+    # fmt: on
+
+    num_pnts = uv_vals.shape[0]
+
+    t_coords = FloatArray()
+    t_coords.SetNumberOfComponents(2)
+    t_coords.SetNumberOfTuples(num_pnts)
+    [t_coords.SetTuple(i, uv_vals[i]) for i in range(num_pnts)]
+
+    set_polydata_tcoords(odf_actor_pd, t_coords)
+
+    min = coeffs.min(axis=1)
+    max = coeffs.max(axis=1)
+    newmin = 0
+    newmax = 1
+    arr = np.array(
+        [
+            (coeffs[i] - min[i]) * ((newmax - newmin) / (max[i] - min[i]))
+            + newmin
+            for i in range(coeffs.shape[0])
+        ]
+    )
+    arr *= 255
+    grid = numpy_to_vtk_image_data(arr.astype(np.uint8))
+
+    texture = Texture()
+    texture.SetInputDataObject(grid)
+    texture.Update()
+
+    odf_actor.GetProperty().SetTexture("texture0", texture)
+
+    # TODO: Set int uniform
+    odf_actor.GetShaderProperty().GetFragmentCustomUniforms().SetUniformf(
+        "numCoeffs", 15
+    )
 
     vs_dec = """
     in vec3 center;
-    in float scale;
+    in vec2 minmax;
 
     out vec4 vertexMCVSOutput;
     out vec3 centerMCVSOutput;
+    out vec2 minmaxVSOutput;
     out vec3 camPosMCVSOutput;
     out vec3 camRightMCVSOutput;
     out vec3 camUpMCVSOutput;
-    out float scaleVSOutput;
     """
 
     vs_impl = """
     vertexMCVSOutput = vertexMC;
     centerMCVSOutput = center;
-    scaleVSOutput = scale;
+    minmaxVSOutput = minmax;
     camPosMCVSOutput = -MCVCMatrix[3].xyz * mat3(MCVCMatrix);
     camRightMCVSOutput = vec3(
         MCVCMatrix[0][0], MCVCMatrix[1][0], MCVCMatrix[2][0]);
@@ -92,10 +176,21 @@ if __name__ == "__main__":
     fs_vs_vars = """
     in vec4 vertexMCVSOutput;
     in vec3 centerMCVSOutput;
+    in vec2 minmaxVSOutput;
     in vec3 camPosMCVSOutput;
     in vec3 camRightMCVSOutput;
     in vec3 camUpMCVSOutput;
-    in float scaleVSOutput;
+    """
+
+    coeffs_norm = """
+    float coeffsNorm(float coef)
+    {
+        float min = 0;
+        float max = 1;
+        float newMin = minmaxVSOutput.x;
+        float newMax = minmaxVSOutput.y;
+        return (coef - min) * ((newMax - newMin) / (max - min)) + newMin;
+    }
     """
 
     eval_sh_2 = import_fury_shader(os.path.join("rt_odfs", "eval_sh_2.frag"))
@@ -239,13 +334,13 @@ if __name__ == "__main__":
     fs_dec = compose_shader([
         def_sh_degree, def_sh_count, def_max_degree,
         def_gl_ext_control_flow_attributes, def_no_intersection,
-        def_pis, fs_vs_vars, eval_sh_2, eval_sh_4, eval_sh_6, eval_sh_8,
-        eval_sh_10, eval_sh_12, eval_sh_grad_2, eval_sh_grad_4, eval_sh_grad_6,
-        eval_sh_grad_8, eval_sh_grad_10, eval_sh_grad_12, newton_bisection,
-        find_roots, eval_sh, eval_sh_grad, get_inv_vandermonde,
-        ray_sh_glyph_intersections, get_sh_glyph_normal, blinn_phong_model,
-        linear_to_srgb, srgb_to_linear, linear_rgb_to_srgb, srgb_to_linear_rgb,
-        tonemap
+        def_pis, fs_vs_vars, coeffs_norm, eval_sh_2, eval_sh_4, eval_sh_6,
+        eval_sh_8, eval_sh_10, eval_sh_12, eval_sh_grad_2, eval_sh_grad_4,
+        eval_sh_grad_6, eval_sh_grad_8, eval_sh_grad_10, eval_sh_grad_12,
+        newton_bisection, find_roots, eval_sh, eval_sh_grad,
+        get_inv_vandermonde, ray_sh_glyph_intersections, get_sh_glyph_normal,
+        blinn_phong_model, linear_to_srgb, srgb_to_linear,
+        linear_rgb_to_srgb, srgb_to_linear_rgb, tonemap
     ])
     # fmt: on
 
@@ -261,16 +356,6 @@ if __name__ == "__main__":
     # camera position/ray origin
     ray_direction = """
     vec3 rd = normalize(pnt - ro);
-    //float aspect = float(iResolution.x) / float(iResolution.y);
-    //float aspect = 1;
-    //float zoom = .8;
-    //vec3 right = (aspect / zoom) * vec3(3.0, 0.0, 0.0);
-    //vec3 up = (1.0 / zoom) * vec3(0.0, 0.0, 3.0);
-    //vec3 bottom_left = -0.5 * (right + up);
-    //vec2 frag_coord = gl_FragCoord.xy;
-    //vec2 uv = frag_coord / vec2(iResolution.xy);
-    //vec3 ray_dir = normalize(bottom_left + uv.x * right + uv.y * up - camera_pos);
-    //vec3 ray_dir = rd;
     """
 
     # Light direction in a retroreflective model is the normalized difference
@@ -279,71 +364,17 @@ if __name__ == "__main__":
 
     # Define SH coefficients (measured up to band 8, noise beyond that)
     sh_coeffs = """
+    float i = 1 / (numCoeffs * 2);
     float sh_coeffs[SH_COUNT];
-    sh_coeffs[0] = -0.2739740312099; sh_coeffs[1] = 0.2526670396328;
-    sh_coeffs[2] = 1.8922271728516; sh_coeffs[3] = 0.2878578901291;
-    sh_coeffs[4] = -0.5339795947075; sh_coeffs[5] = -0.2620058953762;
-    #if SH_DEGREE >= 4
-        sh_coeffs[6] = 0.1580424904823; sh_coeffs[7] = 0.0329004973173;
-        sh_coeffs[8] = -0.1322413831949; sh_coeffs[9] = -0.1332057565451;
-        sh_coeffs[10] = 1.0894461870193; sh_coeffs[11] = -0.6319401264191;
-        sh_coeffs[12] = -0.0416776277125; sh_coeffs[13] = -1.0772529840469;
-        sh_coeffs[14] = 0.1423762738705;
-    #endif
-    #if SH_DEGREE >= 6
-        sh_coeffs[15] = 0.7941166162491; sh_coeffs[16] = 0.7490307092667;
-        sh_coeffs[17] = -0.3428381681442; sh_coeffs[18] = 0.1024847552180;
-        sh_coeffs[19] = -0.0219132602215; sh_coeffs[20] = 0.0499043911695;
-        sh_coeffs[21] = 0.2162453681231; sh_coeffs[22] = 0.0921059995890;
-        sh_coeffs[23] = -0.2611238956451; sh_coeffs[24] = 0.2549301385880;
-        sh_coeffs[25] = -0.4534865319729; sh_coeffs[26] = 0.1922748684883;
-        sh_coeffs[27] = -0.6200597286224;
-    #endif
-    #if SH_DEGREE >= 8
-        sh_coeffs[28] = -0.0532187558711; sh_coeffs[29] = -0.3569841980934;
-        sh_coeffs[30] = 0.0293972902000; sh_coeffs[31] = -0.1977960765362;
-        sh_coeffs[32] = -0.1058669015765; sh_coeffs[33] = 0.2372217923403;
-        sh_coeffs[34] = -0.1856198310852; sh_coeffs[35] = -0.3373193442822;
-        sh_coeffs[36] = -0.0750469490886; sh_coeffs[37] = 0.2146576642990;
-        sh_coeffs[38] = -0.0490148440003; sh_coeffs[39] = 0.1288588196039;
-        sh_coeffs[40] = 0.3173974752426; sh_coeffs[41] = 0.1990085393190;
-        sh_coeffs[42] = -0.1736343950033; sh_coeffs[43] = -0.0482443645597;
-        sh_coeffs[44] = 0.1749017387629;
-    #endif
-    #if SH_DEGREE >= 10
-        sh_coeffs[45] = -0.0151847425660; sh_coeffs[46] = 0.0418366046081;
-        sh_coeffs[47] = 0.0863263587216; sh_coeffs[48] = -0.0649211244490;
-        sh_coeffs[49] = 0.0126096132283; sh_coeffs[50] = 0.0545089217982;
-        sh_coeffs[51] = -0.0275142164626; sh_coeffs[52] = 0.0399986574832;
-        sh_coeffs[53] = -0.0468244261610; sh_coeffs[54] = -0.1292105653111;
-        sh_coeffs[55] = -0.0786858322658; sh_coeffs[56] = -0.0663828464882;
-        sh_coeffs[57] = 0.0382439706831; sh_coeffs[58] = -0.0041550330365;
-        sh_coeffs[59] = -0.0502800566338; sh_coeffs[60] = -0.0732471630735;
-        sh_coeffs[61] = 0.0181751900972; sh_coeffs[62] = -0.0090119333757;
-        sh_coeffs[63] = -0.0604443282359; sh_coeffs[64] = -0.1469985252752;
-        sh_coeffs[65] = -0.0534046899715;
-    #endif
-    #if SH_DEGREE >= 12
-        sh_coeffs[66] = -0.0896672753415; sh_coeffs[67] = -0.0130841364808;
-        sh_coeffs[68] = -0.0112942893801; sh_coeffs[69] = 0.0272257498541;
-        sh_coeffs[70] = 0.0626717616331; sh_coeffs[71] = -0.0222197983050;
-        sh_coeffs[72] = -0.0018541504308; sh_coeffs[73] = -0.1653251944056;
-        sh_coeffs[74] = 0.0409697402846; sh_coeffs[75] = 0.0749921454327;
-        sh_coeffs[76] = -0.0282830872616; sh_coeffs[77] = 0.0006909458525;
-        sh_coeffs[78] = 0.0625599842287; sh_coeffs[79] = 0.0812529816082;
-        sh_coeffs[80] = 0.0914693020772; sh_coeffs[81] = -0.1197222726745;
-        sh_coeffs[82] = 0.0376277453183; sh_coeffs[83] = -0.0832617004142;
-        sh_coeffs[84] = -0.0482175038043; sh_coeffs[85] = -0.0839003635737;
-        sh_coeffs[86] = -0.0349423908400; sh_coeffs[87] = 0.1204519568256;
-        sh_coeffs[88] = 0.0783745984003; sh_coeffs[89] = 0.0297401205976;
-        sh_coeffs[90] = -0.0505947662525;
-    #endif
+    for(int j=0; j<numCoeffs; j++){
+        sh_coeffs[j] = coeffsNorm(texture(texture0, vec2(i + j / numCoeffs, tcoordVCVSOutput.y)).x);
+    }
     """
 
     # Perform the intersection test
     intersection_test = """
     float ray_params[MAX_DEGREE];
-    ray_sh_glyph_intersections(ray_params, sh_coeffs, ro, rd);
+    ray_sh_glyph_intersections(ray_params, sh_coeffs, ro - centerMCVSOutput, rd);
     """
 
     # Identify the first intersection
@@ -362,7 +393,8 @@ if __name__ == "__main__":
     directional_light = """
     vec3 color = vec3(1.);
     if (first_ray_param != NO_INTERSECTION) {
-        vec3 intersection = ro + first_ray_param * rd;
+        //vec3 intersection = ro + first_ray_param * rd;
+        vec3 intersection = ro - centerMCVSOutput + first_ray_param * rd;
         vec3 normal = get_sh_glyph_normal(sh_coeffs, intersection);
         vec3 colorDir = srgb_to_linear_rgb(abs(normalize(intersection)));
         float attenuation = dot(ld, normal);
@@ -370,6 +402,8 @@ if __name__ == "__main__":
             //attenuation, lightColor0, diffuseColor, specularPower,
             attenuation, lightColor0, colorDir, specularPower,
             specularColor, ambientColor);
+    } else {
+        discard;
     }
     """
 
@@ -387,8 +421,7 @@ if __name__ == "__main__":
     ])
     # fmt: on
 
-    shader_to_actor(odf_actor, "fragment", impl_code=fs_impl, block="light")
-
+    shader_to_actor(odf_actor, "fragment", impl_code=fs_impl, block="picking")
     show_man.scene.add(odf_actor)
 
     show_man.start()
