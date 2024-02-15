fixed scaling

docs/experimental/SH-ODF experimental/ray_traced_6.0.py
@@ -75,11 +75,10 @@ if __name__ == "__main__":
     x, y, z, s = coeffs.shape
     coeffs = coeffs[:, :, :].reshape((x * y * z, s))
     n_glyphs = coeffs.shape[0]
-
-    coeffs = np.array(coeffs) * 1.6
     
     max_val = coeffs.min(axis=1)
-    #coeffs = np.dot(np.abs(np.diag(1/max_val)), coeffs) 
+    total = np.sum(abs(coeffs), axis=1)
+    coeffs = np.dot(np.diag(1/total), coeffs) * 1.7
     
     odf_actor = actor.box(centers=centers, scales=1.0)
     odf_actor.GetMapper().SetVBOShiftScaleMethod(False)
@@ -452,7 +451,7 @@ if __name__ == "__main__":
         intersection_test, first_intersection, directional_light, frag_output
     ])
     # fmt: on
-
+    show_man.scene.background([0,0,0])
     shader_to_actor(odf_actor, "fragment", impl_code=fs_impl, block="picking")
     show_man.scene.add(odf_actor)
 