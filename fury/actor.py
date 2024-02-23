18dc83a5727ff880657b9bfc98224ee1f3a531fd
tvcastillod <tvcastillod@unal.edu.co>
2024-02-23 11:19:51 -0500

    adjusted scaling

--- fury/actor.py
+++ fury/actor.py
@@ -4104,6 +4104,9 @@ def odf(
     elif scales.size != centers.shape[0]:
         scales = np.concatenate(
             (scales, np.ones(centers.shape[0] - scales.shape[0])), axis=None)
+        
+    total = np.sum(abs(coeffs), axis=1)
+    coeffs = np.dot(np.diag(1 / total * scales), coeffs) * 1.7
 
     total = np.sum(abs(coeffs), axis=1)
     coeffs = np.dot(np.diag(1 / total * scales), coeffs) * 1.7
