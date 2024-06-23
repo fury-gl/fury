28de27737ecc62f3b8a18ff3825e28e797ee4a0d
WassCodeur <wachioubouraima56@gmail.com>
2024-06-23 09:00:00 +0000

    RF: Add keyword arguments to module: shaders

--- fury/shaders/tests/test_base.py
+++ fury/shaders/tests/test_base.py
@@ -232,7 +232,7 @@ def test_add_shader_callback():
         if program is not None:
             test_values.append(0)
 
-    id_observer = add_shader_callback(cone_actor, callbackLow, 0)
+    id_observer = add_shader_callback(cone_actor, callbackLow, priority=0)
 
     with pytest.raises(Exception):  # noqa: B017
         add_shader_callback(cone_actor, callbackLow, priority="str")
@@ -258,10 +258,10 @@ def test_add_shader_callback():
         if program is not None:
             test_values.append(500)
 
-    add_shader_callback(cone_actor, callbackHigh, 999)
-    add_shader_callback(cone_actor, callbackLow, 0)
+    add_shader_callback(cone_actor, callbackHigh, priority=999)
+    add_shader_callback(cone_actor, callbackLow, priority=0)
 
-    id_mean = add_shader_callback(cone_actor, callbackMean, 500)
+    id_mean = add_shader_callback(cone_actor, callbackMean, priority=500)
 
     # check the priority of each call
     window.snapshot(scene, size=(200, 200))
