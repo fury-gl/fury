4c23f0a8837f4b73e74087b3d3604e6b9448970c
Mohamed Abouagour <mo.aggour@gmail.com>
2025-01-20 19:11:46 -0500

    TEST: Restored actors visual tests

--- fury/tests/test_actor.py
+++ fury/tests/test_actor.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numpy.testing as npt
+from PIL import Image
 
 from fury import actor, window
 
@@ -13,24 +14,24 @@ def test_sphere():
     sphere_actor = actor.sphere(centers=centers, colors=colors, radii=radii)
     scene.add(sphere_actor)
 
-    # window.record(scene=scene, fname="sphere_test_1.png")
+    window.snapshot(scene=scene, fname="sphere_test_1.png")
 
-    # img = Image.open("sphere_test_1.png")
-    # img_array = np.array(img)
+    img = Image.open("sphere_test_1.png")
+    img_array = np.array(img)
 
-    # mean_r, mean_g, mean_b, _ = np.mean(
-    #     img_array.reshape(-1, img_array.shape[2]), axis=0
-    # )
+    mean_r, mean_g, mean_b, _ = np.mean(
+        img_array.reshape(-1, img_array.shape[2]), axis=0
+    )
 
-    # assert mean_r > mean_b and mean_r > mean_g
-    # assert 0 <= mean_r <= 255 and 0 <= mean_g <= 255 and 0 <= mean_b <= 255
+    assert mean_r > mean_b and mean_r > mean_g
+    assert 0 <= mean_r <= 255 and 0 <= mean_g <= 255 and 0 <= mean_b <= 255
 
     npt.assert_array_equal(sphere_actor.local.position, centers[0])
 
     assert sphere_actor.prim_count == 1
 
-    # center_pixel = img_array[img_array.shape[0] // 2, img_array.shape[1] // 2]
-    # npt.assert_array_equal(center_pixel[0], colors[0][0] * 255)
+    center_pixel = img_array[img_array.shape[0] // 2, img_array.shape[1] // 2]
+    npt.assert_array_equal(center_pixel[0], colors[0][0] * 255)
 
     phi, theta = 100, 100
     sphere_actor_2 = actor.sphere(
@@ -45,21 +46,20 @@ def test_sphere():
     scene.remove(sphere_actor)
     scene.add(sphere_actor_2)
 
-    # TODO: Record does not exist in the v2, test needs to be update
-    # window.record(scene=scene, fname="sphere_test_2.png")
+    window.snapshot(scene=scene, fname="sphere_test_2.png")
 
-    # img = Image.open("sphere_test_2.png")
-    # img_array = np.array(img)
+    img = Image.open("sphere_test_2.png")
+    img_array = np.array(img)
 
-    # mean_r, mean_g, mean_b, mean_a = np.mean(
-    #     img_array.reshape(-1, img_array.shape[2]), axis=0
-    # )
+    mean_r, mean_g, mean_b, mean_a = np.mean(
+        img_array.reshape(-1, img_array.shape[2]), axis=0
+    )
 
-    # assert mean_r > mean_b and mean_r > mean_g
-    # assert 0 <= mean_r <= 255 and 0 <= mean_g <= 255 and 0 <= mean_b <= 255
-    # assert mean_a == 255.0
-    # assert mean_b == 0.0
-    # assert mean_g == 0.0
+    assert mean_r > mean_b and mean_r > mean_g
+    assert 0 <= mean_r <= 255 and 0 <= mean_g <= 255 and 0 <= mean_b <= 255
+    assert mean_a == 255.0
+    assert mean_b == 0.0
+    assert mean_g == 0.0
 
     vertices = sphere_actor_2.geometry.positions.view
     faces = sphere_actor_2.geometry.indices.view
