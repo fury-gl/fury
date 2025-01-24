c3d3c23bfc17e23f24206bb814333af09ca6757c
Manish Reddy Rakasi <116704419+ManishReddyR@users.noreply.github.com>
2025-01-24 16:34:54 -0500

    added actor for cylinder (#959)
    * NF: Adding square actor and cylinder actor

--- fury/tests/test_actor.py
+++ fury/tests/test_actor.py
@@ -91,3 +91,85 @@ def test_box():
     assert box_actor.prim_count == 1
 
     scene.remove(box_actor)
+
+
+def test_cylinder():
+    scene = window.Scene()
+    centers = np.array([[0, 0, 0]])
+    colors = np.array([[1, 0, 0]])
+    sectors = 36
+    capped = True
+
+    cylinder_actor = actor.cylinder(
+        centers=centers, colors=colors, sectors=sectors, capped=capped
+    )
+    scene.add(cylinder_actor)
+
+    npt.assert_array_equal(cylinder_actor.local.position, centers[0])
+
+    mean_vertex = np.mean(cylinder_actor.geometry.positions.view, axis=0)
+    npt.assert_array_almost_equal(mean_vertex, centers[0], decimal=2)
+
+    assert cylinder_actor.prim_count == 1
+
+    # window.snapshot(scene=scene, fname="cylinder_test_1.png")
+
+    # img = Image.open("cylinder_test_1.png")
+    # img_array = np.array(img)
+
+    # mean_r, mean_g, mean_b, mean_a = np.mean(
+    #     img_array.reshape(-1, img_array.shape[2]), axis=0
+    # )
+
+    # assert mean_r > mean_b and mean_r > mean_g
+    # assert 0 < mean_r < 255 and 0 < mean_g < 255 and 0 <= mean_b < 255
+
+    # middle_pixel = img_array[img_array.shape[0] // 2, img_array.shape[1] // 2]
+    # r, g, b, a = middle_pixel
+    # assert r > g and r > b
+    # assert g == b
+    # assert r > 0 and g > 0 and b > 0
+
+    scene.remove(cylinder_actor)
+
+    # cylinder_actor_2 = actor.cylinder(
+    #     centers=centers, colors=colors, sectors=sectors, capped=capped,
+    #  material="basic"
+    # )
+    # scene.add(cylinder_actor_2)
+    # window.snapshot(scene=scene, fname="cylinder_test_2.png")
+
+    # img = Image.open("cylinder_test_2.png")
+    # img_array = np.array(img)
+
+    # mean_r, mean_g, mean_b, mean_a = np.mean(
+    #     img_array.reshape(-1, img_array.shape[2]), axis=0
+    # )
+
+    # assert mean_r > mean_b and mean_r > mean_g
+    # assert 0 < mean_r < 255
+    # assert mean_g == 0 and mean_b == 0
+
+    # middle_pixel = img_array[img_array.shape[0] // 2, img_array.shape[1] // 2]
+    # r, g, b, a = middle_pixel
+    # assert r > g and r > b
+    # assert g == 0 and b == 0
+    # assert r == 255
+    # scene.remove(cylinder_actor_2)
+
+
+def test_square():
+    scene = window.Scene()
+    centers = np.array([[0, 0, 0]])
+    colors = np.array([[1, 0, 0]])
+
+    square_actor = actor.square(centers=centers, colors=colors)
+    scene.add(square_actor)
+
+    npt.assert_array_equal(square_actor.local.position, centers[0])
+
+    mean_vertex = np.mean(square_actor.geometry.positions.view, axis=0)
+    npt.assert_array_almost_equal(mean_vertex, centers[0])
+
+    assert square_actor.prim_count == 1
+    scene.remove(square_actor)
