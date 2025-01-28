e4245bca95bb76b46a1614620ca5c36ce1323b75
Manish Reddy Rakasi <manishreddyrakasi@gmail.com>
2025-01-28 17:04:07 -0500

    NF: Adding actor for triangularprism, TEST: Adding uni tests for triangularprism.

--- fury/actor.py
+++ fury/actor.py
@@ -635,3 +635,68 @@ def rhombicuboctahedron(
         material=material,
         enable_picking=enable_picking,
     )
+
+
+def triangularprism(
+    centers,
+    *,
+    directions=(0, 0, 0),
+    colors=(1, 1, 1),
+    scales=(1, 1, 1),
+    opacity=None,
+    material="phong",
+    enable_picking=True,
+):
+    """Visualize one or many triangularprism with different features.
+
+    Parameters
+    ----------
+    centers : ndarray, shape (N, 3)
+        triangularprism positions.
+    directions : ndarray, shape (N, 3), optional
+        The orientation vector of the triangularprism.
+    colors : ndarray (N,3) or (N, 4) or tuple (3,) or tuple (4,), optional
+        RGB or RGBA (for opacity) R, G, B and A should be at the range [0, 1].
+    scales : int or ndarray (N,3) or tuple (3,), optional
+        The size of the triangularprism in each dimension. If a single value is
+        provided, the same size will be used for all triangularprism.
+    opacity : float, optional
+        Takes values from 0 (fully transparent) to 1 (opaque).
+        If both `opacity` and RGBA are provided, the final alpha will be:
+        final_alpha = alpha_in_RGBA * opacity
+    material : str, optional
+        The material type for the triangularprisms. Options are 'phong' and 'basic'.
+    enable_picking : bool, optional
+        Whether the triangularprisms should be pickable in a 3D scene.
+
+    Returns
+    -------
+    mesh_actor : Actor
+        A mesh actor containing the generated triangularprisms, with the specified
+        material and properties.
+
+    Examples
+    --------
+    >>> from fury import window, actor
+    >>> import numpy as np
+    >>> scene = window.Scene()
+    >>> centers = np.random.rand(5, 3) * 10
+    >>> colors = np.random.rand(5, 3)
+    >>> triangularprism_actor = actor.triangularprism(
+    centers=centers, colors=colors)
+    >>> scene.add(triangularprism_actor)
+    >>> show_manager = window.ShowManager(scene=scene, size=(600, 600))
+    >>> show_manager.start()
+    """
+    vertices, faces = fp.prim_triangularprism()
+    return actor_from_primitive(
+        vertices,
+        faces,
+        centers=centers,
+        colors=colors,
+        scales=scales,
+        directions=directions,
+        opacity=opacity,
+        material=material,
+        enable_picking=enable_picking,
+    )
