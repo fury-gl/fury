f3a4a3bb70ce6374fcfbe6735cfecf0a93c90574
Praneeth Shetty <praneethshetty10@gmail.com>
2025-07-05 15:26:27 +0530

    test: adding tests for adding/removing actors to/from scene

--- fury/tests/test_window.py
+++ fury/tests/test_window.py
@@ -336,3 +336,126 @@ def test_display_default(sample_actor):
     with patch("fury.window.ShowManager") as mock_show_manager:
         show([sample_actor])
         mock_show_manager.assert_called_once()
+
+
+def test_add_remove_ui_to_from_scene(sample_actor):
+    """Test add/remove ui hierarchy to/from scene."""
+
+    parent = Rectangle2D()
+    child_1, child_2, child_3 = (
+        Rectangle2D(),
+        Rectangle2D(),
+        Rectangle2D(),
+    )
+    subchild_21 = Rectangle2D()
+    subchild_31, subchild_32 = Rectangle2D(), Rectangle2D()
+
+    parent.children.extend([child_1, child_2, child_3])
+    child_2.children.append(subchild_21)
+    child_3.children.extend([subchild_31, subchild_32])
+
+    all_ui_objects = [
+        parent,
+        child_1,
+        child_2,
+        child_3,
+        subchild_21,
+        subchild_31,
+        subchild_32,
+    ]
+    all_ui_actors = []
+    for ui_obj in all_ui_objects:
+        all_ui_actors.extend(ui_obj.actors)
+
+    scene = Scene()
+
+    # Test Add and Remove Parent
+    scene.add(parent)
+
+    assert len(scene.ui_scene.children) == 1 + len(all_ui_actors)
+    assert parent in scene.ui_elements
+    assert all(actor in scene.ui_scene.children for actor in all_ui_actors)
+
+    scene.remove(parent)
+
+    assert len(scene.ui_elements) == 0
+    assert len(scene.ui_scene.children) == 1  # UI Camera
+    assert all(actor not in scene.ui_scene.children for actor in all_ui_actors)
+
+    # Test Add and Clear Parent
+    scene.add(parent)
+
+    assert len(scene.ui_scene.children) == 1 + len(all_ui_actors)
+    assert len(scene.ui_elements) == 1
+
+    scene.clear()
+
+    assert len(scene.ui_scene.children) == 1  # UI camera
+    assert len(scene.ui_elements) == 0
+
+    # Add Parent, Remove Child
+    scene.add(parent)
+
+    to_be_removed_ui = [child_3, subchild_31, subchild_32]
+    to_be_removed_actors = []
+    for ui_obj in to_be_removed_ui:
+        to_be_removed_actors.extend(ui_obj.actors)
+
+    should_remain_ui = [parent, child_1, child_2, subchild_21]
+    should_remain_actors = []
+    for ui_obj in should_remain_ui:
+        should_remain_actors.extend(ui_obj.actors)
+
+    assert len(scene.ui_elements) == 1
+    assert len(scene.ui_scene.children) == 1 + len(
+        all_ui_actors
+    )  # UI Camera + UI actors
+
+    scene.remove(child_3)
+
+    assert parent in scene.ui_elements
+    for ui_obj in to_be_removed_ui:
+        assert all(actor not in scene.ui_scene.children for actor in ui_obj.actors)
+
+    for ui_obj in should_remain_ui:
+        assert all(actor in scene.ui_scene.children for actor in ui_obj.actors)
+
+    assert len(scene.ui_elements) == 1
+    assert len(scene.ui_scene.children) == 1 + len(should_remain_actors)
+
+    # Remove NonExistant Child
+    scene.remove(subchild_31)
+
+    assert parent in scene.ui_elements
+    for ui_obj in to_be_removed_ui:
+        assert all(actor not in scene.ui_scene.children for actor in ui_obj.actors)
+
+    for ui_obj in should_remain_ui:
+        assert all(actor in scene.ui_scene.children for actor in ui_obj.actors)
+
+    assert len(scene.ui_elements) == 1
+    assert len(scene.ui_scene.children) == 1 + len(should_remain_actors)
+
+
+def test_add_to_scene(sample_actor, sample_ui_actor):
+    """Test add/remove ui hierarchy to/from scene."""
+
+    scene = Scene()
+
+    sample_gfx_scene = GfxScene()
+    sample_camera = PerspectiveCamera()
+    scene.add(sample_actor, sample_ui_actor, sample_gfx_scene, sample_camera)
+
+    assert sample_actor in scene.main_scene.children
+    assert sample_camera in scene.main_scene.children
+    assert sample_ui_actor in scene.ui_elements
+    assert sample_ui_actor.actors[0] in scene.ui_scene.children
+    assert sample_gfx_scene in scene.children
+
+    scene.remove(sample_actor, sample_ui_actor, sample_gfx_scene, sample_camera)
+
+    assert sample_actor not in scene.main_scene.children
+    assert sample_camera not in scene.main_scene.children
+    assert sample_ui_actor not in scene.ui_elements
+    assert sample_ui_actor.actors[0] not in scene.ui_scene.children
+    assert sample_gfx_scene not in scene.children
