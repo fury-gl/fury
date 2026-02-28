b80d87004891476ece4201de85146a8c86bf4484
medha-14 <medha140904@gmail.com>
2026-02-28 10:33:43 +0530

    changes textbox to v2 architecture

--- fury/ui/__init__.pyi
+++ fury/ui/__init__.pyi
@@ -10,7 +10,7 @@ __all__ = [
     "TexturedButton2D",
     "TextButton2D",
     "LineSlider2D",
-    #     "TextBox2D",
+    "TextBox2D",
     #     "LineDoubleSlider2D",
     #     "RingSlider2D",
     #     "RangeSlider",
@@ -48,7 +48,7 @@ __all__ = [
 from .containers import Panel2D
 from .context import UIContext
 from .core import UI, Anchor, Disk2D, Rectangle2D, TextBlock2D
-from .elements import LineSlider2D, TextButton2D, TexturedButton2D
+from .elements import LineSlider2D, TextBox2D, TextButton2D, TexturedButton2D
 
 # from .elements import (
 #     Card2D,
